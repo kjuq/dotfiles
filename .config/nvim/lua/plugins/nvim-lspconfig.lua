c0e2b55af07196a486b809d9e1a78deb18073ad2
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-13 09:52:48 +0900

    [Mod] lsp format keybinding

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -33,7 +33,7 @@ return {
                 vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
                 vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
                 vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<leader>fm', function()
+                vim.keymap.set('n', '<leader>lv', function()
                     vim.lsp.buf.format { async = true }
                 end, opts)
             end,
