814dc288980f3d13a3d1d1d64c5e02e421bdeffb
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-20 18:36:35 +0900

    Tune up lazy-loading Neovim plugins using CursorHold

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -1,12 +1,12 @@
 return {
     'akinsho/bufferline.nvim',
     version = "*",
-    event = { "BufNewFile", "BufReadPost" },
+    event = { "CursorHold" },
     dependencies = 'nvim-tree/nvim-web-devicons',
     opts = {
         options = {
             diagnostics = "nvim_lsp",
-            always_show_bufferline = true,
+            always_show_bufferline = false,
             separator_style = "slant",
             show_buffer_close_icons = false,
             color_icons = true,
