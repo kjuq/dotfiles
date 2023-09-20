560c80947aeb8374a69de1bf6f25161e86ced5d9
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-20 20:02:14 +0900

    Disable auto comment-out in Neovim

---
+++ .config/nvim/after/plugin/common.lua
@@ -0,0 +1,9 @@
+-- Disable auto comment-out
+vim.api.nvim_create_autocmd({ "FileType" }, {
+    pattern = "*",
+    callback = function()
+        vim.opt_local.formatoptions:remove("r")
+        vim.opt_local.formatoptions:remove("o")
+    end
+})
+
