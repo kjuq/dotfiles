745c9e660fee41deb1a09901edda2104e817e599
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-20 20:02:14 +0900

    Disable auto comment-out in Neovim

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,11 +1,3 @@
-vim.api.nvim_create_autocmd({ "FileType" }, {
-    pattern = "*",
-    callback = function()
-        vim.opt_local.formatoptions:remove("r")
-        vim.opt_local.formatoptions:remove("o")
-    end
-})
-
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*.snippets",
     callback = function()
