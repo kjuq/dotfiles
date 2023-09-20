9dcb113ed09580dbd9608cf5f4f75de87fc73697
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-20 20:05:00 +0900

    Tune lazy load

--- .config/nvim/lua/plugins/nvim-osc52.lua
+++ .config/nvim/lua/plugins/nvim-osc52.lua
@@ -1,7 +1,7 @@
 return {
     "ojroques/nvim-osc52",
-    event = { "BufWinEnter" },
-    config = function ()
+    event = { "BufNewFile", "BufReadPost" },
+    opts = function ()
         local function copy(lines, _)
             require('osc52').copy(table.concat(lines, '\n'))
         end
@@ -17,6 +17,12 @@ return {
         }
 
         -- Now the '+' register will copy to system clipboard using OSC52
+
+        return {
+            max_length = 0,     -- Maximum length of selection (0 for no limit)
+            silent     = true,  -- Disable message on successful copy
+            trim       = true,  -- Trim surrounding whitespaces before copy
+        }
     end,
 }
 
