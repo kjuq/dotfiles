49a4502a2ebedcfa4eba87a034d6fa23f481efcc
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-21 15:54:07 +0900

    Set up lspsaga

--- .config/nvim/lua/plugins/lspsaga.lua
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -1,27 +1,84 @@
 return {
     "nvimdev/lspsaga.nvim",
-    -- event = { "LspAttach" },
+    event = { "LspAttach" },
     config = function()
         require("lspsaga").setup({
+            callhierarchy = {
+                keys = {
+                    quit = { "q", "<Esc>" },
+                },
+            },
+            code_action = {
+                keys = {
+                    quit = { "q", "<Esc>" },
+                },
+            },
+            definition = {
+                keys = {
+                    quit = { "q", "<Esc>" },
+                }
+            },
+            diagnostic = {
+                max_height = 0.8,
+                keys = {
+                    quit = { "q", "<Esc>" },
+                }
+            },
+            finder = {
+                max_height = 0.6,
+                keys = {
+                    toggle_or_open = { "o", "<CR>" },
+                    quit = { "q", "<ESC>" },
+                }
+            },
             lightbulb = {
                 sign = false,
             },
-        })
-        require("lualine").setup({
-            sections = {
-                lualine_c = {
-                    require("lspsaga.symbol.winbar").get_bar()
+            outline = {
+                win_width = 45,
+                auto_preview = false,
+                detail = true,
+                keys = {
+                    toggle_or_jump = { "o", "<CR>" },
+                    quit = { "q", "<Esc>" },
+                }
+            },
+            rename = {
+                in_select = false,
+                keys = {
+                    quit = { "q", "<Esc>" },
                 },
             },
         })
-        vim.keymap.set("n", "K",  "<cmd>Lspsaga hover_doc<CR>")
-        vim.keymap.set('n', '<leader>lf', '<cmd>Lspsaga lsp_finder<CR>')
+        -- hierarchy
+        vim.keymap.set("n", "<leader>hi", "<cmd>Lspsaga incoming_calls<CR>")
+        vim.keymap.set("n", "<leader>ho", "<cmd>Lspsaga outgoing_calls<CR>")
+
+        -- code action
+        vim.keymap.set({ "n", "v" }, "<leader>ca", "<cmd>Lspsaga code_action<CR>")
+
+        -- definition
         vim.keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>")
-        vim.keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>")
-        vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
-        vim.keymap.set("n", "<leader>q", "<cmd>Lspsaga show_line_diagnostics<CR>")
+        vim.keymap.set("n", "<leader>td", "<cmd>Lspsaga peek_type_definition<CR>")
+        vim.keymap.set("n", "<leader>gd", "<cmd>Lspsaga goto_definition<CR>")
+        vim.keymap.set("n", "<leader><Space>td", "<cmd>Lspsaga goto_type_definition<CR>")
+
+        -- diagnostic
+        vim.keymap.set("n", "<leader>e", "<cmd>Lspsaga show_line_diagnostics<CR>")
         vim.keymap.set("n", "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>")
         vim.keymap.set("n", "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>")
+
+        -- finder
+        vim.keymap.set('n', '<leader>rf', '<cmd>Lspsaga finder def+ref+imp<CR>')
+
+        -- hover
+        vim.keymap.set("n", "K",  "<cmd>Lspsaga hover_doc<CR>")
+
+        -- outline
+        vim.keymap.set("n", "<leader>ol", "<Cmd>Lspsaga outline<CR>")
+
+        -- rename
+        vim.keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>")
 
     end,
     dependencies = {
--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -3,37 +3,47 @@ return {
     config = function()
         -- Global mappings.
         -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
-        vim.keymap.set('n', '[e', vim.diagnostic.goto_prev)
-        vim.keymap.set('n', ']e', vim.diagnostic.goto_next)
-        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+        -- vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+        -- vim.keymap.set("n", "[e", vim.diagnostic.goto_prev)
+        -- vim.keymap.set("n", "]e", vim.diagnostic.goto_next)
+        -- vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist)
 
         -- Use LspAttach autocommand to only map the following keys
         -- after the language server attaches to the current buffer
-        vim.api.nvim_create_autocmd('LspAttach', {
-            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+
+        vim.api.nvim_create_autocmd("LspAttach", {
+            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
+
                 -- Enable completion triggered by <c-x><c-o>
-                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
                 -- Buffer local mappings.
                 -- See `:help vim.lsp.*` for documentation on any of the below functions
+
                 local opts = { buffer = ev.buf }
-                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-                vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
-                vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
-                vim.keymap.set('n', '<leader>wl', function()
+
+                -- vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+                -- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+                -- vim.keymap.set("n", "<leader>rf", vim.lsp.buf.references, opts)
+                -- vim.keymap.set("n", "<leader>td", vim.lsp.buf.type_definition, opts)
+                -- vim.keymap.set("n", "<leader>im", vim.lsp.buf.implementation, opts)
+
+                -- vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+                -- vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+                -- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+                vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+
+                vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set("n", "<leader>wl", function()
                     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
                 end, opts)
-                vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
-                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-                vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
-                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                vim.keymap.set('n', '<leader>lv', function()
+
+                vim.keymap.set("n", "<leader>lv", function()
                     vim.lsp.buf.format { async = true }
                 end, opts)
             end,
