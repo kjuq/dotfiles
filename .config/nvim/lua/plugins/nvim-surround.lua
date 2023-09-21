4e70260011329638e730d29e3142b5390294710e
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-21 19:48:47 +0900

    Add Comment.nvim and nvim-surround

---
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,12 @@
+return {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    opts = {},
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+}
+
+
