4512b069a442f0eb71103b80b68811cc0035e65e
kjuq <44694396+kjuq@users.noreply.github.com>
2023-09-27 22:07:02 +0900

    Mod: eyeliner.nvim

--- .config/nvim/lua/plugins/eyeliner.lua
+++ .config/nvim/lua/plugins/eyeliner.lua
@@ -5,6 +5,10 @@ return {
         { "F", mode = { "n", "x" } },
         { "t", mode = { "n", "x" } },
         { "T", mode = { "n", "x" } },
+        { "df", mode = { "n" } },
+        { "dF", mode = { "n" } },
+        { "dt", mode = { "n" } },
+        { "dT", mode = { "n" } },
     },
     opts = {
         highlight_on_key = true, -- this must be set to true for dimming to work!
