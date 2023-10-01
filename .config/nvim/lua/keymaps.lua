56ce54cbbc74e1dd8fd476fdf721816e10ffeb47
kjuq <44694396+kjuq@users.noreply.github.com>
2023-10-01 21:41:04 +0900

    Mod: tuned nvim options according to kickstart.nvim

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -1,8 +1,10 @@
+-- Space is leader key
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Disable Ctrl-c to allow <C-c> keymappings (e.g. <C-w><C-c>, etc
-vim.keymap.set("n", "<C-c>", "<Nop>")
+vim.keymap.set("n", "<C-c>", "<Nop>", { silent = true })
 
 -- Emacs-like cursor movement in command mode
 vim.keymap.set("c", "<C-b>", "<Left>") -- Jumps to the beginning of a line by default
@@ -12,27 +14,30 @@ vim.keymap.set("c", "<C-d>", "<Del>") -- Lists completions by default, so <C-i> 
 -- vim.keymap.set("c", "<C-k>", "<c-\>e getcmdpos() == 1 ? '' : getcmdline()[:getcmdpos()-2]<CR>") -- Digraph is important
 
 -- Don't update register when not intend to do so
-vim.keymap.set("n", "x", "\"_x")
-vim.keymap.set("n", "c", "\"_c")
-vim.keymap.set("n", "D", "\"_D")
+vim.keymap.set("n", "x", "\"_x", { silent = true })
+vim.keymap.set("n", "s", "\"_s", { silent = true })
+vim.keymap.set("n", "c", "\"_c", { silent = true })
+vim.keymap.set("n", "d", "\"_d", { silent = true })
+vim.keymap.set("n", "D", "\"_D", { silent = true })
+vim.keymap.set("x", "p", "P", { silent = true })
+vim.keymap.set("x", "P", "p", { silent = true })
 
 -- Move caret on display lines
-vim.keymap.set("n", "j", "gj")
-vim.keymap.set("n", "k", "gk")
-vim.keymap.set("v", "j", "gj")
-vim.keymap.set("v", "k", "gk")
+-- Comfortable line specify movement by v:count
+vim.keymap.set({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Buffer movement instead of tab's
-vim.keymap.set("n", "gt", "<Cmd>bnext<CR>")
-vim.keymap.set("n", "gT", "<Cmd>bprevious<CR>")
+vim.keymap.set("n", "gt", "<Cmd>bnext<CR>", { silent = true })
+vim.keymap.set("n", "gT", "<Cmd>bprevious<CR>", { silent = true })
 
 -- Center cursor when searching
-vim.keymap.set("n", "n", "nzz")
-vim.keymap.set("n", "N", "Nzz")
+vim.keymap.set("n", "n", "nzz", { silent = true })
+vim.keymap.set("n", "N", "Nzz", { silent = true })
 
 -- Comfortable buffer deletion
-vim.keymap.set("n", "<leader>x", vim.cmd.bdelete)
-vim.keymap.set("n", "<leader>X", function () vim.cmd.bdelete { bang = true } end)
+vim.keymap.set("n", "<leader>x", vim.cmd.bdelete, { silent = true })
+vim.keymap.set("n", "<leader>X", function () vim.cmd.bdelete { bang = true } end, { silent = true })
 
 -- Esc can be appended functions in other places such as config files of plugins
 Escs = {}
