a271bdd28925cd85335fb045d2efb0c41ba4149f
kjuq <44694396+kjuq@users.noreply.github.com>
2023-10-02 18:23:09 +0900

    add: more treesitter functions and which-key

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -13,10 +13,10 @@ return {
             incremental_selection = {
                 enable = true,
                 keymaps = {
-                    init_selection = '<M-v>',
-                    node_incremental = '<M-v>',
-                    scope_incremental = '<M-s>',
-                    node_decremental = '<M-d>',
+                    init_selection = "<M-v>",
+                    node_incremental = "<M-v>",
+                    scope_incremental = "<M-s>",
+                    node_decremental = "<M-d>",
                 },
             },
             textobjects = {
@@ -25,48 +25,48 @@ return {
                     lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
                     keymaps = {
                         -- You can use the capture groups defined in textobjects.scm
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
+                        ["aa"] = "@parameter.outer",
+                        ["ia"] = "@parameter.inner",
+                        ["af"] = "@function.outer",
+                        ["if"] = "@function.inner",
+                        ["ac"] = "@class.outer",
+                        ["ic"] = "@class.inner",
                     },
                 },
                 move = {
                     enable = true,
                     set_jumps = true, -- whether to set jumps in the jumplist
                     goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
+                        ["]m"] = "@function.outer",
+                        ["]]"] = "@class.outer",
                     },
                     goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
+                        ["]M"] = "@function.outer",
+                        ["]["] = "@class.outer",
                     },
                     goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
+                        ["[m"] = "@function.outer",
+                        ["[["] = "@class.outer",
                     },
                     goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
+                        ["[M"] = "@function.outer",
+                        ["[]"] = "@class.outer",
                     },
                 },
                 swap = {
                     enable = true,
                     swap_next = {
-                        ['<leader>a'] = '@parameter.inner',
+                        ["<M-t>"] = "@parameter.inner",
                     },
                     swap_previous = {
-                        ['<leader>A'] = '@parameter.inner',
+                        ["<M-S-t>"] = "@parameter.inner",
                     },
                 },
             },
         }
     end,
     dependencies = {
-        'nvim-treesitter/nvim-treesitter-textobjects',
+        "nvim-treesitter/nvim-treesitter-textobjects",
     },
 }
 
