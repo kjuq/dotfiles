61adfa87555c818dfd04f2f7998ab15ebb224047
kjuq <44694396+kjuq@users.noreply.github.com>
2023-10-02 18:48:48 +0900

    add: none-ls

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -23,11 +23,37 @@ return {
         })
         require("mason-lspconfig").setup({})
         require("mason").setup({})
+
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+        require("null-ls").setup({
+            -- you can reuse a shared lspconfig on_attach callback here
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
+        })
+
+        require('mason-null-ls').setup({
+            automatic_setup = true,
+            handlers = {},
+        })
     end,
     opts = {},
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
         "neovim/nvim-lspconfig",
+        "nvimtools/none-ls.nvim",
+        "jay-babu/mason-null-ls.nvim",
     }
 }
 
