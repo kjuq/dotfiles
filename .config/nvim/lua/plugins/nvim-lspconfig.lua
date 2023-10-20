cff73285fd020219aeef10394793435c33bdd8f2
kjuq <44694396+kjuq@users.noreply.github.com>
2023-10-20 15:29:47 +0900

    add: lsp formatting on save even it's not provided by none-ls

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -10,6 +10,14 @@ return {
         vim.api.nvim_create_autocmd("LspAttach", {
             group = vim.api.nvim_create_augroup("UserLspConfig", {}),
             callback = function(ev)
+                vim.api.nvim_create_augroup("AutoFormatting", {})
+                vim.api.nvim_create_autocmd("BufWritePre", {
+                    pattern = "*",
+                    group = "AutoFormatting",
+                    callback = function()
+                        vim.lsp.buf.format({ async = false })
+                    end,
+                })
 
                 -- Enable completion triggered by <c-x><c-o>
                 vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
