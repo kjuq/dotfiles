38f479f0d6acef1a85e4bf36a44d38f84d9721dd
kjuq <44694396+kjuq@users.noreply.github.com>
2023-10-24 02:39:35 +0900

    add: finder keybinds in hammerspoon

--- .config/hammerspoon/init.lua
+++ .config/hammerspoon/init.lua
@@ -1,7 +1,9 @@
 -- Execute the command below when setting up a new environment
 -- defaults write org.hammerspoon.Hammerspoon MJConfigFile "$XDG_CONFIG_HOME/hammerspoon/init.lua"
 
-function toggleApp(appName, key)
+hs.ipc.cliInstall("/opt/homebrew")
+
+local function toggleApp(appName, key)
 	hs.hotkey.bind({}, key, function()
 		local app = hs.application.get(appName)
 		if app == nil then
@@ -14,5 +16,53 @@ function toggleApp(appName, key)
 	end)
 end
 
-toggleApp("WezTerm", "f17")
-toggleApp("Vivaldi", "f18")
+local function hideAllApps()
+	local appnames = { "WezTerm", "Vivaldi", "KeePassXC" }
+	for _, appname in pairs(appnames) do
+		local app = hs.application.get(appname)
+		app:hide()
+	end
+end
+
+hs.hotkey.bind({}, "f16", hideAllApps)
+toggleApp("Vivaldi", "f17")
+toggleApp("WezTerm", "f18")
+toggleApp("KeePassXC", "f19")
+
+-- Mouse button remaps
+
+local middleButton = 2
+local backButton = 3
+local forwardButton = 4
+
+local finderMouseEvent = hs.eventtap.new({ hs.eventtap.event.types.otherMouseDown }, function(e)
+	-- print(hs.eventtap.event.properties['mouseEventButtonNumber'])
+	local mouseButton = e:getProperty(hs.eventtap.event.properties['mouseEventButtonNumber'])
+	if mouseButton == middleButton then
+		hs.eventtap.event.newKeyEvent("cmd", "delete", true):post()
+		return true
+	end
+	if mouseButton == backButton then
+		hs.eventtap.event.newKeyEvent("cmd", "[", true):post()
+		return true
+	end
+	if mouseButton == forwardButton then
+		hs.eventtap.event.newKeyEvent("cmd", "]", true):post()
+		return true
+	end
+end)
+
+-- App-specific remaps
+
+local function handleGlobalEvent(name, event, _) -- name, event, APP
+	if event == hs.application.watcher.activated then
+		if name == "Finder" then
+			finderMouseEvent:start()
+		else
+			finderMouseEvent:stop()
+		end
+	end
+end
+
+local watcher = hs.application.watcher.new(handleGlobalEvent)
+watcher:start()
