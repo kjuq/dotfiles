f860ff674c11ff98486b00302dd2beb02e120ddd
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-02 20:22:17 +0900

    add: copilot.lua
    a

---
+++ .config/nvim/lua/plugins/copilot.lua
@@ -0,0 +1,30 @@
+return {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "VeryLazy",
+    opts = function()
+        local cmp = require("cmp")
+        cmp.event:on("menu_opened", function()
+            vim.b.copilot_suggestion_hidden = true
+        end)
+
+        cmp.event:on("menu_closed", function()
+            vim.b.copilot_suggestion_hidden = false
+        end)
+
+        return {
+            suggestion = {
+                enabled = true,
+                auto_trigger = false,
+                keymap = {
+                    accept = "<C-e>",
+                    accept_word = "<M-f>",
+                    accept_line = false,
+                    next = "<C-f>",
+                    prev = "<C-b>",
+                    dismiss = "<C-c>",
+                },
+            },
+        }
+    end,
+}
--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -13,7 +13,7 @@ return {
         "saadparwaiz1/cmp_luasnip",
         "L3MON4D3/LuaSnip",
     },
-    config = function ()
+    config = function()
         local has_words_before = function()
             unpack = unpack or table.unpack
             local line, col = unpack(vim.api.nvim_win_get_cursor(0))
@@ -97,8 +97,8 @@ return {
             },
             formatting = {
                 format = require("lspkind").cmp_format({
-                    mode = "text_symbol", -- show only symbol annotations
-                    maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                    mode = "text_symbol",  -- show only symbol annotations
+                    maxwidth = 50,         -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
                     ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
                 })
             },
@@ -135,6 +135,3 @@ return {
         })
     end,
 }
-
-
-
