89149947b805d6138e30d623896651d07bbc5438
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-04 16:26:45 +0900

    mod: refactor keymap generator

--- .config/nvim/lua/utils/lazy.lua
+++ .config/nvim/lua/utils/lazy.lua
@@ -1,11 +1,24 @@
-return {
-    generate_map = function(prefix, desc_source)
-        return function(suffix, mode, func, comment)
-            local key = prefix .. suffix
-            local func = func
-            local desc = desc_source .. comment
+local generate_map = function(key_prefix, desc_prefix)
+    local map = function(suffix, mode, func, comment)
+        local key = key_prefix .. suffix
+        local desc = desc_prefix .. comment
 
-            return { key, mode = mode, func, desc = desc }
-        end
+        return { key, mode = mode, func, desc = desc }
     end
+
+    return map
+end
+
+local generate_cmd_map = function(key_prefix, desc_prefix)
+    local map = generate_map(key_prefix, desc_prefix)
+    local cmap = function(suffix, mode, cmd, comment)
+        return map(suffix, mode, function() vim.cmd(cmd) end, comment)
+    end
+
+    return cmap
+end
+
+return {
+    generate_map = generate_map,
+    generate_cmd_map = generate_cmd_map,
 }
