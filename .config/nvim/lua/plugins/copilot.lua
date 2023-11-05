aaf4f7ca9bf3e76f1f8db9ee6aa44d13baac6450
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-05 17:52:59 +0900

    mod: enable copilot ghost text even if cmp-copilot is used

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -1,8 +1,26 @@
 return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
-    event = { "BufNewFile", "BufReadPost" },
+    event = { "InsertEnter" },
     opts = function()
+        local opts = {
+            suggestion = {
+                enabled = true,
+                auto_trigger = false,
+                keymap = {
+                    accept = false,
+                    accept_word = false,
+                    accept_line = false,
+                    next = "<M-i>",
+                    prev = false,
+                    dismiss = false,
+                },
+            },
+            panel = {
+                enabled = false,
+            },
+        }
+
         local success, cmp = pcall(require, "cmp")
         if success then
             cmp.event:on("menu_opened", function()
@@ -14,22 +32,34 @@ return {
             end)
         end
 
-        return {
-            suggestion = {
-                enabled = true,
-                auto_trigger = false,
-                keymap = {
-                    accept = "<C-e>",
-                    accept_word = "<M-f>",
-                    accept_line = false,
-                    next = "<C-f>",
-                    prev = "<C-b>",
-                    dismiss = "<C-c>",
-                },
-            },
-            panel = {
-                enabled = false,
-            },
-        }
+        local mod = require("copilot.suggestion")
+        --- @param key string
+        --- @param normal_behavior function
+        --- @param choosing_behavior function
+        local map = function(key, normal_behavior, choosing_behavior)
+            vim.keymap.set("i", key, function()
+                if not mod.is_visible() then
+                    normal_behavior()
+                else
+                    choosing_behavior()
+                end
+            end)
+        end
+
+        map("<C-f>", mod.next, mod.accept)
+        map("<C-l>", function() vim.cmd.fclose { bang = true } end, mod.dismiss)
+
+        if not opts.suggestion.auto_trigger then
+            map("<M-f>", function() mod.next() end, function()
+                mod.accept_word()
+                mod.next()
+            end)
+            map("<M-n>", function() mod.next() end, function()
+                mod.accept_line()
+                mod.next()
+            end)
+        end
+
+        return opts
     end,
 }
