d00acdc595453f14f3f2bcaae3d7dddd3d89996f
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-05 23:03:25 +0900

    add: aerial.nvim

---
+++ .config/nvim/lua/plugins/aerial.lua
@@ -0,0 +1,19 @@
+local map = require("utils.lazy").generate_cmd_map("<leader>", "Aerial: ")
+
+return {
+    "stevearc/aerial.nvim",
+    keys = {
+        map("lo", "n", "AerialToggle", "Toggle")
+    },
+    opts = {
+        backends = { "lsp", "treesitter", "markdown", "man" },
+        keymaps = {
+            ["<Esc>"] = "actions.close",
+        },
+        show_guides = true,
+    },
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-tree/nvim-web-devicons"
+    },
+}
--- .config/nvim/lua/utils/lazy.lua
+++ .config/nvim/lua/utils/lazy.lua
@@ -1,4 +1,10 @@
+--- @param key_prefix string
+--- @param desc_prefix string
 local generate_map = function(key_prefix, desc_prefix)
+    --- @param suffix string
+    --- @param mode string|table
+    --- @param func function
+    --- @param comment string
     local map = function(suffix, mode, func, comment)
         local key = key_prefix .. suffix
         local desc = desc_prefix .. comment
@@ -9,8 +15,14 @@ local generate_map = function(key_prefix, desc_prefix)
     return map
 end
 
+--- @param key_prefix string
+--- @param desc_prefix string
 local generate_cmd_map = function(key_prefix, desc_prefix)
     local map = generate_map(key_prefix, desc_prefix)
+    --- @param suffix string
+    --- @param mode string|table
+    --- @param cmd string
+    --- @param comment string
     local cmap = function(suffix, mode, cmd, comment)
         return map(suffix, mode, function() vim.cmd(cmd) end, comment)
     end
