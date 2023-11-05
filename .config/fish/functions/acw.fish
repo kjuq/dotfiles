0fa6cd4c83554cbfbd5ae1bf466a46047ba694e1
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-05 23:04:27 +0900

    fix: acw command's `<=` and `>=`

--- .config/fish/functions/acw.fish
+++ .config/fish/functions/acw.fish
@@ -3,8 +3,8 @@ function acw --description='Usage: acw abc290 a'
         echo "Not enough arguments. Usage: acw abc290 a"
         return 1
     end
-    set --local sed_str          "s/\\\le/<=/g;"
-    set --local sed_str $sed_str "s/\\\ge/>=/g;"
+    set --local sed_str          "s/\\\leq/<=/g;"
+    set --local sed_str $sed_str "s/\\\geq/>=/g;"
     set --local sed_str $sed_str "s/\\\dots/.../g;"
     set --local sed_str $sed_str "s/\\\ldots/.../g;"
     w3m "https://atcoder.jp/contests/$argv[1]/tasks/$argv[1]_$argv[2]" | sed "$sed_str" | less
