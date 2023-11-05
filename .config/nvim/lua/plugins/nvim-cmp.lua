4fd57c0477be8c8ae2cb36b6d0f2eecf072fc84e
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-05 16:37:04 +0900

    add: snippy (moved from luasnip)

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -2,20 +2,13 @@ return {
     "hrsh7th/nvim-cmp",
     event = { "InsertEnter", "CmdlineEnter" },
     config = function()
-        local has_words_before = function()
-            unpack = unpack or table.unpack
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-        end
-
         local cmp = require("cmp")
         local types = require("cmp.types")
-        local luasnip = require("luasnip")
 
         cmp.setup({
             snippet = {
                 expand = function(args)
-                    luasnip.lsp_expand(args.body)
+                    require("snippy").expand_snippet(args.body)
                 end,
             },
             completion = {
@@ -29,7 +22,7 @@ return {
                     else
                         cmp.complete()
                     end
-                end, { "i", "c" }),
+                end, { "i" }),
 
                 ["<C-p>"] = cmp.mapping(function()
                     if cmp.visible() then
@@ -37,35 +30,17 @@ return {
                     else
                         cmp.complete()
                     end
-                end, { "i", "c" }),
+                end, { "i" }),
 
                 ["<C-u>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-d>"] = cmp.mapping.scroll_docs(4),
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    if luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
-                    if luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                    elseif has_words_before() then
-                        cmp.complete()
-                    else
-                        fallback()
-                    end
-                end, { "i", "s" }),
             }),
             sources = {
                 { name = "nvim_lsp" },
                 { name = "buffer" },
                 { name = "path" },
                 { name = "nvim_lsp_signature_help" },
-                { name = "luasnip" },
+                { name = "snippy" },
                 { name = "nvim_lua" },
                 { name = "emoji" },
                 { name = "copilot" },
@@ -121,13 +96,11 @@ return {
         "hrsh7th/cmp-cmdline",
         "hrsh7th/cmp-nvim-lua",
         "hrsh7th/cmp-emoji",
+        "hrsh7th/cmp-nvim-lsp-signature-help",
         "ray-x/cmp-treesitter",
         "onsails/lspkind.nvim",
-        "saadparwaiz1/cmp_luasnip",
-        "L3MON4D3/LuaSnip",
-        {
-            "zbirenbaum/copilot-cmp",
-            opts = {},
-        },
+        "dcampos/cmp-snippy",
+        { "zbirenbaum/copilot-cmp", opts = {}, },
+        { "petertriho/cmp-git",     dependencies = "nvim-lua/plenary.nvim" }
     },
 }
