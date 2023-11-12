78f1f5e2bbd10ea42d1a8c46ea9fa6be194a59c4
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-12 21:42:18 +0900

    add: load habamax if colorscheme is missing

---
+++ .config/nvim/lua/colorscheme.lua
@@ -0,0 +1,4 @@
+vim.cmd.colorscheme("habamax")
+vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#808080" })
+vim.api.nvim_set_hl(0, "Normal", { bg = "NONE", ctermbg = "NONE" })
+vim.api.nvim_set_hl(0, "statusline", { bg = "NONE" })
