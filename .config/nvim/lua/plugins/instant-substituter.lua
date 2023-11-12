b4d19ea0dd08857c3ad04b7e6d48a3c170f867ef
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-12 17:09:44 +0900

    del: unnecessary keybinds in instant-substituter.nvim

--- .config/nvim/lua/plugins/instant-substituter.lua
+++ .config/nvim/lua/plugins/instant-substituter.lua
@@ -2,16 +2,11 @@ return {
     "kjuq/instant-substituter.nvim",
     -- dir = "~/codes/instant-substituter.nvim",
     keys = {
-        { "gz",        mode = { "n", "v" } },
-        { "<C-l>",     mode = { "n", "v" } },
-        { "<leader>o", mode = { "n", "v" } },
+        { "gz", mode = { "n", "v" } },
     },
     opts = {
         keys = {
             ["gz"] = { [[']], [["]] },
-            ["<C-l>"] = { [[-]], [[+]] },
-            ["<leader>o"] = { "var", "let" },
         },
-        -- debug = true,
     },
 }
