d39d738694c2051e4d3e79a8ed9e63bfb300da78
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-12 21:42:51 +0900

    add: highlight on yank

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -2,14 +2,21 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     pattern = "*.snippets",
     callback = function()
         vim.opt_local.filetype = "snippets"
-    end
+    end,
 })
 
 vim.api.nvim_create_autocmd({ "CmdwinEnter" }, {
     pattern = "*",
     callback = function()
         vim.keymap.set("n", "<Esc>", "<Cmd>quit<CR>", { buffer = true })
-    end
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+    pattern = "*",
+    callback = function()
+        require("vim.highlight").on_yank({ timeout = 125 })
+    end,
 })
 
 require("utils.common").quit_with_esc({ "qf" })
