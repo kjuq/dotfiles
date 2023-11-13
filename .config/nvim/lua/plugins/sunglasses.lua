93468fbb78b42a8f8b3c62c5f0d2cc3c3b3fbaaf
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-14 02:11:10 +0900

    mod: augroup

--- .config/nvim/lua/plugins/sunglasses.lua
+++ .config/nvim/lua/plugins/sunglasses.lua
@@ -2,13 +2,17 @@ return {
     "miversen33/sunglasses.nvim",
     event = { "BufNewFile", "BufReadPost", "FocusLost", "WinNew" },
     config = function()
+        local unfocused = vim.api.nvim_create_augroup("sunglasses_unfocuesd", {})
         vim.api.nvim_create_autocmd({ "FocusLost" }, {
+            group = unfocused,
             callback = function()
                 vim.cmd("SunglassesOn")
                 vim.cmd("SunglassesRefresh")
             end,
         })
+        local focused = vim.api.nvim_create_augroup("sunglasses_focused", {})
         vim.api.nvim_create_autocmd({ "FocusGained" }, {
+            group = focused,
             callback = function()
                 vim.cmd("SunglassesOff")
                 vim.cmd("SunglassesRefresh")
