9662e7be4b199229af3831f18779a66aedf8b5c6
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-14 01:52:09 +0900

    fix: highlight on yank in VISUAL mode

--- .config/nvim/lua/plugins/nvim-surround.lua
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -2,9 +2,12 @@ return {
     "kylechui/nvim-surround",
     version = "*", -- Use for stability; omit to use `main` branch for the latest features
     keys = {
-        { "ys", mode = { "n", "x" } },
-        { "ds", mode = { "n", "x" } },
-        { "cs", mode = { "n", "x" } },
+        { "ys", mode = { "n" } },
+        { "ds", mode = { "n" } },
+        { "cs", mode = { "n" } },
+        { "y",  mode = { "x" } },
+        { "d",  mode = { "x" } },
+        { "c",  mode = { "x" } },
     },
     opts = {},
     dependencies = {
