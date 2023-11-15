1b8f9cfc2187e8b3a0269d78c4bbcff86017d9e8
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-16 02:21:33 +0900

    mod: refactor nvim-lspconfig.lua

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,73 +1,59 @@
+local map = require("utils.lazy").generate_map("", "LSP: ")
+local vlb = vim.lsp.buf
+
+local vt = false -- if virtual text of diagnostics is on
+
 return {
     "neovim/nvim-lspconfig",
+    event = { "LspAttach" },
+    keys = {
+        -- map("gd", "n", vlb.definition, "Go to definition")
+
+        -- vlb.declaration, "Go to Declaration"
+        -- vlb.type_definition, "Go to type definition"
+        -- vlb.references, "Go to reference"
+        -- vlb.implementation, "Go to implementation"
+
+        -- map("<KEYBIND>", "n", vlb.code_action, "Code action")
+
+        map("<leader>rn", "n", vlb.rename, "Rename"),
+
+        map("K", "n", vlb.hover, "Hover Documentation"),
+        map("<C-s>", { "n", "v", "i" }, vlb.signature_help, "Signature Help"),
+
+        map("<leader>twa", "n", vlb.add_workspace_folder, "Workspace Add folder"),
+        map("<leader>twr", "n", vlb.remove_workspace_folder, "Workspace remove Folder"),
+        map("<leader>twl", "n", function() print(vim.inspect(vlb.list_workspace_folders())) end, "Workspace list Folders"),
+        map("<leader>tf", "n", function() vlb.format { async = false } end, "Format current buffer"),
+
+        map("<leader>tv", "n", function()
+            vt = not vt
+            vim.diagnostic.config({ virtual_text = vt, })
+        end, "Toggle virtual text of diagnotics"),
+
+        map("<leader>e", "n", vim.diagnostic.open_float, "Open floating diagnostic message"),
+        map("[e", "n", vim.diagnostic.goto_prev, "Go to previous diagnostic message"),
+        map("]e", "n", vim.diagnostic.goto_next, "Go to next diagnostic message"),
+        map("<leader>tq", "n", vim.diagnostic.setloclist, "Open diagnostics list"),
+    },
     config = function()
-        local map = function(mode, keys, func, description, buffer)
-            if description then
-                description = "LSP: " .. description
-            end
+        vim.opt.omnifunc = "v:lua.vim.lsp.omnifunc" -- Enable completion triggered by <c-x><c-o>
 
-            local opts = {}
-            if not buffer == nil then
-                opts.buffer = buffer
-            end
-            opts.desc = description
-
-            vim.keymap.set(mode, keys, func, opts)
-        end
-        local vt_enabled = false
         vim.diagnostic.config({
             signs = false,
-            virtual_text = vt_enabled,
+            virtual_text = vt,
             float = { border = "rounded" },
         })
-        require('lspconfig.ui.windows').default_options.border = "rounded"
 
-        vim.api.nvim_create_autocmd("LspAttach", {
-            group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-            callback = function(ev)
-                vim.api.nvim_create_augroup("AutoFormatting", {})
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    pattern = "*",
-                    group = "AutoFormatting",
-                    callback = function()
-                        vim.lsp.buf.format({ async = false })
-                    end,
-                })
+        require("lspconfig.ui.windows").default_options.border = "rounded"
 
-                vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc" -- Enable completion triggered by <c-x><c-o>
-
-                local vlb = vim.lsp.buf
-
-                -- map("n", "gd", vlb.definition, "Go to definition", ev.buf)
-
-                -- vlb.declaration, "Go to Declaration"
-                -- vlb.type_definition, "Go to type definition"
-                -- vlb.references, "Go to reference"
-                -- vlb.implementation, "Go to implementation"
-
-                -- map("n", "<KEYBIND>", vlb.code_action, "code [a]ction", ev.buf)
-
-                map("n", "<leader>rn", vlb.rename, "Rename", ev.buf)
-
-                map("n", "K", vlb.hover, "Hover Documentation", ev.buf)
-                map({ "n", "v", "i" }, "<C-s>", vlb.signature_help, "Signature Documentation", ev.buf)
-
-                map("n", "<leader>twa", vlb.add_workspace_folder, "Workspace Add folder", ev.buf)
-                map("n", "<leader>twr", vlb.remove_workspace_folder, "Workspace remove Folder", ev.buf)
-                map("n", "<leader>twl", function() print(vim.inspect(vlb.list_workspace_folders())) end,
-                    "Workspace list Folders", ev.buf)
-
-                map("n", "<leader>tf", function() vlb.format { async = false } end, "Format current buffer", ev.buf)
-
-                map("n", "<leader>tv", function()
-                    vt_enabled = not vt_enabled
-                    vim.diagnostic.config({ virtual_text = vt_enabled, })
-                end, "Toggle virtual text of diagnostics", ev.buf)
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*",
+            group = vim.api.nvim_create_augroup("AutoFormatting", {}),
+            callback = function()
+                vim.lsp.buf.format({ async = false })
+                vim.cmd("normal zz")
             end,
         })
-        map("n", "<leader>e", vim.diagnostic.open_float, "Open floating diagnostic message")
-        map("n", "[e", vim.diagnostic.goto_prev, "Go to previous diagnostic message")
-        map("n", "]e", vim.diagnostic.goto_next, "Go to next diagnostic message")
-        map("n", "<leader>tq", vim.diagnostic.setloclist, "Open diagnostics list")
     end,
 }
