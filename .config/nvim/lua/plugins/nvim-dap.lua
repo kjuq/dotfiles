8ca7d37e1a5b04677ce39ca164d08394ced39b90
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-16 02:26:56 +0900

    mod: refactor nvim-dap.lua

--- .config/nvim/lua/plugins/nvim-dap.lua
+++ .config/nvim/lua/plugins/nvim-dap.lua
@@ -1,25 +1,25 @@
 local map = require("utils.lazy").generate_map("<leader>d", "Nvim-dap: ")
-local mod = require("dap")
+local dap = require("dap")
 
 return {
     "mfussenegger/nvim-dap",
     keys = {
-        map("B", "n", function() mod.set_breakpoint(vim.fn.input("BP condition: ")) end, "Breakpoint condition"),
-        map("b", "n", function() mod.toggle_breakpoint() end, "Breakpoint"),
-        map("c", "n", function() mod.continue() end, "Continue"),
-        map("a", "n", function() mod.continue({ before = get_args }) end, "Run with args"),
-        map("C", "n", function() mod.run_to_cursor() end, "Run to cursor"),
-        map("g", "n", function() mod.goto_() end, "Go to line (no execute)"),
-        map("i", "n", function() mod.step_into() end, "Step into"),
-        map("j", "n", function() mod.down() end, "Down"),
-        map("k", "n", function() mod.up() end, "Up"),
-        map("l", "n", function() mod.run_last() end, "Run last"),
-        map("o", "n", function() mod.step_out() end, "Step out"),
-        map("O", "n", function() mod.step_over() end, "Step over"),
-        map("p", "n", function() mod.pause() end, "Pause"),
-        map("r", "n", function() mod.repl.toggle() end, "Toggle REPL"),
-        map("s", "n", function() mod.session() end, "Session"),
-        map("t", "n", function() mod.terminate() end, "Terminate"),
+        map("B", "n", function() dap.set_breakpoint(vim.fn.input("BP condition: ")) end, "Breakpoint condition"),
+        map("b", "n", function() dap.toggle_breakpoint() end, "Breakpoint"),
+        map("c", "n", function() dap.continue() end, "Continue"),
+        map("a", "n", function() dap.continue({ before = get_args }) end, "Run with args"),
+        map("C", "n", function() dap.run_to_cursor() end, "Run to cursor"),
+        map("g", "n", function() dap.goto_() end, "Go to line (no execute)"),
+        map("i", "n", function() dap.step_into() end, "Step into"),
+        map("j", "n", function() dap.down() end, "Down"),
+        map("k", "n", function() dap.up() end, "Up"),
+        map("l", "n", function() dap.run_last() end, "Run last"),
+        map("o", "n", function() dap.step_out() end, "Step out"),
+        map("O", "n", function() dap.step_over() end, "Step over"),
+        map("p", "n", function() dap.pause() end, "Pause"),
+        map("r", "n", function() dap.repl.toggle() end, "Toggle REPL"),
+        map("s", "n", function() dap.session() end, "Session"),
+        map("t", "n", function() dap.terminate() end, "Terminate"),
         map("w", "n", function() require("dap.ui.widgets").hover() end, "Widgets"),
     },
     cmd = {
@@ -52,16 +52,10 @@ return {
                 { "<leader>dd", function() require("dapui").toggle() end, desc = "Nvim-dap-ui: Toggle" },
             },
             config = function()
-                local dap, dapui = require("dap"), require("dapui")
-                dap.listeners.after.event_initialized["dapui_config"] = function()
-                    dapui.open()
-                end
-                dap.listeners.before.event_terminated["dapui_config"] = function()
-                    dapui.close()
-                end
-                dap.listeners.before.event_exited["dapui_config"] = function()
-                    dapui.close()
-                end
+                local dapui = require("dapui")
+                dap.listeners.after.event_initialized["dapui_config"] = dapui.open
+                dap.listeners.before.event_terminated["dapui_config"] = dapui.close
+                dap.listeners.before.event_exited["dapui_config"] = dapui.close
                 require("dapui").setup()
             end,
         },
@@ -70,7 +64,7 @@ return {
             opts = {
                 ---@diagnostic disable-next-line: unused-local
                 display_callback = function(variable, buf, stackframe, node, options)
-                    if options.virt_text_pos == 'inline' then
+                    if options.virt_text_pos == "inline" then
                         return " = " .. variable.value
                     else
                         return "  " .. variable.name .. " = " .. variable.value
