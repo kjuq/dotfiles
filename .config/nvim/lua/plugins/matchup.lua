dbfd6cface890a42ece4dc1348a50bd8e4076b5d
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-19 04:23:13 +0900

    add: lazily loaded vim-matchup

--- .config/nvim/lua/plugins/matchup.lua
+++ .config/nvim/lua/plugins/matchup.lua
@@ -1,17 +1,17 @@
 return {
     "andymass/vim-matchup",
-    event = { "BufNewFile", "BufReadPost" },
-    config = function ()
+    event = { "VeryLazy" },
+    config = function()
         require("nvim-treesitter.configs").setup({
             matchup = {
                 enable = true,
             },
         })
+        -- for lazy-load, reload filetype
+        vim.bo.filetype = vim.bo.filetype
     end,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
 }
-
-
