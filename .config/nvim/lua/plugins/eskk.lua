af002a980a3c6ff80f01660e6f0403e324a744d2
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-19 18:20:21 +0900

    add: eskk.vim as a fallback of skkeleton

---
+++ .config/nvim/lua/plugins/eskk.lua
@@ -0,0 +1,56 @@
+local skk = require("utils.skk")
+
+local opts = { enabled = false }
+local jp_toggle = vim.api.nvim_create_augroup("EskkToggle", {})
+
+local toggle_japanese = function()
+    skk.toggle_japanese(opts, jp_toggle, function()
+        require("utils.common").feed_plug("(eskk:enable)")
+    end)
+end
+
+return {
+    "vim-skk/eskk.vim",
+    cond = vim.fn.executable("deno") == 0, -- fellback from skkeleton
+    keys = {
+        { "<C-Space>",  mode = { "i", "c" }, "<Plug>(eskk:enable)", desc = "Eskk: Enable",              noremap = false, silent = true },
+        { "<leader>aj", mode = { "n" },      toggle_japanese,       desc = "Eskk: Toggle Japanese mode" },
+    },
+    config = function()
+        local jisyo_l = os.getenv("HOMEBREW_PREFIX") .. "/share/skk-jisyo-l/SKK-JISYO.L"
+        vim.g["eskk#directory"] = skk.skk_dir
+        vim.g["eskk#dictionary"] = { path = skk.skk_dir .. "/my_jisyo", sorted = 1, encoding = "utf-8" }
+        vim.g["eskk#large_dictionary"] = { path = jisyo_l, sorted = 1, encoding = "euc-jp" }
+        vim.g["eskk#egg_like_newline"] = 1
+        vim.g["eskk#keep_state"] = 0
+        vim.g["eskk#enable_completion"] = 0
+        vim.g["eskk#no_default_mappings"] = 1
+
+        vim.api.nvim_create_autocmd("user", {
+            pattern = "eskk-initialize-pre",
+            callback = function()
+                vim.cmd([[ let t = eskk#table#new("rom_to_hira*", "rom_to_hira") ]])
+                vim.cmd([[ call t.add_map("!", "!") ]])
+                vim.cmd([[ call t.add_map("?", "?") ]])
+                vim.cmd([[ call t.add_map(":", ":") ]])
+                vim.cmd([[ call t.add_map("z ", "　") ]])
+                vim.cmd([[ call eskk#register_mode_table("hira", t) ]])
+            end,
+        })
+
+        if pcall(require, "noice") then
+            vim.api.nvim_create_autocmd("user", {
+                pattern = "eskk-enable-pre",
+                callback = function()
+                    vim.cmd("Noice disable")
+                end,
+            })
+            vim.api.nvim_create_autocmd("user", {
+                pattern = "eskk-disable-post",
+                callback = function()
+                    vim.cmd("Noice enable")
+                end,
+            })
+        end
+    end,
+}
---
+++ .config/nvim/lua/utils/skk.lua
@@ -0,0 +1,23 @@
+---@return table { skk_dir = string, jisyo_l = string, toggle_japanese = fun(opts: table, group: integer, callback: fun()) }
+return {
+    skk_dir = os.getenv("XDG_CONFIG_HOME") .. "/skk",
+    jisyo_l = os.getenv("HOMEBREW_PREFIX") .. "/share/skk-jisyo-l/SKK-JISYO.L",
+
+    ---@param opts table { enabled = boolean }
+    ---@param group integer
+    ---@param callback fun()
+    toggle_japanese = function(opts, group, callback)
+        if opts.enabled then
+            print("Japanese mode disabled")
+            vim.api.nvim_clear_autocmds({ group = group })
+        else
+            print("Japanese mode enabled")
+            vim.api.nvim_create_autocmd("InsertEnter", {
+                group = group,
+                pattern = "*",
+                callback = callback,
+            })
+        end
+        opts.enabled = not opts.enabled
+    end
+}
