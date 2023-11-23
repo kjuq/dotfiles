adb3ef99735e70dc60ae6aaf70793c4b07ff6cd5
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-23 12:36:41 +0900

    add: oil.nvim

---
+++ .config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,22 @@
+local map = require("utils.lazy").generate_cmd_map("", "Oil: ")
+local hiddens = { "../", ".DS_Store", ".git", ".gitmodules", "node_modules" }
+
+return {
+    "stevearc/oil.nvim",
+    lazy = false,
+    keys = {
+        map("-", "n", "Oil", "Open parent directory"),
+    },
+    opts = {
+        delete_to_trash = true,
+        cleanup_delay_ms = 1000,
+        view_options = {
+            show_hidden = true,
+            is_always_hidden = function(name, bufnr)
+                print(name)
+                return vim.list_contains(hiddens, name)
+            end,
+        },
+    },
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+}
