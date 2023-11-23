56c5d7831de2401b031fc46bea4ba62882ebc2c1
kjuq <44694396+kjuq@users.noreply.github.com>
2023-11-23 17:59:25 +0900

    fix: sunglasses.nvim

--- .config/nvim/lua/plugins/sunglasses.lua
+++ .config/nvim/lua/plugins/sunglasses.lua
@@ -5,21 +5,23 @@ return {
         vim.api.nvim_create_autocmd({ "FocusLost" }, {
             group = vim.api.nvim_create_augroup("sunglasses_unfocuesd", {}),
             callback = function()
+                vim.cmd.fclose({ bang = true })
                 vim.cmd("SunglassesOn")
             end,
         })
         vim.api.nvim_create_autocmd({ "FocusGained" }, {
             group = vim.api.nvim_create_augroup("sunglasses_focused", {}),
             callback = function()
+                vim.cmd.fclose({ bang = true })
                 vim.cmd("SunglassesOff")
             end,
         })
 
         require("sunglasses").setup({
             filter_type = "SHADE",
-            filter_percent = .40
+            filter_percent = .40,
         })
 
-        vim.cmd("SunglassesRefresh")
+        vim.cmd("SunglassesRefresh") -- for lazy load
     end,
 }
