0e6f1891ecd5fe89cab57c4c31a70a0855c2486e
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-03 09:45:50 +0900

    mod: fish functions -> config.fish

--- .config/fish/functions/nvimcopy.fish
+++
@@ -1,4 +0,0 @@
-function nvimcopy --description="Open nvim for copying text"
-	set --local tmp "/tmp/clip_tmp_nae18aA6ARaiOF"
-	nvim -c "startinsert" "$tmp"; and [ -e "$tmp" ]; and head -c -1 "$tmp" | pbcopy; and command rm "$tmp"
-end
