42d82206e10406b3388076338b5a01b5dc0e14ed
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-04 09:24:56 +0900

    fix: chatGPT's output cuts off

--- .config/nvim/lua/plugins/chatgpt.lua
+++ .config/nvim/lua/plugins/chatgpt.lua
@@ -24,41 +24,21 @@ return {
         edit_with_instructions = {
             keymaps = {
                 close = "<Esc>",
-                -- accept = "<C-y>",
-                -- toggle_diff = "<C-d>",
-                -- toggle_settings = "<C-o>",
-                -- cycle_windows = "<Tab>",
-                -- use_output_as_input = "<C-i>",
             },
         },
         chat = {
             keymaps = {
                 close = { "<Esc>" },
-                -- yank_last = "<C-y>",
-                -- yank_last_code = "<C-k>",
-                -- scroll_up = "<C-u>",
                 scroll_down = "<C-d>",
-                -- new_session = "<C-n>",
-                -- cycle_windows = "<Tab>",
-                -- cycle_modes = "<C-f>",
-                -- next_message = "<C-j>",
-                -- prev_message = "<C-k>",
-                -- select_session = "<Space>",
-                -- rename_session = "r",
-                -- delete_session = "d",
                 draft_message = "<M-d>",
-                -- edit_message = "e",
-                -- delete_message = "d",
-                -- toggle_settings = "<C-o>",
-                -- toggle_message_role = "<C-r>",
-                -- toggle_system_role_open = "<C-s>",
-                -- stop_generating = "<C-x>",
             },
         },
         popup_input = {
             submit = "<Enter>",
-            -- submit_n = "<Enter>",
-            -- max_visible_lines = 20,
+        },
+        openai_params = {
+            -- model = "gpt-3.5-turbo",
+            max_tokens = 3000, -- https://github.com/jackMort/ChatGPT.nvim/issues/302
         },
     },
     dependencies = {
