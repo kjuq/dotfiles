4a5abe5757bc52fd185be064aef69ad920d2c760
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-12 23:15:01 +0900

    add: floating nvim to input Japanese in Linux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ local wezterm = require("wezterm")
 -- help provide clearer error messages
 local config
 if wezterm.config_builder then
-    config = wezterm.config_builder() -- Start in capital letter to declare global var
+	config = wezterm.config_builder() -- Start in capital letter to declare global var
 end
 
 -- Startup with fullscreen
@@ -17,65 +17,73 @@ end
 
 -- This table will hold the configuration.
 config = {
-    -- color_scheme = "Catppuccin Mocha",
-    -- color_scheme = "Clone Of Ubuntu (Gogh)",
-    -- color_scheme = "Gruvbox Dark (Gogh)",
-    -- color_scheme = "GruvboxDarkHard",
-    color_scheme = "Tokyo Night",
-    -- color_scheme = "iceberg-dark",
+	-- color_scheme = "Catppuccin Mocha",
+	-- color_scheme = "Clone Of Ubuntu (Gogh)",
+	-- color_scheme = "Gruvbox Dark (Gogh)",
+	-- color_scheme = "GruvboxDarkHard",
+	color_scheme = "Tokyo Night",
+	-- color_scheme = "iceberg-dark",
 
-    window_background_opacity = 0.65,
-    font_size = 20,
-    -- font = wezterm.font("Hack Nerd Font"),
-    font = wezterm.font_with_fallback {
-        "Hack Nerd Font",
-        "Hiragino Sans",
-    },
-    freetype_load_target = "Normal", -- "Normal", "Light", "Mono"
-    default_prog = {
-        "/opt/homebrew/bin/fish",
-        -- "--login",
-        -- "--command",
-        -- "tmux attach; or tmux",
-    },
-    enable_tab_bar = false,
-    window_decorations = "RESIZE",
-    disable_default_key_bindings = true,
-    send_composed_key_when_left_alt_is_pressed = false,
-    send_composed_key_when_right_alt_is_pressed = false,
-    keys = {
-        {
-            key = "c",
-            mods = "CMD",
-            action = wezterm.action({ CopyTo = "Clipboard" }),
-        },
-        {
-            key = "v",
-            mods = "CMD",
-            action = wezterm.action({ PasteFrom = "Clipboard" }),
-        },
-        {
-            key = "f",
-            mods = "CMD|CTRL",
-            action = wezterm.action.ToggleFullScreen,
-        },
-        {
-            key = "-",
-            mods = "CMD",
-            action = wezterm.action.DecreaseFontSize,
-        },
-        {
-            key = "=",
-            mods = "CMD|SHIFT",
-            action = wezterm.action.IncreaseFontSize,
-        },
-        {
-            key = "Backspace",
-            mods = "CMD",
-            action = wezterm.action.SendKey({ key = "u", mods = "CTRL" }),
-        },
-    },
+	default_prog = {
+		"/opt/homebrew/bin/fish",
+		-- "--login",
+		-- "--command",
+		-- "tmux attach; or tmux",
+	},
+
+	window_background_opacity = 0.65,
+	-- font = wezterm.font("Hack Nerd Font"),
+	font = wezterm.font_with_fallback {
+		"Hack Nerd Font",
+		"Hiragino Sans",
+	},
+	freetype_load_target = "Normal", -- "Normal", "Light", "Mono"
+
+	enable_tab_bar = false,
+	window_decorations = "RESIZE",
+	disable_default_key_bindings = true,
+	send_composed_key_when_left_alt_is_pressed = false,
+	send_composed_key_when_right_alt_is_pressed = false,
+	keys = {
+		{
+			key = "c",
+			mods = "CMD",
+			action = wezterm.action({ CopyTo = "Clipboard" }),
+		},
+		{
+			key = "v",
+			mods = "CMD",
+			action = wezterm.action({ PasteFrom = "Clipboard" }),
+		},
+		{
+			key = "f",
+			mods = "CMD|CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+		{
+			key = "-",
+			mods = "CMD",
+			action = wezterm.action.DecreaseFontSize,
+		},
+		{
+			key = "=",
+			mods = "CMD|SHIFT",
+			action = wezterm.action.IncreaseFontSize,
+		},
+		{
+			key = "Backspace",
+			mods = "CMD",
+			action = wezterm.action.SendKey({ key = "u", mods = "CTRL" }),
+		},
+	},
 }
+
+-- check os
+if os.execute("[ $(uname) = 'Darwin' ]") then
+	config.font_size = 20
+elseif os.execute("[ $(uname) = 'Linux' ]") then
+	config.font_size = 16
+end
 
 -- and finally, return the configuration to wezterm
 return config
--- scripts/floatingnvim.sh
+++ scripts/floatingnvim.sh
@@ -2,7 +2,8 @@
 
 tmp="/tmp/floatingnvim_tmp_nae18aA6ARaiOF"
 
-wezterm --config initial_rows=10 --config initial_cols=70 start --class FloatingVim nvim -c "startinsert" "$tmp"\
-    && [ -e "$tmp" ] \
-    && head -c -1 "$tmp" | pbcopy \
-    && command rm "$tmp"
+wezterm --config initial_rows=10 --config initial_cols=70 start --class FloatingVim fish -c \
+    "nvim -c 'startinsert' '$tmp' \
+    && [ -e '$tmp' ] \
+    && head -c -1 '$tmp' | pbcopy \
+    && command rm '$tmp'"
