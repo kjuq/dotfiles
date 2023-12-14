92b00f4c2e7eb4861cebcc632c909fb26c7a7903
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-14 16:39:20 +0900

    mod: use nvimcopy in floatingnvim.sh

--- scripts/floatingnvim.sh
+++ scripts/floatingnvim.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-tmp="/tmp/floatingnvim_tmp_nae18aA6ARaiOF"
-
-wezterm --config initial_rows=10 --config initial_cols=70 start --class FloatingVim fish -c \
-    "nvim -c 'startinsert' '$tmp' \
-    && [ -e '$tmp' ] \
-    && head -c -1 '$tmp' | pbcopy \
-    && command rm '$tmp'"
+wezterm --config initial_rows=10 --config initial_cols=70 start --class FloatingVim fish -c nvimcopy
