c58d5ab2c335f99272636a23ebe0a9858262e826
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-14 21:36:25 +0900

    mov: scripts/* -> .local/bin/*

--- scripts/browserpass-init.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-brew install amar1729/formulae/browserpass
-PREFIX='/opt/homebrew/opt/browserpass' make hosts-vivaldi-user -f '/opt/homebrew/opt/browserpass/lib/browserpass/Makefile'
-echo "pinentry-program /opt/homebrew/bin/pinentry-mac" # >> "$XDG_CONFIG_HOME/gnupg/gpg-agent.conf"
--- scripts/floatingnvim.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-wezterm --config initial_rows=10 --config initial_cols=70 start --class FloatingVim fish -c nvimcopy
