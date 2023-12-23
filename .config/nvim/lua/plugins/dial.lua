0662d15536e24a9a7205076d2a6755a3ec8ca586
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-24 05:27:39 +0900

    add: dial.nvim

---
+++ .config/nvim/lua/plugins/dial.lua
@@ -0,0 +1,16 @@
+local map = require("utils.lazy").generate_map("", "Dial: ")
+local dm = "dial.map"
+
+return {
+    "monaqa/dial.nvim",
+    keys = {
+        map("<C-a>", "n", function() require(dm).manipulate("increment", "normal") end, "Increment"),
+        map("<C-x>", "n", function() require(dm).manipulate("decrement", "gnormal") end, "Decrement"),
+        map("g<C-a>", "n", function() require(dm).manipulate("increment", "normal") end, "G Increment"),
+        map("g<C-x>", "n", function() require(dm).manipulate("decrement", "gnormal") end, "G Decrement"),
+        map("<C-a>", "v", function() require(dm).manipulate("increment", "visual") end, "Increment"),
+        map("<C-x>", "v", function() require(dm).manipulate("decrement", "gvisual") end, "Decrement"),
+        map("g<C-a>", "v", function() require(dm).manipulate("increment", "visual") end, "G Increment"),
+        map("g<C-x>", "v", function() require(dm).manipulate("decrement", "gvisual") end, "G Decrement"),
+    },
+}
