7b6dd3ee6730416bbf4782a69c6a9ff832900a2a
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-24 06:03:53 +0900

    mod: re-enable alpha.nvim

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,5 +1,6 @@
 return {
     "goolord/alpha-nvim",
+    cond = vim.fn.argc() == 0, -- if nvim was launched without files specified by args
     event = { "BufWinEnter" },
     config = function()
         require("alpha").setup(require("alpha.themes.theta").config)
