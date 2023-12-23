a45bdbe8fd6020e81d5124bfdea80838bb11e0dd
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-24 04:27:44 +0900

    mod: minor

--- .config/nvim/lua/plugins/qfreplace.lua
+++ .config/nvim/lua/plugins/qfreplace.lua
@@ -2,6 +2,6 @@ return {
     "thinca/vim-qfreplace",
     cmd = "Qfreplace",
     keys = {
-        { "<leader>aq", mode = "n", function() vim.cmd("Qfreplace") end, { desc = "Qfreplace" } },
+        { "<leader>aq", mode = "n", function() vim.cmd("Qfreplace") end, { desc = "Qfreplace: Open" } },
     },
 }
