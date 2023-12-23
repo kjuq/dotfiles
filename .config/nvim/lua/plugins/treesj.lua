c422ba0e858bc4485ff74c7e4de5e6e814a85db8
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-23 20:59:46 +0900

    add: treesj for nvim

---
+++ .config/nvim/lua/plugins/treesj.lua
@@ -0,0 +1,14 @@
+local map = require("utils.lazy").generate_map("", "TreeSJ: ")
+
+return {
+    "Wansmer/treesj",
+    keys = {
+        map("gS", "n", function() require("treesj").toggle() end, "Toggle")
+    },
+    config = function()
+        require("treesj").setup({
+            use_default_keymaps = false,
+        })
+    end,
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+}
