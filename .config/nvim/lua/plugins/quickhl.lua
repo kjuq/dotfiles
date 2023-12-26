40d09f5da862b399b193e03f9e1a71a70cfc3f0f
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-27 07:15:27 +0900

    add: vim-quickhl

---
+++ .config/nvim/lua/plugins/quickhl.lua
@@ -0,0 +1,14 @@
+local map = require("utils.lazy").generate_map("<leader>a", "QuickHL: ")
+
+return {
+    "t9md/vim-quickhl",
+    lazy = false,
+    keys = {
+        map("m", { "n", "x" }, function()
+            require("utils.common").feed_plug("(quickhl-manual-this)")
+        end, "Mark"),
+        map("M", { "n", "x" }, function()
+            require("utils.common").feed_plug("(quickhl-manual-reset)")
+        end, "Reset")
+    }
+}
