fba12a134f4f08c8ba27b012e7ade9be7fe3ba54
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-27 04:15:10 +0900

    mod: cosmetic

--- .config/nvim/lua/plugins/linediff.lua
+++ .config/nvim/lua/plugins/linediff.lua
@@ -2,9 +2,7 @@ local map = require("utils.lazy").generate_map("<leader>a", "Linediff: ")
 
 return {
     "AndrewRadev/linediff.vim",
-    cmd = {
-        "Linediff",
-    },
+    cmd = { "Linediff" },
     keys = {
         map("d", "x", function()
             vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "x", false)
