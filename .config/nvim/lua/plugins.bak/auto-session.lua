24f2145654f5c5aec26169b927d4e9ef22be4c61
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-27 07:06:17 +0900

    add: auto-session, bqf, and replacer (but I felt they are not good)

---
+++ .config/nvim/lua/plugins.bak/auto-session.lua
@@ -0,0 +1,11 @@
+return {
+    "rmagatti/auto-session",
+    lazy = false,
+    opts = function()
+        return {
+            log_level = "error",
+            auto_session_enable_last_session = true,
+            auto_session_suppress_dirs = { "/", "~/" },
+        }
+    end
+}
---
+++ .config/nvim/lua/plugins.bak/replacer.lua
@@ -0,0 +1,8 @@
+local map = require("utils.lazy").generate_map("<leader>a", "Replacer: ")
+
+return {
+    "gabrielpoca/replacer.nvim",
+    keys = {
+        map("q", "n", function() require("replacer").run() end, "Run")
+    },
+}
