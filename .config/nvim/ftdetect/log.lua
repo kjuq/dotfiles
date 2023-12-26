44c628b7b1bd0642594c742ba23d439f2f1fa064
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-27 06:26:45 +0900

    mod: minor

--- .config/nvim/ftdetect/log.lua
+++ .config/nvim/ftdetect/log.lua
@@ -1,5 +1,5 @@
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = {"*.log", "*_log", ".LOG", "_LOG"},
+    pattern = { "*.log", "*_log", ".LOG", "_LOG" },
     group = vim.api.nvim_create_augroup("user_ft_log", {}),
     callback = function()
         vim.opt_local.filetype = "log"
