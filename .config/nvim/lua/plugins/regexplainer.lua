97342deda8372e10dda9ebdc55b8d4b4a254805f
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-27 05:10:23 +0900

    add: regexplainer

---
+++ .config/nvim/lua/plugins/regexplainer.lua
@@ -0,0 +1,22 @@
+return {
+    "bennypowers/nvim-regexplainer",
+    event = "VeryLazy",
+    config = function()
+        require("regexplainer").setup({
+            auto = true,
+            mappings = {
+                toggle = "", -- disable gR which is the default keymap
+            },
+            popup = {
+                border = {
+                    padding = { 0, 1 },
+                    style = "rounded",
+                }
+            }
+        })
+    end,
+    dependencies = {
+        "nvim-treesitter/nvim-treesitter",
+        "MunifTanjim/nui.nvim",
+    },
+}
