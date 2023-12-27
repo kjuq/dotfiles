9d157c61505210239496f00f3a217e0767719bd0
kjuq <44694396+kjuq@users.noreply.github.com>
2023-12-27 09:40:01 +0900

    mod: tuned vim-log-highlighting

--- .config/nvim/ftdetect/log.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = { "*.log", "*_log", ".LOG", "_LOG" },
-    group = vim.api.nvim_create_augroup("user_ft_log", {}),
-    callback = function()
-        vim.opt_local.filetype = "log"
-    end,
-})
