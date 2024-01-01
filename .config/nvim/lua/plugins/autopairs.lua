b44df11d18dd7b60483ab4f9e8570256585c36be
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-02 04:02:56 +0900

    mod: tune autopairs.nvim up

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -1,5 +1,10 @@
 return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = {},
+    opts = {
+        map_cr = false,
+        map_c_h = true,
+        map_c_w = true,
+        check_ts = true, -- treesitter
+    },
 }
