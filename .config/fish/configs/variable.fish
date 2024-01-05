808f9a52b57f12632b62ac63a975d1c4187a7e5c
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-05 13:13:03 +0900

    mod: nvim as a PAGER

--- .config/fish/configs/variable.fish
+++ .config/fish/configs/variable.fish
@@ -14,7 +14,7 @@ set --export LANGUAGE en_US.UTF-8
 
 set --export EDITOR nvim
 set --export VISUAL nvim
-set --export PAGER less
+set --export PAGER "nvim -R +'set norelativenumber' +'set nonumber'"
 set --export MANPAGER "nvim +Man!"
 
 set --export SUDO_ASKPASS "$LOCAL_BIN_PATH/sudo_pass"
