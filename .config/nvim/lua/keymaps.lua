c07a9ca0e75a67399b6d3cf46ff7ad67adfd8dfb
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-07 07:50:16 +0900

    add: keymaps for builtin functions (gh, gl, gs, and etc)

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -33,13 +33,13 @@ map("n", "s", function()
     return vim.v.count == 0 and "\"_s" or "s"
 end, { expr = true, silent = true, desc = "Delete N characters and start insert" })
 
--- map("n", "S", "\"_S", { silent = true, desc = "Delete N lines and start insert" })
--- map("n", "c", "\"_c", { silent = true, desc = "Delete Nmove text and start insert" })
--- map("n", "C", "\"_C", { silent = true, desc = "Delete Nmove text and start insert" })
--- map("n", "d", "\"_d", { silent = true, desc = "Delete Nmove text" })
--- map("n", "D", "\"_D", { silent = true, desc = "Delete until the end of the line" })
--- map("x", "p", "P", { silent = true, desc = "Paste; registers are unchanged" })
--- map("x", "P", "p", { silent = true, desc = "Paste; deleted text in unnamed register" })
+-- map("n", "S", '"_S', { silent = true, desc = "Delete N lines and start insert" })
+-- map("n", "c", '"_c', { silent = true, desc = "Delete Nmove text and start insert" })
+-- map("n", "C", '"_C', { silent = true, desc = "Delete Nmove text and start insert" })
+-- map("n", "d", '"_d', { silent = true, desc = "Delete Nmove text" })
+-- map("n", "D", '"_D', { silent = true, desc = "Delete until the end of the line" })
+map("x", "p", "P", { silent = true, desc = "Paste; registers are unchanged" })
+map("x", "P", "p", { silent = true, desc = "Paste; deleted text in unnamed register" })
 
 -- Move caret on display lines
 -- Comfortable line specify movement by v:count
@@ -57,10 +57,27 @@ map("n", "]l", vim.cmd.cnext, { desc = "Next location on QuickFix" })
 map("n", "[l", vim.cmd.cprevious, { desc = "Previous location on QuickFix" })
 
 -- Frequently used keymaps
-map("n", "<leader>w", vim.cmd.write, { silent = true, desc = "Write" })
-map("n", "<leader>W", function() vim.cmd("noautocmd w") end, { silent = true, desc = "Write w/o format" })
-map("n", "<leader>q", vim.cmd.quit, { silent = true, desc = "Quit" })
-map("n", "<leader>Q", vim.cmd.quitall, { silent = true, desc = "Quit all" })
+local write = function(key)
+    map("n", key, vim.cmd.write, { silent = true, desc = "Write" })
+end
+local write_noautocmd = function(key)
+    map("n", key, function() vim.cmd("noautocmd w") end, { silent = true, desc = "Write w/o autocmd" })
+end
+local quit = function(key)
+    map("n", key, vim.cmd.quit, { silent = true, desc = "Quit" })
+end
+local quit_all = function(key)
+    map("n", key, vim.cmd.quitall, { silent = true, desc = "Quit all" })
+end
+write("gs")
+write("<leader>w")
+write_noautocmd("gS")
+write_noautocmd("<leader>W")
+quit("gh")
+quit("<leader>q")
+quit_all("gH")
+quit_all("<leader>Q")
+
 map("n", "<C-q>", "<C-w><C-w>", { desc = "Switch window" })
 
 -- Center cursor when searching
@@ -68,14 +85,22 @@ map("n", "n", "nzz", { silent = true, desc = "Repeat the latest search" })
 map("n", "N", "Nzz", { silent = true, desc = "Repeat the latest search in opposite direction" })
 
 -- Comfortable buffer deletion
-map("n", "<leader>x", vim.cmd.bdelete, { silent = true, desc = "Delete the current buffer" })
-map("n", "<leader>X", function() vim.cmd.bdelete { bang = true } end, {
-    silent = true,
-    desc = "Force delete a current buffer"
-})
+local bdelete = function(key)
+    map("n", key, vim.cmd.bdelete, { silent = true, desc = "Delete the current buffer" })
+end
+local bdelete_force = function(key)
+    map("n", key, function() vim.cmd.bdelete { bang = true } end, {
+        silent = true,
+        desc = "Force delete a current buffer"
+    })
+end
+bdelete("gl")
+bdelete("<leader>x")
+bdelete_force("gL")
+bdelete_force("<leader>X")
 
 map("n", "gW", function() vim.opt.wrap = not vim.o.wrap end, { desc = "Toggle wrap" })
-map("n", "gL", function() vim.cmd("messages") end, { desc = "History of messages" })
+map("n", "gA", function() vim.cmd("messages") end, { desc = "History of messages" })
 map("n", "gC", function() -- number [C]olumn (?)
     local opt = vim.opt
     local o = vim.o
--- .config/nvim/lua/plugins/bufdelete.lua
+++ .config/nvim/lua/plugins/bufdelete.lua
@@ -1,10 +1,12 @@
-local map = require("utils.lazy").generate_map("<leader>", "Bufdelete: ")
+local map = require("utils.lazy").generate_map("", "Bufdelete: ")
 
 return {
     "famiu/bufdelete.nvim",
     cmd = { "Bdelete", "Bwipeout" },
     keys = {
-        map("x", "n", function() require("bufdelete").bufdelete(0, false) end, "Delete buffer"),
-        map("X", "n", function() require("bufdelete").bufdelete(0, true) end, "Delete buffer forcibly"),
+        map("gl", "n", function() require("bufdelete").bufdelete(0, false) end, "Delete buffer"),
+        map("<leader>x", "n", function() require("bufdelete").bufdelete(0, false) end, "Delete buffer"),
+        map("gL", "n", function() require("bufdelete").bufdelete(0, true) end, "Delete buffer forcibly"),
+        map("<leader>X", "n", function() require("bufdelete").bufdelete(0, true) end, "Delete buffer forcibly"),
     },
 }
