572d5b9d1e373c30944e11a08596abaf53c2e234
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-06 13:47:25 +0900

    mod: nvim's utils/

--- .config/nvim/lua/utils/skeleton.lua
+++ .config/nvim/lua/utils/skeleton.lua
@@ -3,11 +3,13 @@ if not success then
     conf_dir = os.getenv("HOME") .. "/.config"
 end
 
+local common = require("utils.common")
+
 return {
     skel_path = conf_dir .. "/nvim/ftplugin/skeleton/",
     paste_skeleton = function(filename)
         -- if vim.fn.filereadable(vim.fn.bufname()) == 0 and vim.fn.line("$") == 1 and vim.fn.getline(1) == "" then
-        if require("utils.common").is_empty_buffer() then
+        if (not common.is_current_file_exists()) and common.is_empty_buffer() then
             vim.cmd.read({
                 args = { require("utils.skeleton").skel_path .. filename },
                 mods = { silent = true }
