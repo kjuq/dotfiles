c894f851f5109f3fe0fb4a1a82f184a00aaa1194
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-17 22:24:23 +0900

    mod: minor

--- .config/nvim/lua/plugins/himalaya.lua
+++ .config/nvim/lua/plugins/himalaya.lua
@@ -3,9 +3,7 @@ local map = require("utils.lazy").generate_cmd_map("<leader>a", "Himalaya: ")
 return {
     "https://git.sr.ht/~soywod/himalaya-vim",
     commit = "64fb17067cf5dbf5349726b9ed1b1b38065cdb82",
-    cmd = {
-        "Himalaya",
-    },
+    cmd = "Himalaya",
     keys = {
         map("h", "n", "Himalaya", "Open"),
     },
