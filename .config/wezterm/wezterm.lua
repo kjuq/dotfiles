d6a541707cbdd5c6e601e00ca0fad55fdf493027
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-17 22:49:05 +0900

    mod: refactor wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,88 +1,63 @@
--- Pull in the wezterm API
-local wezterm = require("wezterm")
+local wez = require("wezterm")
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
-local config
-if wezterm.config_builder then
-	config = wezterm.config_builder() -- Start in capital letter to declare global var
+local C -- config
+if wez.config_builder then
+	C = wez.config_builder()
 end
 
 -- Startup with fullscreen
--- wezterm.on('gui-startup', function(cmd)
---     local _, _, window = wezterm.mux.spawn_window(cmd or {})
+-- wez.on("gui-startup", function(cmd)
+--     local _, _, window = wez.mux.spawn_window(cmd or {})
 --     -- window:gui_window():maximize()
 --     window:gui_window():toggle_fullscreen()
 -- end)
 
--- This table will hold the configuration.
-config = {
-	-- color_scheme = "Catppuccin Mocha",
-	-- color_scheme = "Clone Of Ubuntu (Gogh)",
-	-- color_scheme = "Gruvbox Dark (Gogh)",
-	-- color_scheme = "GruvboxDarkHard",
-	color_scheme = "Tokyo Night",
-	-- color_scheme = "iceberg-dark",
+C.color_scheme = "GruvboxDarkHard" -- "Catppuccin Mocha", "Tokyo Night", "iceberg-dark"
 
-	-- prefer_egl = false,
-	max_fps = 250,
-	front_end = "WebGpu",
-	webgpu_power_preference = "HighPerformance",
+-- C.prefer_egl = false
+C.max_fps = 250
+C.front_end = "WebGpu"
+C.webgpu_power_preference = "HighPerformance"
 
-	window_background_opacity = 0.65,
-	-- font = wezterm.font("Hack Nerd Font"),
-	font = wezterm.font_with_fallback {
-		"Hack Nerd Font",
-		"Hiragino Sans",
-		"SourceHanSansJP-Normal", -- for linux, install `adobe-source-han-sans-jp-fonts`
-	},
-	freetype_load_target = "Normal", -- "Normal", "Light", "Mono"
+C.font = wez.font_with_fallback {
+	"Hack Nerd Font",
+	"Hiragino Sans",
+	"SourceHanSansJP-Normal",     -- for linux, install `adobe-source-han-sans-jp-fonts`
+}
+C.freetype_load_target = "Normal" -- "Normal", "Light", "Mono"
 
-	enable_tab_bar = false,
-	window_decorations = "RESIZE",
-	disable_default_key_bindings = true,
-	send_composed_key_when_left_alt_is_pressed = false,
-	send_composed_key_when_right_alt_is_pressed = false,
-	keys = {
-		{
-			key = "c",
-			mods = "CMD",
-			action = wezterm.action({ CopyTo = "Clipboard" }),
-		},
-		{
-			key = "v",
-			mods = "CMD",
-			action = wezterm.action({ PasteFrom = "Clipboard" }),
-		},
-		{
-			key = "f",
-			mods = "CMD|CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-		{
-			key = "-",
-			mods = "CMD",
-			action = wezterm.action.DecreaseFontSize,
-		},
-		{
-			key = "=",
-			mods = "CMD|SHIFT",
-			action = wezterm.action.IncreaseFontSize,
-		},
-		{
-			key = "Backspace",
-			mods = "CMD",
-			action = wezterm.action.SendKey({ key = "u", mods = "CTRL" }),
-		},
-	},
+C.window_background_opacity = 0.65
+
+C.enable_tab_bar = false
+C.window_decorations = "RESIZE"
+
+C.disable_default_key_bindings = true
+C.send_composed_key_when_left_alt_is_pressed = false
+C.send_composed_key_when_right_alt_is_pressed = false
+
+local key = function(key, mods, action)
+	local table = {}
+	table.key = key
+	table.mods = mods
+	table.action = action
+	return table
+end
+
+C.keys = {
+	key("c", "CMD", wez.action({ CopyTo = "Clipboard" })),
+	key("v", "CMD", wez.action({ PasteFrom = "Clipboard" })),
+	key("f", "CMD|CTRL", wez.action.ToggleFullScreen),
+	key("-", "CMD", wez.action.DecreaseFontSize),
+	key("=", "CMD|SHIFT", wez.action.IncreaseFontSize),
+	key("Backspace", "CMD", wez.action.SendKey({ key = "u", mods = "CTRL" })),
 }
 
 -- check os
 if os.execute("[ $(uname) = 'Darwin' ]") then
-	config.font_size = 20
+	C.font_size = 19
 elseif os.execute("[ $(uname) = 'Linux' ]") then
-	config.font_size = 16
+	C.font_size = 16
 end
 
 -- and finally, return the configuration to wezterm
-return config
+return C
