645de2c6d1d6028eb2519a35c84339f9acb8d023
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-18 10:40:58 +0900

    fix: gx.nvim

--- .config/nvim/lua/plugins/gx.lua
+++ .config/nvim/lua/plugins/gx.lua
@@ -1,6 +1,10 @@
 return {
     "chrishrb/gx.nvim",
-    event = require("utils.lazy").verylazy,
+    keys = { { "gx", "<cmd>Browse<cr>", mode = { "n", "x" } } },
+    cmd = { "Browse" },
+    init = function()
+        vim.g.netrw_nogx = 1 -- disable netrw gx
+    end,
     opts = {},
     dependencies = { "nvim-lua/plenary.nvim" },
 }
