4a9b4c0f866e5bd1a2e1fd652c3e9243de43aa87
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-23 14:58:33 +0900

    mod: alacritty -> wezterm in Linux

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -50,13 +50,14 @@ C.keys = {
 	key("-", "CMD", wez.action.DecreaseFontSize),
 	key("=", "CMD|SHIFT", wez.action.IncreaseFontSize),
 	key("Backspace", "CMD", wez.action.SendKey({ key = "u", mods = "CTRL" })),
+	key("Backspace", "CTRL", wez.action.SendKey({ key = "w", mods = "CTRL" })),
 }
 
 -- check os
 if os.execute("[ $(uname) = 'Darwin' ]") then
 	C.font_size = 19
 elseif os.execute("[ $(uname) = 'Linux' ]") then
-	C.font_size = 16
+	C.font_size = 13
 end
 
 -- and finally, return the configuration to wezterm
