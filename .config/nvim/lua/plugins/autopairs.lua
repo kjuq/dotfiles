487574cbddf7dca4c153afaaf0b66ff033c19910
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-26 04:04:00 +0900

    mod: minor (nvim-autopairs)

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -5,6 +5,11 @@ return {
         map_cr = false,
         map_c_h = true,
         map_c_w = true,
+        break_undo = false,
         check_ts = true, -- treesitter
+        fast_wrap = {
+            map = "<C-g>e",
+            keys = "arstneioghwfpluy",
+        },
     },
 }
