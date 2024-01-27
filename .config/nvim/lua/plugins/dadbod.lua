a38439df33d172920c88759b58169e297913d3d9
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-28 05:23:07 +0900

    mod: dadbod shows notifications via vim's native message

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -14,6 +14,7 @@ return {
     init = function()
         vim.g.db_ui_use_nerd_fonts = 1
         vim.g.db_ui_save_location = os.getenv("XDG_CONFIG_HOME") .. "/dadbod"
+        vim.g.db_ui_force_echo_notifications = 1
     end,
     dependencies = {
         {
