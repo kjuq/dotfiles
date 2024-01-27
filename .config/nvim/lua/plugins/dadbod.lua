bf94a8673581c76cd56cb2575128e1a301350a37
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-28 03:27:06 +0900

    add: vim-dadbod

---
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -0,0 +1,35 @@
+local map = require("utils.lazy").generate_cmd_map("<leader>a", "Dadbod: ")
+
+return {
+    "kristijanhusak/vim-dadbod-ui",
+    cmd = {
+        "DBUI",
+        "DBUIToggle",
+        "DBUIAddConnection",
+        "DBUIFindBuffer",
+    },
+    keys = {
+        map("d", "n", "DBUIToggle", "Toggle")
+    },
+    init = function()
+        vim.g.db_ui_use_nerd_fonts = 1
+        vim.g.db_ui_save_location = os.getenv("XDG_CONFIG_HOME") .. "/dadbod"
+    end,
+    dependencies = {
+        {
+            "tpope/vim-dadbod",
+            cmd = "DB",
+        },
+        {
+            "kristijanhusak/vim-dadbod-completion",
+            config = function()
+                vim.api.nvim_create_autocmd({ "FileType" }, {
+                    pattern = { "sql", "mysql", "plsql" },
+                    callback = function()
+                        require("cmp").setup.buffer({ sources = { { name = "vim-dadbod-completion" } } })
+                    end
+                })
+            end,
+        },
+    },
+}
