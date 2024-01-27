d3a405e8c90f90bd32edb3091fb2080ca1bcde23
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-28 05:30:05 +0900

    mod: hide dadbod's help message

--- .config/nvim/lua/plugins/dadbod.lua
+++ .config/nvim/lua/plugins/dadbod.lua
@@ -15,6 +15,7 @@ return {
         vim.g.db_ui_use_nerd_fonts = 1
         vim.g.db_ui_save_location = os.getenv("XDG_CONFIG_HOME") .. "/dadbod"
         vim.g.db_ui_force_echo_notifications = 1
+        vim.g.db_ui_show_help = 0
     end,
     dependencies = {
         {
