2f84c7ffbf1ab2029946ef1d4585c66e099bde2f
kjuq <44694396+kjuq@users.noreply.github.com>
2024-01-31 19:10:04 +0900

    mod: use Nvim-bqf as a substitution of Vim-qfedit

--- .config/nvim/lua/plugins/bqf.lua
+++ .config/nvim/lua/plugins/bqf.lua
@@ -1,29 +1,47 @@
 return {
     "kevinhwang91/nvim-bqf",
     ft = "qf",
-    opts = {
-        preview = {
-            winblend = 0,
-        },
-        func_map = {
-            openc = "o",
-            drop = "O",
-            tab = "",
-            tabb = "",
-            tabc = "",
-            split = "",
-            vsplit = "<C-s>",
-
-            pscrollup = require("utils.lazy").floatscrollup,
-            pscrolldown = require("utils.lazy").floatscrolldown,
-            pscrollorig = "zo",
-            ptogglemode = "zp",
-            ptoggleitem = "",
-            ptoggleauto = "dp",
-            filter = "zn",
-            filterr = "zN",
-        },
-    },
+    opts = function()
+        local utils = require("utils.lazy")
+        local scrolldown = utils.floatscrolldown
+        local scrollup = utils.floatscrollup
+        return {
+            preview = {
+                auto_preview = false,
+                winblend = 0,
+            },
+            func_map = {
+                open = "<CR>",            -- <CR>
+                openc = "o",              -- "o"
+                drop = "<Nop>",           -- "O" (I have no idea what this is)
+                tabdrop = "<Nop>",        -- ""
+                tab = "<Nop>",            -- "t"
+                tabb = "<Nop>",           -- "T"
+                tabc = "<Nop>",           -- "<C-t>"
+                split = "<Nop>",          -- "<C-x>"
+                vsplit = "<C-s>",         -- "<C-v>"
+                prevfile = "<C-p>",       -- "<C-p>"
+                nextfile = "<C-n>",       -- "<C-n>"
+                prevhist = "<",           -- "<"
+                nexthist = ">",           -- ">"
+                lastleave = "'\"",        -- "'\""
+                stoggleup = "X",          -- "<S-Tab>" (Substitution for Qfedit)
+                stoggledown = "x",        -- "<Tab>" (Substitution for Qfedit)
+                stogglevm = "x",          -- "<Tab>" (Substitution for Qfedit)
+                stogglebuf = "'x",        -- "'<Tab>" (Substitution for Qfedit)
+                sclear = "U",             -- "z<Tab>" (Substitution for Qfedit)
+                pscrollup = scrollup,     -- "<C-b>"
+                pscrolldown = scrolldown, -- "<C-f>"
+                pscrollorig = "zo",       -- "zo"
+                ptogglemode = "zp",       -- "zp"
+                ptoggleitem = "<Nop>",    -- "p"
+                ptoggleauto = "K",        -- "P"
+                filter = "D",             -- "zn" (Substitution for Qfedit)
+                filterr = "d",            -- "zN" (Substitution for Qfedit)
+                fzffilter = "<Nop>",      -- "zf" (fzf isn't installed)
+            },
+        }
+    end,
     dependencies = {
         "nvim-treesitter/nvim-treesitter",
     },
