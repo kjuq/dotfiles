24add7cbf4f380cc9a8ddf09380e858804ad9fbf
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-01 23:21:32 +0900

    mod: disable FocusLost and FocusGained in sunglasses.nvim

--- .config/nvim/lua/plugins/sunglasses.lua
+++ .config/nvim/lua/plugins/sunglasses.lua
@@ -2,18 +2,18 @@ return {
     "miversen33/sunglasses.nvim",
     event = require("utils.lazy").verylazy,
     config = function()
-        vim.api.nvim_create_autocmd({ "FocusLost" }, {
-            group = vim.api.nvim_create_augroup("sunglasses_unfocuesd", {}),
-            callback = function()
-                vim.cmd("SunglassesOn")
-            end,
-        })
-        vim.api.nvim_create_autocmd({ "FocusGained" }, {
-            group = vim.api.nvim_create_augroup("sunglasses_focused", {}),
-            callback = function()
-                vim.cmd("SunglassesOff")
-            end,
-        })
+        -- vim.api.nvim_create_autocmd({ "FocusLost" }, {
+        --     group = vim.api.nvim_create_augroup("sunglasses_unfocuesd", {}),
+        --     callback = function()
+        --         vim.cmd("SunglassesOn")
+        --     end,
+        -- })
+        -- vim.api.nvim_create_autocmd({ "FocusGained" }, {
+        --     group = vim.api.nvim_create_augroup("sunglasses_focused", {}),
+        --     callback = function()
+        --         vim.cmd("SunglassesOff")
+        --     end,
+        -- })
 
         require("sunglasses").setup({
             filter_type = "SHADE",
