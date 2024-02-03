1352c4739bf3839413e3ca4a4be1e9eb6b2a1e0e
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-04 08:52:20 +0900

    mod: refactor

--- .config/nvim/lua/utils/skeleton.lua
+++ .config/nvim/lua/utils/skeleton.lua
@@ -5,19 +5,21 @@ end
 
 local common = require("utils.common")
 
-return {
-    skel_path = conf_dir .. "/nvim/ftplugin/skeleton/",
-    paste_skeleton = function(filename)
-        -- if vim.fn.filereadable(vim.fn.bufname()) == 0 and vim.fn.line("$") == 1 and vim.fn.getline(1) == "" then
-        if (not common.is_current_file_exists()) and common.is_empty_buffer() then
-            vim.cmd.read({
-                args = { require("utils.skeleton").skel_path .. filename },
-                mods = { silent = true }
-            })
-            vim.cmd.delete({
-                range = { 1 },
-                mods = { silent = true }
-            })
-        end
+local M = {}
+
+M.skel_path = conf_dir .. "/nvim/ftplugin/skeleton/"
+M.paste_skeleton = function(filename)
+    -- if vim.fn.filereadable(vim.fn.bufname()) == 0 and vim.fn.line("$") == 1 and vim.fn.getline(1) == "" then
+    if (not common.is_current_file_exists()) and common.is_empty_buffer() then
+        vim.cmd.read({
+            args = { require("utils.skeleton").skel_path .. filename },
+            mods = { silent = true }
+        })
+        vim.cmd.delete({
+            range = { 1 },
+            mods = { silent = true }
+        })
     end
-}
+end
+
+return M
