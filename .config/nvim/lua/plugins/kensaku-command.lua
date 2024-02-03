e957e13b74c7f07089ea94a20523bf50028022df
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-04 07:56:15 +0900

    add: kensaku-search.vim

---
+++ .config/nvim/lua/plugins/kensaku-command.lua
@@ -0,0 +1,17 @@
+local map = require("utils.lazy").generate_map("", "Kensaku-search: ")
+
+---@type LazySpec
+local spec = {
+    "lambdalisue/kensaku-search.vim",
+    keys = {
+        map("<C-s>", "c", function()
+            require("utils.common").feed_plug("kensaku-search-replace")
+        end, "Confirm"),
+    },
+    dependencies = {
+        "vim-denops/denops.vim",
+        "lambdalisue/kensaku.vim",
+    },
+}
+
+return spec
