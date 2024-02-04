0236d8d39fe67ea769f405febc1736a2ad036eaa
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-05 06:52:59 +0900

    mod: keymaps to change vim's builtin options (wrap, number, and spell)

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -103,10 +103,10 @@ bdelete("<leader>x")
 bdelete_force("gL")
 bdelete_force("<leader>X")
 
-map("n", "gW", function() vim.opt.wrap = not vim.o.wrap end, { desc = "Toggle wrap" })
 map("n", "gA", function() vim.cmd("messages") end, { desc = "History of messages" })
-map("n", "gK", function() vim.opt.spell = not vim.o.spell end, { desc = "Toggle spell check" })
-map("n", "gC", function() -- number [C]olumn (?)
+map("n", "gaw", function() vim.opt.wrap = not vim.o.wrap end, { desc = "Toggle wrap" })
+map("n", "gas", function() vim.opt.spell = not vim.o.spell end, { desc = "Toggle spell check" })
+map("n", "gan", function()
 	local opt = vim.opt
 	local o = vim.o
 	if o.relativenumber then
