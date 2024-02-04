03ce2209844225b78c80b18028e37835995195a8
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-05 06:53:53 +0900

    add: function to pass other options via utils.lazy.generate_map.map

--- .config/nvim/lua/utils/lazy.lua
+++ .config/nvim/lua/utils/lazy.lua
@@ -5,13 +5,22 @@ local M = {}
 M.generate_map = function(key_prefix, desc_prefix)
 	--- @param suffix string
 	--- @param mode string|table
-	--- @param func function
+	--- @param func function|string
 	--- @param comment string
-	local map = function(suffix, mode, func, comment)
+	--- @param other_opts table?
+	local map = function(suffix, mode, func, comment, other_opts)
 		local key = key_prefix .. suffix
 		local desc = desc_prefix .. comment
 
-		return { key, mode = mode, func, desc = desc }
+		local result = { key, mode = mode, func, desc = desc }
+
+		if other_opts ~= nil then
+			for k, v in pairs(other_opts) do
+				result[k] = v
+			end
+		end
+
+		return result
 	end
 
 	return map
