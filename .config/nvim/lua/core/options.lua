a87dee687efb6c70a32c6cd4c7ee867b43981cfa
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-11 09:57:15 +0900

    mod: minor

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,35 +1,36 @@
-local opt          = vim.opt
+local opt         = vim.opt
 
-opt.backup         = true
-opt.writebackup    = true
-opt.backupdir      = os.getenv("XDG_STATE_HOME") .. "/nvim/backup//"
+opt.backup        = true
+opt.writebackup   = true
+opt.backupdir     = os.getenv("XDG_STATE_HOME") .. "/nvim/backup//"
 
-opt.swapfile       = false
-opt.autoread       = true
+opt.swapfile      = false
+opt.autoread      = true
 
 -- opt.scrolloff      = 6
 
-opt.clipboard      = "unnamedplus"
+opt.clipboard     = "unnamedplus"
 
-opt.cmdheight      = 0
-opt.laststatus     = 3 -- global status line
-opt.showcmdloc     = "statusline"
+opt.cmdheight     = 0
+opt.laststatus    = 3  -- global status line
+opt.showcmdloc    = "statusline"
 
-opt.termguicolors  = true
+opt.termguicolors = true
 
-opt.wildmenu       = true
-opt.completeopt    = "menu,menuone,preview,noinsert"
+opt.wildmenu      = true
+opt.completeopt   = "menu,menuone,preview,noinsert"
 
-opt.pumheight      = 10
-opt.pumwidth       = 40
+opt.pumheight     = 10
+opt.pumwidth      = 40
 -- o.pumblend = 20 -- Disable this when using transparent env
 
-opt.updatetime     = 200
+opt.updatetime    = 200
 
-opt.ignorecase     = true
-opt.smartcase      = true
+opt.ignorecase    = true
+opt.smartcase     = true
 
-opt.foldmethod     = "marker"
+opt.foldmethod    = "marker"
+opt.foldopen:remove("block")
 -- opt.foldtext       = "v:lua.vim.treesitter.foldtext()" -- laggy
 
 opt.wrap           = false
@@ -63,6 +64,8 @@ opt.fileencodings  = { "ucs-bom", "utf-8", "sjis", "euc-jp", "latin1" }
 -- Disable a leader key time out
 opt.timeout        = false
 opt.ttimeout       = false
+
+opt.iskeyword:remove("_")
 
 -- Disable welcome message
 opt.shortmess:append("I")
