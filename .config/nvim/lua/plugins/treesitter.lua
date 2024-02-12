2c8d610d62612ae2ef3534902a277c2289a31035
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-13 02:37:18 +0900

    fix: treesitter parser's installation

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,85 +1,86 @@
 ---@type LazySpec
 local spec = { "nvim-treesitter/nvim-treesitter" }
 spec.event = require("utils.lazy").verylazy
-
 spec.build = function()
-	require("nvim-treesitter.install").update({ with_sync = true })
+	require("nvim-treesitter.install").update({ with_sync = false, })()
 end
 
-spec.config = function()
-	require("nvim-treesitter.configs").setup({
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "regex", "markdown", "markdown_inline" },
-		sync_install = true,
-		auto_install = true,
+spec.opts = {
+	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "regex", "markdown", "markdown_inline", "bash" },
+	sync_install = false,
+	auto_install = false, -- don't turn this on, it causes error when `tree-sitter` CLI is not installed
 
-		highlight = {
-			enable = true,
-			disable = { "perl" },
+	highlight = {
+		enable = true,
+		disable = { "perl" },
+	},
+
+	indent = { enable = true },
+
+	incremental_selection = {
+		enable = true,
+		keymaps = {
+			init_selection = "<M-v>",
+			node_incremental = "<M-v>",
+			-- scope_incremental = "",
+			node_decremental = "<M-b>",
 		},
+	},
 
-		indent = { enable = true },
-
-		incremental_selection = {
+	textobjects = {
+		select = {
 			enable = true,
+			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
 			keymaps = {
-				init_selection = "<M-v>",
-				node_incremental = "<M-v>",
-				-- scope_incremental = "",
-				node_decremental = "<M-b>",
+				-- You can use the capture groups defined in textobjects.scm
+				["aa"] = "@parameter.outer",
+				["ia"] = "@parameter.inner",
+				["af"] = "@function.outer",
+				["if"] = "@function.inner",
+				["aC"] = "@class.outer",
+				["iC"] = "@class.inner",
+				["ai"] = "@conditional.outer", -- [i]f
+				["ii"] = "@conditional.inner",
+				["al"] = "@loop.outer",
+				["il"] = "@loop.inner",
+				["a/"] = "@comment.outer",
+				["i/"] = "@comment.inner",
+				["ag"] = "@call.outer", -- ar[g]uments
+				["ig"] = "@call.inner",
+				["ah"] = "@block.outer", -- [h]ere
+				["ih"] = "@block.inner",
 			},
 		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]f"] = "@function.outer",
+			},
+			goto_next_end = {
+				["]F"] = "@function.outer",
+			},
+			goto_previous_start = {
+				["[f"] = "@function.outer",
+			},
+			goto_previous_end = {
+				["[F"] = "@function.outer",
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<M-t>"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["g<M-t>"] = "@parameter.inner",
+			},
+		},
+	},
+}
 
-		textobjects = {
-			select = {
-				enable = true,
-				lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-				keymaps = {
-					-- You can use the capture groups defined in textobjects.scm
-					["aa"] = "@parameter.outer",
-					["ia"] = "@parameter.inner",
-					["af"] = "@function.outer",
-					["if"] = "@function.inner",
-					["aC"] = "@class.outer",
-					["iC"] = "@class.inner",
-					["ai"] = "@conditional.outer", -- [i]f
-					["ii"] = "@conditional.inner",
-					["al"] = "@loop.outer",
-					["il"] = "@loop.inner",
-					["a/"] = "@comment.outer",
-					["i/"] = "@comment.inner",
-					["ag"] = "@call.outer", -- ar[g]uments
-					["ig"] = "@call.inner",
-					["ah"] = "@block.outer", -- [h]ere
-					["ih"] = "@block.inner",
-				},
-			},
-			move = {
-				enable = true,
-				set_jumps = true, -- whether to set jumps in the jumplist
-				goto_next_start = {
-					["]f"] = "@function.outer",
-				},
-				goto_next_end = {
-					["]F"] = "@function.outer",
-				},
-				goto_previous_start = {
-					["[f"] = "@function.outer",
-				},
-				goto_previous_end = {
-					["[F"] = "@function.outer",
-				},
-			},
-			swap = {
-				enable = true,
-				swap_next = {
-					["<M-t>"] = "@parameter.inner",
-				},
-				swap_previous = {
-					["<M-S-t>"] = "@parameter.inner",
-				},
-			},
-		},
-	})
+spec.config = function()
+	require("nvim-treesitter.configs").setup(spec.opts)
 end
 
 spec.dependencies = {
