2b51df8e57f17d1063f7c9edd465bec7a6f6faff
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-15 02:08:41 +0900

    add: stylua

--- .config/nvim/lua/plugins/mason-null-ls.lua
+++ .config/nvim/lua/plugins/mason-null-ls.lua
@@ -2,15 +2,26 @@
 local spec = { "jay-babu/mason-null-ls.nvim" }
 spec.event = "LspAttach"
 
-spec.opts = {
-	ensure_installed = {
-		"autopep8",
-		"shellcheck",
-		"codelldb",
-		"debugpy",
-	},
-	automatic_installation = true,
-}
+spec.opts = function()
+	local null_ls = require("null-ls")
+
+	return {
+		automatic_installation = true,
+		ensure_installed = {
+			"autopep8",
+			"shellcheck",
+			"codelldb",
+			"debugpy",
+			"stylua",
+		},
+		handlers = {
+			function() end, -- disables automatic setup of all null-ls sources
+			stylua = function(_, _)
+				null_ls.register(null_ls.builtins.formatting.stylua)
+			end,
+		},
+	}
+end
 
 spec.dependencies = {
 	"williamboman/mason.nvim",
