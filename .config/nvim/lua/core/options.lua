e9dd8a2fb1c524e6caa466f0b145d8c78e06ca53
kjuq <44694396+kjuq@users.noreply.github.com>
2024-02-18 12:32:34 +0900

    add: configs for spellcheck

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -68,6 +68,11 @@ opt.iskeyword:remove("_")
 -- Disable welcome message
 opt.shortmess:append("I")
 
+opt.spell = false
+opt.spellfile = vim.fs.joinpath(os.getenv("XDG_STATE_HOME"), "nvim", "spell.en.utf-8.add")
+opt.spelllang = "en,cjk"
+opt.spelloptions = "camel"
+
 opt.matchpairs:append("「:」")
 opt.matchpairs:append("（:）")
 opt.matchpairs:append("［:］")
