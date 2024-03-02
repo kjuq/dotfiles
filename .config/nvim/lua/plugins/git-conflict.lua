9cab06ec25b79087ead27c81092c3aebcbd1856f
kjuq <44694396+kjuq@users.noreply.github.com>
2024-03-03 01:22:32 +0900

    mod: load git-conflict.nvim on BufReadPost

--- .config/nvim/lua/plugins/git-conflict.lua
+++ .config/nvim/lua/plugins/git-conflict.lua
@@ -3,20 +3,7 @@ local map = require("utils.lazy").generate_map("<leader>g", "Git-conflict: ")
 ---@type LazySpec
 local spec = { "akinsho/git-conflict.nvim" }
 spec.version = "*"
-
-spec.cmd = {
-	"GitConflictChooseOurs",
-	"GitConflictChooseTheirs",
-	"GitConflictChooseBoth",
-	"GitConflictChooseNone",
-	"GitConflictNextConflict",
-	"GitConflictPrevConflict",
-	"GitConflictListQf",
-}
-
-spec.keys = {
-	map("c", "n", "<cmd>GitConflictListQf<CR>", "List in Quickfix"),
-}
+spec.event = { "BufReadPost" }
 
 spec.opts = {}
 
