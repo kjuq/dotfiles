47e1d0ff1a0929c78465b19c34e0c060f95450d6
kjuq <44694396+kjuq@users.noreply.github.com>
2024-03-03 00:18:41 +0900

    add: yanky.nvim

---
+++ .config/nvim/lua/plugins/yanky.lua
@@ -0,0 +1,79 @@
+local map = require("utils.lazy").generate_map("", "Yanky: ")
+
+---@type LazySpec
+local spec = { "gbprod/yanky.nvim" }
+
+spec.keys = {
+	map("p", { "n", "x" }, "<Plug>(YankyPutAfter)", "Put after"),
+	map("P", { "n", "x" }, "<Plug>(YankyPutBefore)", "Put before"),
+	map("gp", { "n", "x" }, "<Plug>(YankyGPutAfter)", "GPut after"),
+	map("gP", { "n", "x" }, "<Plug>(YankyGPutBefore)", "GPut before"),
+
+	map("<C-p>", "n", function()
+		if require("yanky").can_cycle() then
+			require("utils.common").feed_plug("YankyPreviousEntry")
+		else
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-p>", true, false, true), "n", false)
+		end
+	end, "Previous entry"),
+	map("<C-n>", "n", function()
+		if require("yanky").can_cycle() then
+			require("utils.common").feed_plug("YankyNextEntry")
+		else
+			vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<C-n>", true, false, true), "n", false)
+		end
+	end, "Next entry"),
+
+	map("<leader>fp", "n", function()
+		local has_telescope, telescope = pcall(require, "telescope")
+		if has_telescope then
+			telescope.extensions.yank_history.yank_history()
+		end
+	end, "search with Telescope"),
+}
+
+spec.config = function()
+	local utils = require("yanky.utils")
+	local opts = {
+		ring = {
+			history_length = 20,
+		},
+		system_clipboard = {
+			sync_with_ring = false,
+		},
+		highlight = {
+			on_put = true,
+			on_yank = false,
+			timer = 125,
+		},
+		picker = {
+			select = {
+				action = require("yanky.picker").actions.set_register(utils.get_default_register()),
+			},
+		},
+	}
+
+	local has_telescope, telescope = pcall(require, "telescope")
+	if has_telescope then
+		telescope.load_extension("yank_history")
+
+		local mapping = require("yanky.telescope.mapping")
+
+		opts.picker.telescope = {
+			use_default_mappings = false,
+			mappings = {
+				default = mapping.set_register(utils.get_default_register()),
+				i = {
+					["<C-x>"] = mapping.delete(),
+				},
+				n = {
+					d = mapping.delete(),
+				},
+			},
+		}
+	end
+
+	require("yanky").setup(opts)
+end
+
+return spec
