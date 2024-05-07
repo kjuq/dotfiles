4883b985d492c270886db9529c638cc759d9cd36
kjuq <44694396+kjuq@users.noreply.github.com>
2024-05-08 07:31:11 +0900

    add: markdown-preview.nvim

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+local spec = { "iamcco/markdown-preview.nvim" }
+
+spec.cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" }
+spec.ft = { "markdown" }
+spec.build = function()
+	vim.fn["mkdp#util#install"]()
+end
+
+return spec
