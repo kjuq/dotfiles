50920eb2ff8a56910965832cdfd7c24c57286389
kjuq <44694396+kjuq@users.noreply.github.com>
2024-05-21 16:42:52 +0900

    mod: re-enable comment.nvim

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -4,7 +4,8 @@
 local spec = { "numToStr/Comment.nvim" }
 
 spec.keys = {
-	{ "gc", mode = { "n", "x" }, desc = "Comment: add line comment" },
+	{ "gc", mode = { "n", "x", "o" }, desc = "Comment: add line comment" },
+	{ "gcc", mode = { "n" }, desc = "Comment: add line comment" },
 	{ "gb", mode = { "n", "x" }, desc = "Comment: add block comments" },
 }
 
