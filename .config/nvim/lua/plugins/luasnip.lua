fa8e2771686f029fc45bb18388d736ca7d690ea1
kjuq <44694396+kjuq@users.noreply.github.com>
2024-05-31 03:50:42 +0900

    mod: change the key of expanding snippet to <C-k> from <C-i>

--- .config/nvim/lua/plugins/luasnip.lua
+++ .config/nvim/lua/plugins/luasnip.lua
@@ -3,20 +3,23 @@ local map = require("utils.lazy").generate_map("", "Luasnip: ")
 ---@type LazySpec
 local spec = { "L3MON4D3/LuaSnip" }
 
+local key_next = "<C-k>"
+local key_prev = "<C-M-k>"
+
 spec.keys = {
-	map("<Tab>", { "i", "s" }, function()
-		if require("luasnip").expand_or_jumpable() then
+	map(key_next, { "i", "s" }, function()
+		if require("luasnip").expand_or_locally_jumpable() then
 			require("luasnip").expand_or_jump()
 		else
-			local key = vim.api.nvim_replace_termcodes("<Tab>", true, false, true)
+			local key = vim.api.nvim_replace_termcodes(key_next, true, false, true)
 			vim.api.nvim_feedkeys(key, "n", false)
 		end
 	end, "expand or jump to a next placeholder"),
-	map("<S-Tab>", { "i", "s" }, function()
-		if require("luasnip").expand_or_jumpable() then
+	map(key_prev, { "i", "s" }, function()
+		if require("luasnip").expand_or_locally_jumpable() then
 			require("luasnip").jump(-1)
 		else
-			local key = vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true)
+			local key = vim.api.nvim_replace_termcodes(key_prev, true, false, true)
 			vim.api.nvim_feedkeys(key, "n", false)
 		end
 	end, "jump to a previous placeholder"),
