7b33f823c69a6e34eb9873236c25527c8c42ddf0
kjuq <44694396+kjuq@users.noreply.github.com>
2024-05-31 03:50:01 +0900

    mod: minor (edgemotion)

--- .config/nvim/lua/plugins/edgemotion.lua
+++ .config/nvim/lua/plugins/edgemotion.lua
@@ -4,8 +4,8 @@ local map = require("utils.lazy").generate_map("", "Edgemotion: ")
 local spec = { "haya14busa/vim-edgemotion" }
 
 spec.keys = {
-	map("<M-j>", "n", "<Plug>(edgemotion-j)", "Down"),
-	map("<M-k>", "n", "<Plug>(edgemotion-k)", "Up"),
+	map("<M-j>", { "n", "x" }, "<Plug>(edgemotion-j)", "Down"),
+	map("<M-k>", { "n", "x" }, "<Plug>(edgemotion-k)", "Up"),
 }
 
 return spec
