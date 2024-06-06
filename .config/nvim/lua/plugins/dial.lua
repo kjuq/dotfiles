6ab57a6eeafe4156d01c68e4b198589f3a7bb4e8
kjuq <44694396+kjuq@users.noreply.github.com>
2024-06-06 20:00:57 +0900

    mod: minor

--- .config/nvim/lua/plugins/dial.lua
+++ .config/nvim/lua/plugins/dial.lua
@@ -6,8 +6,8 @@ local spec = { "monaqa/dial.nvim" }
 spec.keys = {
 	map("<C-a>", { "n", "v" }, "<Plug>(dial-increment)", "Increment"),
 	map("<C-x>", { "n", "v" }, "<Plug>(dial-decrement)", "Decrement"),
-	map("g<C-a>", { "n", "v" }, "g<Plug>(dial-increment)", "G Increment", { remap = true }),
-	map("g<C-x>", { "n", "v" }, "g<Plug>(dial-decrement)", "G Decrement", { remap = true }),
+	map("g<C-a>", "v", "g<Plug>(dial-increment)", "G Increment", { remap = true }),
+	map("g<C-x>", "v", "g<Plug>(dial-decrement)", "G Decrement", { remap = true }),
 }
 
 return spec
