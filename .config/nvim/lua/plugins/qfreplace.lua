26b69017c515b262aa474f4841a144450789f7d3
kjuq <44694396+kjuq@users.noreply.github.com>
2024-06-15 00:28:29 +0900

    mod: `r` launchs qfreplace

--- .config/nvim/lua/plugins/qfreplace.lua
+++ .config/nvim/lua/plugins/qfreplace.lua
@@ -1,16 +1,17 @@
 ---@type LazySpec
 local spec = { "thinca/vim-qfreplace" }
-spec.cmd = "Qfreplace"
 
-spec.keys = {
-	{
-		"<leader>aq",
-		mode = "n",
-		function()
-			vim.cmd("Qfreplace")
+spec.ft = "qf"
+
+spec.config = function()
+	vim.g.qfreplace_no_save = 1
+	vim.api.nvim_create_autocmd({ "FileType" }, {
+		pattern = "qf",
+		group = vim.api.nvim_create_augroup("user_qfreplace", {}),
+		callback = function()
+			vim.keymap.set("n", "r", "<Cmd>Qfreplace<CR>", { buffer = true })
 		end,
-		{ desc = "Qfreplace: Open" },
-	},
-}
+	})
+end
 
 return spec
