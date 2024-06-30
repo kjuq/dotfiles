d1f315355ff010e348f7edf143259dbca7f8ad50
kjuq <44694396+kjuq@users.noreply.github.com>
2024-06-30 19:48:13 +0900

    add: cd <project_root> with project.nvim

--- .config/nvim/lua/plugins/project.lua
+++ .config/nvim/lua/plugins/project.lua
@@ -1,22 +1,24 @@
+local map = require("utils.lazy").generate_map("", "Project: ")
+
 ---@type LazySpec
 local spec = { "ahmedkhalf/project.nvim" }
 spec.name = "project_nvim"
-spec.event = { "BufNewFile", "BufReadPost" } -- necessary
+
+spec.cmd = {
+	"ProjectRoot",
+	"AddProject",
+}
 
 spec.keys = {
-	{
-		"<leader>fP",
-		mode = { "n" },
-		function()
-			local has_telescope, telescope = pcall(require, "telescope")
-			if has_telescope then
-				telescope.extensions.projects.projects({})
-			else
-				vim.notify("Telescope is not installed")
-			end
-		end,
-		desc = "Project: Pick projects",
-	},
+	map("<leader>ar", "n", "<Cmd>ProjectRoot<CR>", "cd <project_root>"),
+	map("<leader>fP", "n", function()
+		local has_telescope, telescope = pcall(require, "telescope")
+		if has_telescope then
+			telescope.extensions.projects.projects({})
+		else
+			vim.notify("Telescope is not installed")
+		end
+	end, "Pick projects"),
 }
 
 spec.opts = {
