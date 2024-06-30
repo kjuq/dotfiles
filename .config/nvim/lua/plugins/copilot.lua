716def551c8414483df174e05e17fb7340b6c563
kjuq <44694396+kjuq@users.noreply.github.com>
2024-06-30 20:04:45 +0900

    mod: disable all interfaces of copilot.lua

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -6,24 +6,6 @@ spec.cmd = "Copilot"
 spec.event = { "InsertEnter" }
 
 spec.opts = function()
-	local opts = {
-		suggestion = {
-			enabled = true,
-			auto_trigger = false,
-			keymap = {
-				accept = false,
-				accept_word = false,
-				accept_line = false,
-				next = "<M-i>",
-				prev = false,
-				dismiss = false,
-			},
-		},
-		panel = {
-			enabled = false,
-		},
-	}
-
 	local success, cmp = pcall(require, "cmp")
 	if success then
 		cmp.event:on("menu_opened", function()
@@ -35,42 +17,10 @@ spec.opts = function()
 		end)
 	end
 
-	local mod = require("copilot.suggestion")
-
-	--- @param key string
-	--- @param normal_behavior function
-	--- @param choosing_behavior function
-	local map = function(key, normal_behavior, choosing_behavior)
-		vim.keymap.set("i", key, function()
-			if not mod.is_visible() then
-				normal_behavior()
-			else
-				choosing_behavior()
-			end
-		end)
-	end
-
-	map("<C-g><C-f>", mod.next, mod.accept)
-	map("<C-l>", function()
-		vim.cmd.fclose({ bang = true })
-	end, mod.dismiss)
-
-	if not opts.suggestion.auto_trigger then
-		map("<M-f>", function()
-			mod.next()
-		end, function()
-			mod.accept_word()
-			mod.next()
-		end)
-		map("<M-n>", function()
-			mod.next()
-		end, function()
-			mod.accept_line()
-			mod.next()
-		end)
-	end
-
-	return opts
+	return {
+		panel = { enabled = false },
+		suggestion = { enabled = false },
+	}
 end
 
 return spec
