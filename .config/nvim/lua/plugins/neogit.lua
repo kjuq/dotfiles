df527054306e8e73e842bc8d136ef10d89c17b95
kjuq <44694396+kjuq@users.noreply.github.com>
2024-07-02 15:58:34 +0900

    add: Neogit

---
+++ .config/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+local spec = { "NeogitOrg/neogit" }
+
+spec.cmd = { "Neogit" }
+
+local map = require("utils.lazy").generate_map("", "Neogit: ")
+spec.keys = {
+	map("<leader>gn", "n", "<Cmd>Neogit kind=replace<CR>", "open"),
+}
+
+spec.opts = {}
+
+spec.dependencies = {
+	"nvim-lua/plenary.nvim",
+	"sindrets/diffview.nvim",
+	"nvim-telescope/telescope.nvim",
+}
+
+return spec
