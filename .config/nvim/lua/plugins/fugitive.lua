8c4efb5afdbfd4a40f78df26c71b922c11f5cac0
kjuq <44694396+kjuq@users.noreply.github.com>
2024-07-02 16:22:17 +0900

    add: vim-fugitive

---
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -0,0 +1,32 @@
+---@type LazySpec
+local spec = { "tpope/vim-fugitive" }
+
+spec.cmd = {
+	"G",
+	"GBrowse",
+	"GDelete",
+	"GMove",
+	"GRemove",
+	"GUnlink",
+	"Gcd",
+	"Gclog",
+	"Gdiffsplit",
+	"Gdrop",
+	"Gedit",
+	"Ggrep",
+	"Ghdiffsplit",
+	"Git",
+	"Glcd",
+	"Glgrep",
+	"Gllog",
+	"Gpedit",
+	"Gread",
+	"Gsplit",
+	"Gtabedit",
+	"Gvdiffsplit",
+	"Gvsplit",
+	"Gwq",
+	"Gwrite",
+}
+
+return spec
