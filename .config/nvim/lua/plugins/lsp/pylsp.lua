147a72a12c1b4ad1f54f4e804f14088bc818a147
kjuq <44694396+kjuq@users.noreply.github.com>
2024-07-05 04:49:25 +0900

    del: path set in plugins/lsp/pylsp.lua

--- .config/nvim/lua/plugins/lsp/pylsp.lua
+++ .config/nvim/lua/plugins/lsp/pylsp.lua
@@ -1,13 +1,9 @@
 local M = {}
 
-local default_path = vim.system({ "which", "python" }):wait().stdout
-local path = vim.env.VIRTUAL_ENV and vim.env.VIRTUAL_ENV .. "/bin/python3" or default_path
-
 M.opts = {
 	settings = {
 		pylsp = {
 			plugins = {
-				jedi = { environment = path },
 				pycodestyle = { enabled = false }, -- lint (contained to flake8)
 				pylint = { enabled = false },
 				mccabe = { enabled = false }, -- lint (contained to flake8)
