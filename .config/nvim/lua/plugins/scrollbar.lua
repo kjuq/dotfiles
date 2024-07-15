10ed2e42516755f1cc45441ef0850a2899d4f6c1
kjuq <44694396+kjuq@users.noreply.github.com>
2024-07-15 19:28:04 +0900

    mod: refactor scrollbar.lua

--- .config/nvim/lua/plugins/scrollbar.lua
+++ .config/nvim/lua/plugins/scrollbar.lua
@@ -1,22 +1,19 @@
-local hide_by_default = true
+local show_on_start = true
 
 ---@type LazySpec
 local spec = { "petertriho/nvim-scrollbar" }
-spec.event = hide_by_default and {} or { "WinScrolled" }
+spec.event = show_on_start and { "WinScrolled" } or {}
 
+local map = require("utils.lazy").generate_map("", "Scrollbar: ")
 spec.keys = {
-	{
-		"<leader>as",
-		mode = "n",
-		function()
-			require("scrollbar.utils").toggle()
-		end,
-		desc = "Scrollbar: Toggle",
-	},
+	map("<leader>as", "n", function()
+		require("scrollbar.utils").toggle()
+	end, "Toggle"),
 }
 
-spec.config = function()
-	require("scrollbar").setup({
+spec.opts = function()
+	return {
+		show = show_on_start,
 		excluded_filetypes = {
 			"ccc-ui",
 			"cmp_docs",
@@ -28,23 +25,23 @@ spec.config = function()
 			"saga_codeaction",
 			"sagarename",
 		},
-	})
-
-	require("scrollbar.handlers.search").setup({
-		override_lens = function() end,
-	})
-
-	require("gitsigns").setup() -- in case that gitsigns.lua doesn't exist
-	require("scrollbar.handlers.gitsigns").setup()
-
-	if hide_by_default then
-		require("scrollbar.utils").hide()
-	end
+	}
 end
 
 spec.dependencies = {
-	"kevinhwang91/nvim-hlslens",
-	"lewis6991/gitsigns.nvim",
+	{
+		"kevinhwang91/nvim-hlslens",
+		config = function(_, opts)
+			require("scrollbar.handlers.search").setup(opts)
+		end,
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function(_, opts)
+			require("gitsigns").setup(opts)
+			require("scrollbar.handlers.gitsigns").setup()
+		end,
+	},
 }
 
 return spec
