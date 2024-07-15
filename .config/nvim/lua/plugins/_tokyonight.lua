cacc2f36e576316a693577dabe01d245e9e126ce
kjuq <44694396+kjuq@users.noreply.github.com>
2024-07-15 21:33:03 +0900

    mov: catppuccin -> tokyonight

--- .config/nvim/lua/plugins/_tokyonight.lua
+++ .config/nvim/lua/plugins/_tokyonight.lua
@@ -3,15 +3,17 @@ local spec = { "folke/tokyonight.nvim" }
 spec.lazy = false
 spec.priority = 9999
 
-spec.config = function()
-	require("tokyonight").setup({
-		transparent = true,
-		dim_inactive = false,
-		styles = {
-			sidebars = "transparent", -- "dark", "transparent", "normal"
-			floats = "transparent", -- "dark", "transparent", "normal"
-		},
-	})
+spec.opts = {
+	transparent = true,
+	dim_inactive = false,
+	styles = {
+		sidebars = "transparent", -- "dark", "transparent", "normal"
+		floats = "transparent", -- "dark", "transparent", "normal"
+	},
+}
+
+spec.config = function(_, opts)
+	require("tokyonight").setup(opts)
 	vim.cmd.colorscheme("tokyonight")
 	vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#808080" })
 	vim.api.nvim_set_hl(0, "statusline", { bg = "NONE", ctermbg = "NONE" })
