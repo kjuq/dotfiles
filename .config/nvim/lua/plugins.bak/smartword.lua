6bf6cf1093ca84b77df05081ae6d2325cb5db8ef
kjuq <44694396+kjuq@users.noreply.github.com>
2024-07-19 09:50:20 +0900

    add: vim-smartword to plugins.bak

---
+++ .config/nvim/lua/plugins.bak/smartword.lua
@@ -0,0 +1,22 @@
+---@type LazySpec
+local spec = { "kana/vim-smartword" }
+
+spec.keys = {}
+
+local map = require("utils.lazy").generate_map("", "Smartword: ")
+spec.keys = {
+	map("w", { "n", "x", "o" }, function()
+		require("utils.common").feed_plug("smartword-w")
+	end, "w", { expr = true }),
+	map("b", { "n", "x", "o" }, function()
+		require("utils.common").feed_plug("smartword-b")
+	end, "b", { expr = true }),
+	map("e", { "n", "x", "o" }, function()
+		require("utils.common").feed_plug("smartword-e")
+	end, "e", { expr = true }),
+	map("ge", { "n", "x", "o" }, function()
+		require("utils.common").feed_plug("smartword-ge")
+	end, "ge", { expr = true }),
+}
+
+return spec
