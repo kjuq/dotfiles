87f4f18b986d2b81c21db56b4d616654a10f0b31
kjuq <44694396+kjuq@users.noreply.github.com>
2024-07-25 09:11:37 +0900

    mod: better utils/word_move.lua
    - not scrolling window weirdly
    - handle count

--- .config/nvim/lua/utils/word_move.lua
+++ .config/nvim/lua/utils/word_move.lua
@@ -2,44 +2,82 @@
 
 local M = {}
 
-local motion = function(key, back_key)
-	if vim.v.count1 > 1 then
-		vim.cmd("normal! " .. vim.v.count1 .. key)
+M.motion = function(motion)
+	-- TODO: implement operator pending mode
+	-- TODO: not moving cursor when the cursor on the posision beyond a first non-blank char (ie. indent)
+
+	local initial_line = vim.fn.line(".")
+	local initial_bottom = vim.fn.line("w$") -- displayed bottom line
+	motion()
+	local new_line = vim.fn.line(".")
+	local new_bottom = vim.fn.line("w$")
+
+	local line_diff = new_line - initial_line
+	if line_diff == 0 then
 		return
 	end
 
-	local initial_line = vim.fn.line(".")
-	vim.cmd("normal! " .. vim.v.count1 .. key)
-	local new_line = vim.fn.line(".")
+	local newline_is_below_init_line = line_diff > 0
+	local newline_is_above_init_line = line_diff < 0
 
-	if initial_line ~= new_line then
-		vim.cmd("normal! " .. vim.v.count1 .. back_key)
+	local back_key
+	if newline_is_below_init_line then
+		back_key = "k$"
+	elseif newline_is_above_init_line then
+		back_key = "j^"
 	end
+
+	local back_count = math.abs(line_diff)
+	vim.cmd("normal! " .. back_count .. back_key)
+
+	local scroll_diff = new_bottom - initial_bottom
+	if scroll_diff == 0 then
+		return
+	end
+
+	local scrolled_down = scroll_diff > 0
+	local scrolled_up = scroll_diff < 0
+
+	local scroll_back
+	if scrolled_down then
+		scroll_back = "\\<C-y>"
+	elseif scrolled_up then
+		scroll_back = "\\<C-e>"
+	end
+
+	local scroll_count = math.abs(scroll_diff)
+	vim.cmd('execute "normal! ' .. scroll_count .. scroll_back .. '"')
+end
+
+local key_motion = function(key)
+	M.motion(function()
+		return vim.cmd("normal! " .. vim.v.count1 .. key)
+	end)
 end
 
 M.w = function()
-	motion("w", "k$")
+	key_motion("w")
 end
 M.W = function()
-	motion("W", "k$")
+	key_motion("W")
 end
 M.e = function()
-	motion("e", "k$")
+	key_motion("e")
 end
 M.E = function()
-	motion("E", "k$")
+	key_motion("E")
 end
 M.b = function()
-	motion("b", "j^")
+	key_motion("b")
 end
 M.B = function()
-	motion("B", "j^")
+	key_motion("B")
 end
 M.ge = function()
-	motion("ge", "j^")
+	key_motion("ge")
 end
 M.gE = function()
-	motion("gE", "j^")
+	key_motion("gE")
 end
 
 return M
