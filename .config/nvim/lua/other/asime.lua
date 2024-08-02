3fe49cce2a782dc8ee13a1398755c2865eeb6f2e
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-02 10:38:10 +0900

    add: asime (Linux only so far)

---
+++ .config/nvim/lua/other/asime.lua
@@ -0,0 +1,10 @@
+-- loaded by `$LOCAL_BIN_PATH/nvimcopy`
+
+vim.opt_local.number = false
+vim.opt_local.relativenumber = false
+vim.opt_local.wrap = false
+
+vim.keymap.set({ "n", "i" }, "<C-g><C-g>", function()
+	vim.cmd.normal([[ggvG$h"+d]]) -- yank entire buffer
+	vim.cmd([[silent exec "!i3-msg scratchpad show"]])
+end, { buffer = true })
