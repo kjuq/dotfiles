083e966476041e60b13465861110ea7978ad258c
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-02 11:16:51 +0900

    mod: enter insert mode after yank in asime

--- .config/nvim/lua/other/asime.lua
+++ .config/nvim/lua/other/asime.lua
@@ -5,6 +5,6 @@ vim.opt_local.relativenumber = false
 vim.opt_local.wrap = false
 
 vim.keymap.set({ "n", "i" }, "<C-g><C-g>", function()
-	vim.cmd.normal([[ggvG$h"+d]]) -- yank entire buffer
+	vim.cmd.normal([[ggvG$h"+di]]) -- yank entire buffer
 	vim.cmd([[silent exec "!i3-msg scratchpad show"]])
 end, { buffer = true })
