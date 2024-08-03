4a919d7880f41ac4c7ebfc1c117eb3ada58d09cc
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-03 23:57:40 +0900

    mod: lazy load git-conflict.nvim

--- .config/nvim/lua/plugins/git-conflict.lua
+++ .config/nvim/lua/plugins/git-conflict.lua
@@ -1,8 +1,17 @@
 ---@type LazySpec
 local spec = { "akinsho/git-conflict.nvim" }
+
 spec.version = "*"
-spec.event = { "BufReadPost" }
+
+spec.event = "VeryLazy"
 
 spec.opts = {}
+
+spec.config = function(_, opts)
+	vim.defer_fn(function()
+		vim.cmd.edit()
+	end, 0)
+	require("git-conflict").setup(opts)
+end
 
 return spec
