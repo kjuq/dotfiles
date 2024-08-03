bebff34fccd962164f984d7c20860cf9d88968ee
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-04 00:05:57 +0900

    mod: lazyload sixelview.nvim

--- .config/nvim/lua/plugins/sixelview.lua
+++ .config/nvim/lua/plugins/sixelview.lua
@@ -1,8 +1,15 @@
 ---@type LazySpec
 local spec = { "kjuq/sixelview.nvim" }
 
-spec.event = { "BufWinEnter" }
+spec.event = "VeryLazy"
 
 spec.opts = {}
+
+spec.config = function(_, opts)
+	require("sixelview").setup(opts)
+	vim.defer_fn(function()
+		vim.cmd.edit()
+	end, 0)
+end
 
 return spec
