f3b35817ff1783cf7d07731b6393b11b6cd8f0ff
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-04 13:57:58 +0900

    add: configuration of Git-conflict.nvim

--- .config/nvim/lua/plugins/git-conflict.lua
+++ .config/nvim/lua/plugins/git-conflict.lua
@@ -5,13 +5,21 @@ spec.version = "*"
 
 spec.event = "VeryLazy"
 
-spec.opts = {}
+local map = require("utils.lazy").generate_map("", "Git-conflict: ")
+spec.keys = {
+	map("<leader>gcl", "n", "<Cmd>GitConflictListQf<CR>", "List QuickFix"),
+	map("<leader>gc]", "n", "<Cmd>GitConflictChooseTheirs<CR>", "Choose theirs"),
+	map("<leader>gc[", "n", "<Cmd>GitConflictChooseOurs<CR>", "Choose ours"),
+	map("<leader>gc=", "n", "<Cmd>GitConflictChooseBoth<CR>", "Choose both"),
+	map("<leader>gc-", "n", "<Cmd>GitConflictChooseNone<CR>", "Choose none"),
+	map("<leader>gc_", "n", "<Cmd>GitConflictChooseBase<CR>", "Choose base"),
+	map("]c", "n", "<Cmd>GitConflictNextConflict<CR>", "Next conflict"),
+	map("[c", "n", "<Cmd>GitConflictPrevConflict<CR>", "Previous conflict"),
+}
 
-spec.config = function(_, opts)
-	vim.defer_fn(function()
-		vim.cmd.edit()
-	end, 0)
-	require("git-conflict").setup(opts)
+spec.opts = function()
+	vim.notify("Git-conflict: Conflict detected")
+	return {}
 end
 
 return spec
