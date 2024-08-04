262f223ea91ef42055a2e685f07f94e71d54a9e3
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-04 16:42:28 +0900

    mod: lazyload sixelview.nvim

--- .config/nvim/lua/plugins/sixelview.lua
+++ .config/nvim/lua/plugins/sixelview.lua
@@ -3,13 +3,21 @@ local spec = { "kjuq/sixelview.nvim" }
 
 spec.event = "VeryLazy"
 
-spec.opts = {}
+spec.opts = {
+	delay_ms = 10,
+}
 
 spec.config = function(_, opts)
 	require("sixelview").setup(opts)
-	vim.defer_fn(function()
-		vim.cmd.edit()
-	end, 0)
+	vim.schedule(function()
+		local path = vim.fn.expand("%:p")
+		local default_pat = require("sixelview.config").default.pattern
+		local pattern = not opts.pattern and default_pat or opts.pattern
+		local is_image_buffer = require("sixelview.utils").is_image_buffer(path, pattern)
+		if is_image_buffer then
+			vim.cmd.SixelView()
+		end
+	end)
 end
 
 return spec
