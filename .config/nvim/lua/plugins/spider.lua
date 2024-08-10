d50bde17d5c9c20f1618eccdd9542ba5649234f8
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-10 21:21:14 +0900

    mod: minor

--- .config/nvim/lua/plugins/spider.lua
+++ .config/nvim/lua/plugins/spider.lua
@@ -1,18 +1,24 @@
 ---@type LazySpec
 local spec = { 'chrisgrieser/nvim-spider' }
 
-spec.cond = false
-
 local map = require('utils.lazy').generate_map('', 'Spider: ')
 spec.keys = {
-	-- map('w', { 'n', 'x', 'o' }, function() require('jolyne').motion(function() require('spider').motion('w') end) end, 'w'),
-	-- map('b', { 'n', 'x', 'o' }, function() require('jolyne').motion(function() require('spider').motion('b') end) end, 'b'),
-	-- map('e', { 'n', 'x', 'o' }, function() require('jolyne').motion(function() require('spider').motion('e') end) end, 'e'),
-	-- map('ge', { 'n', 'x', 'o' }, function() require('jolyne').motion(function() require('spider').motion('ge') end) end, 'ge'),
-	map('w', { 'n', 'x', 'o' }, function() require('spider').motion('w') end, 'w'),
-	map('b', { 'n', 'x', 'o' }, function() require('spider').motion('b') end, 'b'),
-	map('e', { 'n', 'x', 'o' }, function() require('spider').motion('e') end, 'e'),
-	map('ge', { 'n', 'x', 'o' }, function() require('spider').motion('ge') end, 'ge'),
+	map('w', { 'n', 'x', 'o' }, function()
+		require('jolyne').motion(function() require('spider').motion('w') end)
+	end, 'w'),
+	map('b', { 'n', 'x', 'o' }, function()
+		require('jolyne').motion(function() require('spider').motion('b') end)
+	end, 'b'),
+	map('e', { 'n', 'x', 'o' }, function()
+		require('jolyne').motion(function() require('spider').motion('e') end)
+	end, 'e'),
+	map('ge', { 'n', 'x', 'o' }, function()
+		require('jolyne').motion(function() require('spider').motion('ge') end)
+	end, 'ge'),
+	-- map('w', { 'n', 'x', 'o' }, function() require('spider').motion('w') end, 'w'),
+	-- map('b', { 'n', 'x', 'o' }, function() require('spider').motion('b') end, 'b'),
+	-- map('e', { 'n', 'x', 'o' }, function() require('spider').motion('e') end, 'e'),
+	-- map('ge', { 'n', 'x', 'o' }, function() require('spider').motion('ge') end, 'ge'),
 }
 
 spec.opts = {}
