d60098459063510380d4c2d4c55e8d2da3aa4b87
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-20 09:18:01 +0900

    fix: cmp not appearing automatically

--- .config/nvim/lua/plugins/lazydev.lua
+++ .config/nvim/lua/plugins/lazydev.lua
@@ -12,6 +12,7 @@ spec.opts = {
 
 spec.config = function(_, opts)
 	local group = vim.api.nvim_create_augroup('user_lazyload_lazydev', {})
+
 	vim.api.nvim_create_autocmd({ 'FileType' }, {
 		pattern = 'lua',
 		group = group,
@@ -20,36 +21,33 @@ spec.config = function(_, opts)
 		end,
 		once = true,
 	})
+
 	vim.api.nvim_exec_autocmds('FileType', { pattern = vim.o.filetype })
 
-	vim.api.nvim_create_autocmd({ 'InsertEnter' }, {
+	local cmp_init = function()
+		require('cmp').setup({
+			name = 'lazydev',
+			group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+		})
+	end
+
+	local callback = function(ev)
+		local plugname = ev.data
+		if plugname == 'nvim-cmp' then
+			cmp_init()
+		end
+	end
+
+	-- To load cmp lazily, not using `spec.dependencies`
+	vim.api.nvim_create_autocmd({ 'User' }, {
+		pattern = 'LazyLoad',
 		group = group,
-		callback = function()
-			local cmp_opts = require('plugins.cmp').opts or {} --[[@ as cmp.ConfigSchema]]
-			cmp_opts.sources = cmp_opts.sources or {}
-			table.insert(cmp_opts.sources, {
-				name = 'lazydev',
-				group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-			})
-			require('cmp').setup(cmp_opts)
-		end,
-		once = true,
+		callback = callback,
 	})
 end
 
 spec.dependencies = {
 	'Bilal2453/luvit-meta', -- optional `vim.uv` typings
-	-- {
-	-- 	-- TODO: lazy loading cmp
-	-- 	"hrsh7th/nvim-cmp",
-	-- 	opts = function(_, opts)
-	-- 		opts.sources = opts.sources or {}
-	-- 		table.insert(opts.sources, {
-	-- 			name = "lazydev",
-	-- 			group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-	-- 		})
-	-- 	end,
-	-- },
 }
 
 return spec
