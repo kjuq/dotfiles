927ea3b28b324ab6db0ef8cca5e83a3561b39e64
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-20 09:36:23 +0900

    add: quicker.nvim

--- .config/nvim/lua/plugins.disabled/qfreplace.lua
+++ .config/nvim/lua/plugins.disabled/qfreplace.lua
@@ -7,7 +7,7 @@ spec.config = function()
 	vim.g.qfreplace_no_save = 1
 	vim.api.nvim_create_autocmd({ 'FileType' }, {
 		pattern = 'qf',
-		group = vim.api.nvim_create_augroup('user_qfreplace', {}),
+		group = vim.api.nvim_create_augroup('kjuq_qfreplace', {}),
 		callback = function()
 			vim.keymap.set('n', 'r', '<Cmd>Qfreplace<CR>', { buffer = true })
 		end,
---
+++ .config/nvim/lua/plugins/quicker.lua
@@ -0,0 +1,10 @@
+---@type LazySpec
+local spec = { 'stevearc/quicker.nvim' }
+
+spec.ft = 'qf'
+
+---@module "quicker"
+---@type quicker.SetupOptions
+spec.opts = {}
+
+return spec
