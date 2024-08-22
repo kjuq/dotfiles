6e8434bf6b054f642a0be06606a492c7d6e75db7
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-22 11:29:31 +0900

    fix: center-bduf when wrap is enabled

--- .config/nvim/lua/center-bduf/init.lua
+++ .config/nvim/lua/center-bduf/init.lua
@@ -15,7 +15,7 @@ local top = function()
 	return vim.fn.line('w0') -- displayed top line
 end
 
-local height = function()
+local win_height = function()
 	return vim.fn.winheight(0)
 end
 
@@ -31,8 +31,22 @@ local is_top = function()
 	return top() == 1
 end
 
+local is_wrapped = function(linenum)
+	return vim.fn.virtcol({ linenum, '$' }) > vim.fn.winwidth(0)
+end
+
+local wrapped_lines_num = function()
+	local cnt = 0
+	for linenum = top(), bottom() do
+		if is_wrapped(linenum) then
+			cnt = cnt + 1
+		end
+	end
+	return cnt
+end
+
 local is_exceeded = function()
-	return visual_height() < height()
+	return visual_height() < win_height() - wrapped_lines_num()
 end
 
 local cursor_line = function()
@@ -44,41 +58,43 @@ local relative_cursorline = function()
 end
 
 local scroll_down = function(key)
-	if not is_bottom() then
-		local initial_rel_line = relative_cursorline()
-		vim.cmd.execute([["normal! \]] .. key .. '"')
-
-		if is_exceeded() then -- only for <C-f>
-			vim.cmd.normal({ args = { 'Gzb' }, bang = true })
-		end
-
-		local new_line
-		if initial_rel_line <= vim.o.scrolloff then
-			new_line = top() + vim.o.scrolloff
-		else
-			new_line = top() + initial_rel_line - 1
-		end
-
-		-- force `startofline` like behavior (It is too hard to implement `nostartofline`)
-		vim.fn.cursor(new_line, 0)
+	if is_bottom() then
+		return
 	end
+	local initial_rel_line = relative_cursorline()
+	vim.cmd.execute([["normal! \]] .. key .. '"')
+
+	if is_exceeded() then -- only for <C-f>
+		vim.cmd.normal({ args = { 'Gzb' }, bang = true })
+	end
+
+	local new_line
+	if initial_rel_line <= vim.o.scrolloff then
+		new_line = top() + vim.o.scrolloff
+	else
+		new_line = top() + initial_rel_line - 1
+	end
+
+	-- force `startofline` like behavior (It is too hard to implement `nostartofline`)
+	vim.fn.cursor(new_line, 0)
 end
 
 local scroll_up = function(key)
-	if not is_top() then
-		local initial_rel_line = relative_cursorline()
-		vim.cmd.execute([["normal! \]] .. key .. '"')
-
-		local new_line
-		if initial_rel_line >= height() - vim.o.scrolloff then
-			new_line = bottom() - vim.o.scrolloff
-		else
-			new_line = top() + initial_rel_line - 1
-		end
-
-		-- `startofline` same as scroll_down
-		vim.fn.cursor(new_line, 0)
+	if is_top() then
+		return
 	end
+	local initial_rel_line = relative_cursorline()
+	vim.cmd.execute([["normal! \]] .. key .. '"')
+
+	local new_line
+	if initial_rel_line >= win_height() - vim.o.scrolloff then
+		new_line = bottom() - vim.o.scrolloff
+	else
+		new_line = top() + initial_rel_line - 1
+	end
+
+	-- `startofline` same as scroll_down
+	vim.fn.cursor(new_line, 0)
 end
 
 M.cd = function()
