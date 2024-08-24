53fcf38728c24011dd502bcccce808928fa5f76b
kjuq <44694396+kjuq@users.noreply.github.com>
2024-08-25 00:36:39 +0900

    mod: nvim-surround (code block etc)

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -3,15 +3,26 @@ local spec = { 'kylechui/nvim-surround' }
 spec.version = '*' -- Use for stability; omit to use `main` branch for the latest features
 
 spec.keys = {
+	{ '<C-g>s', mode = { 'i' } },
+	{ '<C-g>S', mode = { 'i' } },
 	{ 'ys', mode = { 'n' } },
+	{ 'yS', mode = { 'n' } },
+	{ 'S', mode = { 'x' } },
+	{ 'gS', mode = { 'x' } },
 	{ 'ds', mode = { 'n' } },
 	{ 'cs', mode = { 'n' } },
-	{ 'y', mode = { 'x' } },
-	{ 'd', mode = { 'x' } },
-	{ 'c', mode = { 'x' } },
+	{ 'cS', mode = { 'n' } },
 }
 
-spec.opts = {}
+spec.opts = {
+	surrounds = {
+		['c'] = { -- Markdown fenced code blocks
+			add = function()
+				return { '```', '```' }
+			end,
+		},
+	},
+}
 
 spec.dependencies = {
 	'nvim-treesitter/nvim-treesitter',
