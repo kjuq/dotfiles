88757a150b83a6e726ef5c5fd99ff61193f141fc
kjuq <44694396+kjuq@users.noreply.github.com>
2024-09-10 10:18:27 +0900

    mod: order of fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,8 @@ source "$__fish_config_dir/configs/checkhealth.fish"
 
 source "$__fish_config_dir/configs/variable.fish" # need to load first
 
+source "$__fish_config_dir/configs/path.fish"
 source "$__fish_config_dir/configs/alias.fish"
 source "$__fish_config_dir/configs/function.fish"
 source "$__fish_config_dir/configs/misc.fish"
 source "$__fish_config_dir/configs/option.fish"
-source "$__fish_config_dir/configs/path.fish"
