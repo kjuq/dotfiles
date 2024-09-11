dcf015713b17e4d90fc3250d0677d350aa860489
kjuq <44694396+kjuq@users.noreply.github.com>
2024-09-11 10:24:05 +0900

    add: load local fish config if it exists

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,3 +7,8 @@ source "$__fish_config_dir/configs/alias.fish"
 source "$__fish_config_dir/configs/function.fish"
 source "$__fish_config_dir/configs/misc.fish"
 source "$__fish_config_dir/configs/option.fish"
+
+set --local localconf "$__fish_config_dir/config_local.fish"
+if [ -e "$localconf" ]
+	source "$localconf"
+end
