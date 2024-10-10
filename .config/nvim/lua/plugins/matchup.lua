8b7a3ed5a838c028351c1f99fbf5500e2193f684
kjuq <44694396+kjuq@users.noreply.github.com>
2024-10-10 19:20:24 +0900

    mod: WIP (disable vim-matchup in big file)

--- .config/nvim/lua/plugins/matchup.lua
+++ .config/nvim/lua/plugins/matchup.lua
@@ -12,7 +12,38 @@ spec.config = function()
 		},
 	})
 	-- for lazy-load, reload filetype
+	-- TODO: use `:MatchupReload` ?
 	vim.bo.filetype = vim.bo.filetype
+
+	local is_big_file = function()
+		local max_line = 50000
+		return vim.api.nvim_buf_line_count(0) > max_line
+	end
+
+	local reenable = function()
+		vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+			group = vim.api.nvim_create_augroup('kjuq_matchup_enable', {}),
+			callback = function()
+				vim.cmd('DoMatchParen')
+			end,
+			once = true,
+		})
+	end
+
+	local disable = function()
+		vim.schedule(function()
+			if is_big_file() then
+				vim.cmd('NoMatchParen')
+				reenable()
+			end
+		end)
+	end
+
+	disable()
+	vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
+		group = vim.api.nvim_create_augroup('kjuq_matchup_disable', {}),
+		callback = disable,
+	})
 end
 
 spec.dependencies = {
