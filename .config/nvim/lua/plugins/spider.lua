506c4d3eb215553daa7a879b8eebd152d0c84881
kjuq <44694396+kjuq@users.noreply.github.com>
2024-10-21 02:55:31 +0900

    mod: alleviate spider's movement on Japanese letters

--- .config/nvim/lua/plugins/spider.lua
+++ .config/nvim/lua/plugins/spider.lua
@@ -1,6 +1,17 @@
 ---@type LazySpec
 local spec = { 'chrisgrieser/nvim-spider' }
 
+-- Hiragana (U+3040 - U+309F)
+-- Katakana (U+30A0 - U+30FF)
+-- Sub-Katakana (カタカナ補助) (U+31F0 - U+31FF)
+-- Kanji (U+4E00 - U+9FFF)
+-- CJK-compatible Kanji (U+F900 - U+FAFF)
+local japanese_regex = '[\u{3040}-\u{30FF}\u{31F0}-\u{31FF}\u{4E00}-\u{9FFF}\u{F900}-\u{FAFF}]+'
+
+local is_japanese_letter = function(str)
+	return string.match(str, japanese_regex) ~= nil
+end
+
 local map = require('utils.lazy').generate_map('', 'Spider: ')
 spec.keys = {
 	map('w', { 'n', 'x', 'o' }, function()
@@ -23,12 +34,13 @@ spec.keys = {
 			require('spider').motion('ge')
 		end)
 	end, 'ge'),
-	-- map('w', { 'n', 'x', 'o' }, function() require('spider').motion('w') end, 'w'),
-	-- map('b', { 'n', 'x', 'o' }, function() require('spider').motion('b') end, 'b'),
-	-- map('e', { 'n', 'x', 'o' }, function() require('spider').motion('e') end, 'e'),
-	-- map('ge', { 'n', 'x', 'o' }, function() require('spider').motion('ge') end, 'ge'),
 }
 
-spec.opts = {}
+spec.opts = {
+	customPatterns = {
+		patterns = { japanese_regex },
+		overrideDefault = false,
+	},
+}
 
 return spec
