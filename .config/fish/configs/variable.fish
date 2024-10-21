24f5f701629303c49cf9d4f902f53204c86373d5
kjuq <44694396+kjuq@users.noreply.github.com>
2024-10-22 05:48:14 +0900

    mod: dotfiles of PostgreSQL is now stored at XDG_DATA_HOME

--- .config/fish/configs/variable.fish
+++ .config/fish/configs/variable.fish
@@ -49,6 +49,9 @@ set --export W3M_DIR "$XDG_CONFIG_HOME/w3m"
 set --export RCLONE_PROGRESS true
 set --export RCLONE_EXCLUDE "*.DS_Store"
 
+# PostgreSQL
+set --export PSQL_HISTORY "$XDG_DATA_HOME/psql_history"
+
 # GnuPG
 set --export GNUPGHOME "$XDG_CONFIG_HOME/gnupg"
 set --export GPG_TTY (tty)
