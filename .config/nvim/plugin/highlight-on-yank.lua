a7c2487dbe7a85d31cea9112a9a65905e0a8ac57
kjuq <44694396+kjuq@users.noreply.github.com>
2024-11-03 00:45:17 +0900

    fix: highlight-on-yank

--- .config/nvim/plugin/highlight-on-yank.lua
+++ .config/nvim/plugin/highlight-on-yank.lua
@@ -1,10 +1,20 @@
-local callback = function()
+local group = vim.api.nvim_create_augroup('kjuq_highlight_on_yank', {})
+
+local set_hl = function()
+	vim.api.nvim_set_hl(0, 'kjuq_highlight_on_yank', { bg = '#c43963' }) -- color is from SagaBeacon of LspSaga
+end
+vim.api.nvim_create_autocmd({ 'ColorScheme' }, {
+	group = group,
+	callback = set_hl,
+})
+
+local highlight = function()
 	local timeout = require('utils.common').highlight_duration
 	require('vim.hl').on_yank({ higroup = 'kjuq_highlight_on_yank', timeout = timeout })
 end
-
-vim.api.nvim_set_hl(0, 'kjuq_highlight_on_yank', { bg = '#c43963' }) -- color is from SagaBeacon of LspSaga
 vim.api.nvim_create_autocmd({ 'TextYankPost' }, {
-	group = vim.api.nvim_create_augroup('kjuq_highlight_on_yank', {}),
-	callback = callback,
+	group = group,
+	callback = highlight,
 })
+
+set_hl()
