3758e64d0d24dcc5a869e147c9218d01b8fc3633
kjuq <44694396+kjuq@users.noreply.github.com>
2024-11-05 01:42:04 +0900

    mod: better osc52

--- .config/nvim/plugin/clipboard.lua
+++ .config/nvim/plugin/clipboard.lua
@@ -1,30 +1,46 @@
--- vim.g.clipboard = {
--- 	name = 'OSC 52',
--- 	copy = {
--- 		['+'] = require('vim.ui.clipboard.osc52').copy('+'),
--- 		['*'] = require('vim.ui.clipboard.osc52').copy('*'),
--- 	},
--- 	paste = {
--- 		['+'] = require('vim.ui.clipboard.osc52').paste('+'),
--- 		['*'] = require('vim.ui.clipboard.osc52').paste('*'),
--- 	},
--- }
+-- NOTE: If you ssh from a tmux session to a remote system and run neovim there, pasting does not work.
+-- In spite of copying is working flawlessly
+-- https://github.com/neovim/neovim/discussions/29350#discussioncomment-11127983
 
-local paste = function()
+if not os.getenv('SSH_TTY') then
+	return
+end
+
+local ssh_without_tmux = {
+	name = 'OSC 52 (ssh without tmux)',
+	copy = {
+		['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+		['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+	},
+	paste = {
+		['+'] = require('vim.ui.clipboard.osc52').paste('+'),
+		['*'] = require('vim.ui.clipboard.osc52').paste('*'),
+	},
+}
+
+local paste_nothing = function()
 	return {
 		vim.fn.split(vim.fn.getreg(''), '\n'),
 		vim.fn.getregtype(''),
 	}
 end
 
-vim.g.clipboard = {
-	name = 'OSC 52',
+local ssh_within_tmux = {
+	name = 'OSC 52 (SSH in tmux)',
 	copy = {
 		['+'] = require('vim.ui.clipboard.osc52').copy('+'),
 		['*'] = require('vim.ui.clipboard.osc52').copy('*'),
 	},
 	paste = {
-		['+'] = paste,
-		['*'] = paste,
+		['+'] = paste_nothing,
+		['*'] = paste_nothing,
 	},
 }
+
+require('vim.termcap').query('Ms', function(_, found, _)
+	if found then
+		vim.g.clipboard = ssh_without_tmux
+	else
+		vim.g.clipboard = ssh_within_tmux
+	end
+end)
