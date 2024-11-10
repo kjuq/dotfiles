bdce995e3ceb8b6d8f5140cfc360c6627a9fee64
kjuq <44694396+kjuq@users.noreply.github.com>
2024-11-11 04:50:11 +0900

    mod: minor

--- .config/fish/configs/misc.fish
+++ .config/fish/configs/misc.fish
@@ -1,21 +1,19 @@
-# fzf
-fzf_configure_bindings --directory=\cs --variables=\ev
-set fzf_fd_opts --hidden --exclude .git # used by fzf.fish
-set --export FZF_DEFAULT_COMMAND "fd --type f $fzf_fd_opts"
-set --export FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border'
+if command --search --quiet fzf
+	fzf_configure_bindings --directory=\cs --variables=\ev
+	set fzf_fd_opts --hidden --exclude .git # used by fzf.fish
+	set --export FZF_DEFAULT_COMMAND "fd --type f $fzf_fd_opts"
+	set --export FZF_DEFAULT_OPTS '--height 40% --layout=reverse --border'
+end
 
-# Zoxide initialization
 if command --search --quiet zoxide
 	zoxide init --cmd c fish | source
 end
 
-# direnv
 if command --search --quiet direnv
 	direnv hook fish | source
 	set --export DIRENV_LOG_FORMAT ""
 end
 
-# mise
 if command --search --quiet mise
 	mise activate fish | source
 end
--- .config/fish/configs/path.fish
+++ .config/fish/configs/path.fish
@@ -5,18 +5,16 @@ set -l brew_path
 
 if [ (uname) = "Darwin" ]
 	set brew_path "/opt/homebrew/bin/brew"
+	fish_add_path "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
 else if [ (uname) = "Linux" ]
 	set brew_path "/home/linuxbrew/.linuxbrew/bin/brew"
 end
 
 if [ -x $brew_path ]
 	eval ($brew_path shellenv)
-	fish_add_path "$HOMEBREW_PREFIX/opt/coreutils/libexec/gnubin"
 end
 
-# create local bin if it not exists
 if [ ! -d "$LOCAL_BIN_PATH" ]
 	mkdir "$LOCAL_BIN_PATH"
 end
-
 fish_add_path "$LOCAL_BIN_PATH"
