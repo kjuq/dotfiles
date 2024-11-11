a28f8bd43d1ebe8e357b2f9047dc1a30af470587
kjuq <44694396+kjuq@users.noreply.github.com>
2024-11-11 09:32:11 +0900

    add: <leader>aL to LspInfo

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,6 +1,11 @@
 ---@type LazySpec
 local spec = { 'neovim/nvim-lspconfig' }
 
+local map = require('utils.lazy').generate_map('', 'Lspconfig: ')
+spec.keys = {
+	map('<leader>aL', 'n', '<CMD>LspInfo<CR>', 'LspInfo'),
+}
+
 spec.config = function()
 	require('core.lsp').setup()
 	require('lspconfig.ui.windows').default_options.border = require('utils.common').floatwinborder
