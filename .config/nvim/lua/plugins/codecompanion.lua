ae09d60a5ae064cdb7ddc676349030404a9cf782
kjuq <44694396+kjuq@users.noreply.github.com>
2024-11-12 11:48:34 +0900

    mod: minor

--- .config/nvim/lua/plugins/codecompanion.lua
+++ .config/nvim/lua/plugins/codecompanion.lua
@@ -135,13 +135,11 @@ spec.opts = {
 spec.specs = {
 	'nvim-lua/plenary.nvim',
 	'nvim-treesitter/nvim-treesitter',
+	'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
 }
 
 spec.dependencies = {
-	-- 'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
-	-- 'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
-	{ 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } }, -- Optional: For prettier markdown rendering
-	{ 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
+	'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
 }
 
 return spec
