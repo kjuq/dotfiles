b6d816f2b0b62d68822c919ce8f4156faedf6edb
kjuq <44694396+kjuq@users.noreply.github.com>
2024-11-25 03:32:26 +0900

    mod: Copilot Chat with Gp.nvim

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -1,5 +1,3 @@
-local map = require('utils.lazy').generate_map('<leader>c', 'GP: ')
-
 ---@type LazySpec
 local spec = { 'robitx/gp.nvim' }
 
@@ -15,6 +13,7 @@ spec.cmd = {
 	'GpNew',
 	'GpVnew',
 	'GpPopup',
+	'GpImplement',
 	'GpContext',
 	'GpNextAgent',
 	'GpAgent',
@@ -22,41 +21,90 @@ spec.cmd = {
 	'GpImageAgent',
 }
 
+local map = require('utils.lazy').generate_map('<leader>c', 'GP: ')
 spec.keys = {
-	map('g', 'n', '<CMD>GpChatToggle<CR>', 'Open chat'),
+	{ 'G', mode = 'c' }, -- lazyload commands
 
-	-- map('q', 'n', '<CMD>GpStop<CR>', 'Stop responses and jobs'),
-	-- map('n', 'n', '<CMD>GpChatNew<CR>', 'Open new chat'),
-	-- map('d', 'n', '<CMD>GpChatDelete<CR>', 'Delete the current chat'),
-	-- map('r', 'n', '<CMD>GpRewrite<CR>', 'Open prompt to rewrite codes'),
-	--
-	-- map('f', 'n', '<CMD>GpChatFinder<CR>', 'Search through chats'),
-	-- map('i', 'n', '<CMD>GpImage<CR>', 'Open prompt to generate image'),
-	--
-	-- map('y', 'x', '<CMD>GpChatPaste<CR>', 'Paste selected text to a chat'),
-	-- map('j', 'n', '<CMD>GpAppend<CR>', 'Open prompt to append codes'),
-	-- map('k', 'n', '<CMD>GpPrepend<CR>', 'Open prompt to prepend codes'),
-	--
-	-- map('X', 'n', '<CMD>GpContext<CR>', 'Configure custom context per repo'),
+	map('o', 'n', '<CMD>GpChatToggle<CR>', 'Open chat'),
+	map('r', 'n', '<CMD>GpRewrite<CR>', 'Open prompt to rewrite codes'),
+	map('y', 'x', '<CMD>GpChatPaste<CR>', 'Paste selected text to a chat'),
+	map('j', 'n', '<CMD>GpAppend<CR>', 'Open prompt to append codes'),
+	map('k', 'n', '<CMD>GpPrepend<CR>', 'Open prompt to prepend codes'),
+
+	map('x', 'n', '<CMD>GpContext<CR>', 'Configure custom context per repo'),
+
+	map('n', 'n', '<CMD>GpChatNew<CR>', 'Open new chat'),
 }
 
+---@type GpConfig
 spec.opts = {
-	openai_api_key = { 'pass', 'openai.com/api_key' },
 	-- chat_dir = os.getenv("HOME") .. "/documents/chatgpt",
+	providers = {
+		openai = {
+			secret = { 'pass', 'openai.com/api_key' },
+		},
+		copilot = {
+			disable = false,
+			-- Secret is automatically fetched with `copilot.vim`
+			-- `$XDG_CONFIG_HOME/github-copilot/hosts.json`
+		},
+	},
+
+	agents = {
+		{
+			provider = 'copilot',
+			name = 'ChatCopilot-Claude (kjuq configured)',
+			chat = true,
+			command = false,
+			-- string with model name or table with model name and parameters
+			model = { model = 'claude-3.5-sonnet', temperature = 1.1, top_p = 1 },
+			system_prompt = require('utils.ai').system_prompt('Japanese'),
+		},
+	},
+
+	default_chat_agent = 'ChatCopilot-Claude (kjuq configured)',
+	default_command_agent = 'CodeCopilot',
 
 	chat_user_prefix = '󰭹 :',
 	chat_assistant_prefix = { '󰚩 :', '[{{agent}}]' },
+	chat_template = table.concat({
+		'# topic: ?',
+		'- file: {{filename}}',
+		'---',
+		'',
+		'{{user_prefix}}',
+	}, '\n'),
 
 	chat_confirm_delete = true,
 	chat_conceal_model_params = false,
 
-	chat_shortcut_respond = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<C-g><C-g>' },
+	chat_shortcut_respond = {
+		modes = { 'n', 'i', 'v', 'x' },
+		shortcut = '<C-g><C-g>',
+	},
+	chat_shortcut_delete = {
+		modes = { 'n' },
+		shortcut = '<leader>cD',
+	},
+	chat_shortcut_stop = {
+		modes = { 'n' },
+		shortcut = '<leader>cQ',
+	},
+	chat_shortcut_new = {
+		modes = { 'n' },
+		shortcut = '<Nop>',
+	},
+	chat_finder_mappings = {
+		delete = { modes = { 'n', 'i', 'v', 'x' }, shortcut = '<Nop>' },
+	},
+
+	chat_free_cursor = true,
 
 	-- how to display GpChatToggle or GpContext: popup / split / vsplit / tabnew
 	toggle_target = '', -- empty for keeping current layout
 
 	-- styling for chatfinder
-	style_chat_finder_border = 'single',
+	style_chat_finder_border = require('utils.common').floatwinborder,
 
 	style_chat_finder_margin_bottom = 8,
 	style_chat_finder_margin_left = 1,
@@ -66,7 +114,7 @@ spec.opts = {
 	style_chat_finder_preview_ratio = 0.5,
 
 	-- styling for popup
-	style_popup_border = 'single',
+	style_popup_border = require('utils.common').floatwinborder,
 	-- margins are number of characters or lines
 	style_popup_margin_bottom = 8,
 	style_popup_margin_left = 1,
@@ -87,25 +135,29 @@ spec.config = function(_, opts)
 	gp.setup(opts)
 
 	-- There are no way to close a chat window when setting `toggle_target = ''`
-	-- to render a chat window same as normal buffer.
-	-- To fix that, buffer-local keymap is set.
-	local chat_dir = opts.chat_dir or gp.config.chat_dir
-	local close_key = '<C-Tab>'
-	local is_buf_listed = function()
-		return vim.fn.buflisted(vim.fn.bufnr('%')) == 1
-	end
-	local set_close_key = function()
-		if is_buf_listed() then
+	-- to render a chat window same as normal buffer. To fix that, use buffer-local keymap.
+	local set_close_key = function(key)
+		if vim.fn.buflisted(vim.fn.bufnr('%')) == 1 then
 			return
 		end
-		vim.keymap.set('n', close_key, '<Cmd>bnext<CR>', { buffer = true })
+		vim.keymap.set('n', key, '<Cmd>bnext<CR>', { buffer = true })
 	end
 
+	local setlocalkeys = function()
+		local map = function(key, cmd, desc)
+			return vim.keymap.set('n', key, '<CMD>' .. cmd .. '<CR>', { desc = desc })
+		end
+		vim.keymap.set('n', '<leader>cR', '<Cmd>GpChatRespond<CR>', { desc = 'Request new GPT Response(s)' })
+	end
+
+	local chat_dir = opts.chat_dir or gp.config.chat_dir
 	vim.api.nvim_create_autocmd({ 'BufNew' }, {
 		pattern = chat_dir .. '*',
 		group = vim.api.nvim_create_augroup('kjuq_gp_bufnew', {}),
 		callback = function()
-			vim.schedule(set_close_key)
+			vim.schedule(function()
+				set_close_key('<C-Tab>')
+			end)
 		end,
 	})
 end
---
+++ .config/nvim/lua/utils/ai.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+---@param lang 'Japanese'|'English'
+---@return string
+M.system_prompt = function(lang)
+	-- From CodeCompanion.nvim
+	-- https://github.com/olimorris/codecompanion.nvim/blob/926027bec8d7251730fe696794ced003152033fc/lua/codecompanion/config.lua#L876-L908
+	return string.format(
+		table.concat({
+			[[You are an AI programming assistant named "kjuq-Assister".]],
+			[[You are currently plugged in to the Neovim text editor on a user's machine.]],
+			[[Your core tasks include:]],
+			[[- Answering general programming questions.]],
+			[[- Explaining how the code in a Neovim buffer works.]],
+			[[- Reviewing the selected code in a Neovim buffer.]],
+			[[- Generating unit tests for the selected code.]],
+			[[- Proposing fixes for problems in the selected code.]],
+			[[- Scaffolding code for a new workspace.]],
+			[[- Finding relevant code to the user's query.]],
+			[[- Proposing fixes for test failures.]],
+			[[- Answering questions about Neovim.]],
+			[[- Running tools.]],
+			[[You must:]],
+			[[- Follow the user's requirements carefully and to the letter.]],
+			[[- Keep your answers short and impersonal, especially if the user responds with context outside of your tasks.]],
+			[[- Minimize other prose.]],
+			[[- Use Markdown formatting in your answers.]],
+			[[- Include the programming language name at the start of the Markdown code blocks.]],
+			[[- Avoid line numbers in code blocks.]],
+			[[- Avoid wrapping the whole response in triple backticks.]],
+			[[- Only return code that's relevant to the task at hand. You may not need to return all of the code that the user has shared.]],
+			[[- Use actual line breaks instead of '\n' in your response to begin new lines.]],
+			[[- Use '\n' only when you want a literal backslash followed by a character 'n'.]],
+			[[- All non-code responses must use %s.]],
+			[[When given a task:]],
+			[[1. Think step-by-step and describe your plan for what to build in pseudocode, written out in great detail, unless asked not to do so.]],
+			[[2. Output the code in a single code block, being careful to only return relevant code.]],
+			[[3. You should always generate short suggestions for the next user turns that are relevant to the conversation.]],
+			[[4. You can only give one reply for each conversation turn.]],
+		}, '\n'),
+		lang
+	)
+end
+
+return M
