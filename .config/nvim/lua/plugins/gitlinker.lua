a2028fd68b10da9c3a7958a19880db1c7c50f946
kjuq <44694396+kjuq@users.noreply.github.com>
2024-12-07 23:00:53 +0900

    add: gitlinker

---
+++ .config/nvim/lua/plugins/gitlinker.lua
@@ -0,0 +1,14 @@
+---@type LazySpec
+local spec = { 'linrongbin16/gitlinker.nvim' }
+
+spec.cmd = 'GitLink'
+
+local map = require('utils.lazy').generate_map('', 'Gitlinker: ')
+spec.keys = {
+	map('<Space>gy', { 'n', 'x' }, '<Cmd>GitLink<CR>', 'Yank git link'),
+	map('<Space>gY', { 'n', 'x' }, '<Cmd>GitLink!<CR>', 'Open git link'),
+}
+
+spec.opts = {}
+
+return spec
