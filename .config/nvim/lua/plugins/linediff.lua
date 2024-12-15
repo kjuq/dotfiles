192843df59f7268ae15fde522b5312538aaf5538
kjuq <44694396+kjuq@users.noreply.github.com>
2024-12-16 03:11:05 +0900

    mod: minor

--- .config/nvim/lua/plugins/linediff.lua
+++ .config/nvim/lua/plugins/linediff.lua
@@ -5,11 +5,11 @@ local spec = { 'AndrewRadev/linediff.vim' }
 spec.cmd = { 'Linediff' }
 
 spec.keys = {
-	map('f', 'x', function()
+	map('d', 'x', function()
 		vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('<esc>', true, false, true), 'x', false)
 		vim.cmd("'<,'>Linediff")
 	end, 'Set range'),
-	map('f', 'n', function()
+	map('d', 'n', function()
 		vim.cmd('LinediffReset')
 	end, 'Linediff Reset current selection'),
 }
