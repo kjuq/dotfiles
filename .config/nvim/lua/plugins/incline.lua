256fba6ff07efcd5d7604d955758941a03536f4f
kjuq <44694396+kjuq@users.noreply.github.com>
2024-12-18 02:18:49 +0900

    add: incline.nvim

---
+++ .config/nvim/lua/plugins/incline.lua
@@ -0,0 +1,32 @@
+---@type LazySpec
+local spec = { 'b0o/incline.nvim' }
+spec.event = 'VeryLazy'
+
+spec.opts = {
+	highlight = {
+		groups = {
+			InclineNormal = {
+				default = true,
+				group = 'Error',
+			},
+			InclineNormalNC = {
+				default = true,
+				group = 'Error',
+			},
+		},
+	},
+	window = {
+		margin = {
+			horizontal = 0,
+			vertical = 0,
+		},
+		overlap = {
+			borders = true,
+			statusline = false,
+			tabline = false,
+			winbar = true,
+		},
+	},
+}
+
+return spec
