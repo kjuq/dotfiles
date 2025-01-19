9ef4fa07729d7a6f4f20c40e9d655036b548f076
kjuq <44694396+kjuq@users.noreply.github.com>
2025-01-19 21:15:41 +0900

    add: codeaction on save in Ruff

--- .config/nvim/lua/plugins/lsp/ruff.lua
+++ .config/nvim/lua/plugins/lsp/ruff.lua
@@ -3,6 +3,24 @@ local M = {}
 M.opts = {
 	on_attach = function(client, _)
 		client.server_capabilities.hoverProvider = false
+		vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+			group = vim.api.nvim_create_augroup('kjuq_ruff_codeaction_onsave', {}),
+			callback = function()
+				vim.lsp.buf.code_action({
+					context = {
+						diagnostics = {},
+						only = { 'source.fixAll' },
+					},
+					filter = function(action)
+						if action.kind and action.kind == 'source.fixAll.ruff' then
+							return true
+						end
+						return false
+					end,
+					apply = true,
+				})
+			end,
+		})
 	end,
 }
 
