5091cfa635090d6048ddbacb105d5b7c5fb4716b
kjuq <44694396+kjuq@users.noreply.github.com>
2025-02-04 14:03:39 +0900

    mod: minor

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,19 +4,19 @@ if [ -z $_KJUQ_FISH_CONFIG_LOADED ]
 	source "$__fish_config_dir/configs/path.fish"
 end
 
-# set --local localinitpre "$__fish_config_dir/configs/local/initpre.fish"
-# if [ -e "$localinitpre" ]
-# 	source "$localinitpre"
-# end
+set --local localinitpre "$__fish_config_dir/configs/local/initpre.fish"
+if [ -e "$localinitpre" ]
+	source "$localinitpre"
+end
 
 source "$__fish_config_dir/configs/alias.fish"
 source "$__fish_config_dir/configs/function.fish"
 source "$__fish_config_dir/configs/misc.fish"
 source "$__fish_config_dir/configs/option.fish"
 
-# set --local localinitpost "$__fish_config_dir/configs/local/initpost.fish"
-# if [ -e "$localinitpost" ]
-# 	source "$localinitpost"
-# end
+set --local localinitpost "$__fish_config_dir/configs/local/initpost.fish"
+if [ -e "$localinitpost" ]
+	source "$localinitpost"
+end
 
 set --export _KJUQ_FISH_CONFIG_LOADED 1
