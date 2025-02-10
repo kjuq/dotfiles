152107a7d2c07595891f47b01864e508f5c04432
kjuq <44694396+kjuq@users.noreply.github.com>
2025-02-10 21:52:22 +0900

    mod: keymaps of fzf-lua

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -25,14 +25,63 @@ spec.keys = {
 	map('r', 'n', '<Cmd>FzfLua resume<CR>', 'Resume last search'),
 }
 
-spec.opts = {
-	winopts = {
-		border = require('kjuq.utils.common').floatwinborder,
-	},
-	grep = {
-		rg_opts = '--hidden --glob "!.git/*" --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
-	},
-}
+spec.opts = function()
+	local actions = require('fzf-lua.actions')
+	return {
+		winopts = {
+			border = require('kjuq.utils.common').floatwinborder,
+		},
+		grep = {
+			rg_opts = '--hidden --glob "!.git/*" --column --line-number --no-heading --color=always --smart-case --max-columns=4096 -e',
+		},
+		keymap = {
+			builtin = {
+				['<Esc>'] = 'hide', -- hide fzf-lua, `:FzfLua resume` to continue
+				['<F1>'] = 'toggle-help',
+				['<F11>'] = 'toggle-fullscreen',
+
+				['<F2>'] = 'toggle-preview-wrap', -- Only valid with the 'builtin' previewer
+				['<F3>'] = 'toggle-preview-ccw',
+				['<F5>'] = 'preview-reset',
+				['<F6>'] = 'toggle-preview',
+
+				['<F7>'] = 'toggle-preview-ts-ctx', -- `ts-ctx` binds require `nvim-treesitter-context`
+				['<F8>'] = 'preview-ts-ctx-dec',
+				['<F9>'] = 'preview-ts-ctx-inc',
+				[require('kjuq.utils.common').floatscrolldown] = 'preview-down',
+				[require('kjuq.utils.common').floatscrollup] = 'preview-up',
+			},
+			fzf = {
+				-- fzf '--bind=' options
+				['ctrl-z'] = 'abort',
+				['ctrl-u'] = 'unix-line-discard',
+				['ctrl-f'] = 'forward-char',
+				['ctrl-b'] = 'backward-char',
+				['ctrl-a'] = 'beginning-of-line',
+				['ctrl-e'] = 'end-of-line',
+				['alt-a'] = 'toggle-all',
+				['alt-g'] = 'first',
+				['alt-G'] = 'last',
+				-- Only valid with fzf previewers (bat/cat/git/etc)
+				['f2'] = 'toggle-preview-wrap',
+				['f6'] = 'toggle-preview',
+				['alt-f'] = 'preview-page-down',
+				['alt-b'] = 'preview-page-up',
+			},
+		},
+		actions = {
+			files = {
+				['enter'] = actions.file_edit_or_qf,
+				['ctrl-s'] = actions.file_split,
+				['ctrl-v'] = actions.file_vsplit,
+				['alt-q'] = actions.file_sel_to_qf,
+				['alt-h'] = { fn = actions.toggle_hidden, reuse = true, header = false },
+				['alt-i'] = { fn = actions.toggle_ignore, reuse = true, header = false }, -- respect ".gitignore"
+				['alt-l'] = { fn = actions.toggle_follow, reuse = true, header = false }, -- follow symlink
+			},
+		},
+	}
+end
 
 spec.specs = {
 	'nvim-tree/nvim-web-devicons',
