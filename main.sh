94df10d93511a93cdf421e2688c4f3ef0ced9e41
kjuq <44694396+kjuq@users.noreply.github.com>
2025-02-11 00:58:27 +0900

    mod: renew main.sh

--- main.sh
+++ main.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -eu
+
 # check if XDG_CONFIG_HOME is set
 if [ -z "$XDG_CONFIG_HOME" ]; then
 	echo "XDG_CONFIG_HOME is not set. Quit." 1>&2
@@ -8,182 +10,67 @@ fi
 
 script_dir="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
-xdg_config_without_prefix="${XDG_CONFIG_HOME/$HOME\//}"
-script_xdg_config="$script_dir/$xdg_config_without_prefix"
-
-source "$script_dir/targets.sh"
-
 backup() {
-	target_file="$1"
-	dirtype="$2"
-
-	# Set a source path and a destination path based on a second argument
-	if [ "$dirtype" == "xdg_config" ]; then
-		src="$XDG_CONFIG_HOME/$target_file"
-		dst="$script_xdg_config/$target_file"
-		parent_dir="$script_xdg_config/$(dirname "$target_file")"
-	elif [ "$dirtype" == "root" ]; then
-		src="$HOME/$target_file"
-		dst="$script_dir/$target_file"
-		parent_dir="$(dirname "$target_file")"
-	else
-		echo "XDG option is invalid." 1>&2
-		echo "Target: $target_file" 1>&2
-		exit 1
-	fi
-
-	if [ -L "$src" ]; then
-		return 0
-	fi
-
-	echo "Backing up: $src"
-
-	# Create directories including parent ones
-	if ! [ -d "$parent_dir" ]; then
-		mkdir --parents "$parent_dir"
-	fi
-
-	# Copy directory or file
-	if [ -d "$src" ]; then
-		cp -r "$src" "$(dirname "$dst")"
-	else
-		cp "$src" "$dst"
-	fi
+	while read -r line; do
+		local="$(eval echo "$line")"
+		remote="${local//$HOME/$script_dir}"
+		mkdir --parents "$(dirname remote)"
+		echo "$local" "$remote"
+	done <"$script_dir"/targets.txt
 }
 
 symlink() {
-	target_file="$1"
-	dirtype="$2"
-
-	# Set a source path and a destination path based on a second argument
-	if [ "$dirtype" == "xdg_config" ]; then
-		src="$script_xdg_config/$target_file"
-		dst="$XDG_CONFIG_HOME/$target_file"
-		parent_dir="$XDG_CONFIG_HOME/$(dirname "$target_file")"
-	elif [ "$dirtype" == "root" ]; then
-		src="$script_dir/$target_file"
-		dst="$HOME/$target_file"
-		parent_dir="$HOME/$(dirname "$target_file")"
-	else
-		echo "XDG option is invalid." 1>&2
-		echo "Target: $target_file" 1>&2
-		exit 1
-	fi
-
-	echo "Creating Symlink: $src"
-
-	# Create directories including parent ones
-	if ! [ -d "$parent_dir" ]; then
-		mkdir --parents "$parent_dir"
-	fi
-
-	# Create a symlink
-	# Delete a pre-exist link
-	if [ -L "$dst" ]; then
-		return 0
-	fi
-	# Delete a pre-exist dir/file
-	if [ -e "$dst" ]; then
-		read -r -p "$dst already exists. Delete it? (y/N) " INPUT
-		if [ "$INPUT" == "y" ] || [ "$INPUT" == "Y" ]; then
-			if [ -d "$dst" ]; then
-				rm -r "$dst"
-			elif [ -f "$dst" ]; then
-				rm "$dst"
-			else
-				echo "Unknown Filetype Error: when deleting $dst" 1>&2
-				exit 1
-			fi
-		else
-			return 0
-		fi
-	fi
-	ln --symbolic "$src" "$dst"
+	while read -r line; do
+		local="$(eval echo "$line")"
+		remote="${local//$HOME/$script_dir}"
+		mkdir --parents "$(dirname local)"
+		echo "$remote" "$local"
+	done <"$script_dir"/targets.txt
 }
 
-unsymlink() {
-	false
-	# TODO: remove symlinks and restore real files
-}
-
-link_other() {
-	src="$1"
-	dst="$2"
-
-	if [ -L "$dst" ]; then
-		return 0
-	fi
-
-	echo "Symlinking: $dst"
-
-	ln -s "$src" "$dst"
-}
-
-unlink_other() {
-	false
-	# TODO
+unlink() {
+	while read -r line; do
+		local="$(eval echo "$line")"
+		echo "unlink" "$local"
+	done <"$script_dir"/targets.txt
 }
 
 linux_etc() {
+	if [ ! "$(uname)" == "Linux" ]; then
+		echo 'This machine is not Linux' 1>&2
+		exit 1
+	fi
 	link_other "$XDG_CONFIG_HOME/alacritty/linux.toml.bak" "$XDG_CONFIG_HOME/alacritty/linux.toml"
 }
 
 macos_etc() {
+	if [ ! "$(uname)" == "Darwin" ]; then
+		echo 'This machine is not MacOS' 1>&2
+		exit 1
+	fi
 	link_other "$XDG_CONFIG_HOME/alacritty/macos.toml.bak" "$XDG_CONFIG_HOME/alacritty/macos.toml"
 	link_other "$XDG_CONFIG_HOME/qutebrowser/config.py" "$HOME/.qutebrowser/config.py"
 	link_other "$XDG_CONFIG_HOME/clangd/config.yaml" "$HOME/Library/Preferences/clangd/config.yaml"
-
-	# These 3 lines need reboot to apply
+	defaults write org.hammerspoon.Hammerspoon MJConfigFile "$XDG_CONFIG_HOME/hammerspoon/init.lua"
+	# These 3 lines need a reboot to apply
 	defaults write -g KeyRepeat -int 2         # MacOS's minimum is 2 (30 ms)
 	defaults write -g InitialKeyRepeat -int 15 # MacOS's minimum is 15 (225 ms)
 	defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-	defaults write org.hammerspoon.Hammerspoon MJConfigFile "$XDG_CONFIG_HOME/hammerspoon/init.lua"
 }
 
-main() {
-	if [ "$1" == "backup" ] || [ "$1" == "b" ]; then
-		action="backup"
-	elif [ "$1" == "symlink" ] || [ "$1" == "s" ]; then
-		action="symlink"
-	else
-		echo "Unknown or no argument was given. Quit."
-		exit 1
-	fi
+# ------------------------- main ------------------------- #
 
-	IFS_BAK=$IFS
-	IFS=$'\n'
-	for f in $xdg_config_files; do
-		IFS=$IFS_BAK
-		if ! [ "$f" == "" ]; then
-			if [ "$action" == "backup" ]; then
-				backup "$f" "xdg_config"
-			elif [ "$action" == "symlink" ]; then
-				symlink "$f" "xdg_config"
-			fi
-		fi
-	done
-
-	IFS_BAK=$IFS
-	IFS=$'\n'
-	for f in $root; do
-		IFS=$IFS_BAK
-		if ! [ "$f" == "" ]; then
-			if [ "$action" == "backup" ]; then
-				backup "$f" "root"
-			elif [ "$action" == "symlink" ]; then
-				symlink "$f" "root"
-			fi
-		fi
-	done
-
-	if [ "$(uname)" == "Linux" ]; then
-		linux_etc
-	fi
-
-	if [ "$(uname)" == "Darwin" ]; then
-		macos_etc
-	fi
-}
-
-main "$1"
+if [ "$1" == "backup" ] || [ "$1" == "b" ]; then
+	backup
+elif [ "$1" == "symlink" ] || [ "$1" == "s" ]; then
+	symlink
+elif [ "$1" == "unlink" ] || [ "$1" == "u" ]; then
+	unlink
+elif [ "$1" == "linux" ]; then
+	linux_etc
+elif [ "$1" == "macos" ]; then
+	macos_etc
+else
+	echo "Unknown or no argument was given. Quit."
+	exit 1
+fi
