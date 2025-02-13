e10db8679805b6019cf8f239c43b27e423aef97c
kjuq <44694396+kjuq@users.noreply.github.com>
2025-02-14 00:58:46 +0900

    fix: async tinymist preview

--- .config/nvim/lua/plugins/lsp/tinymist.lua
+++ .config/nvim/lua/plugins/lsp/tinymist.lua
@@ -2,12 +2,17 @@ local M = {}
 
 M.opts = {
 	on_attach = function()
+		local open = vim.fn.has('unix') == 1 and 'xdg-open' or 'open'
 		vim.keymap.set('n', '<Space>ap', function()
 			local dir = vim.fn.expand('%:p:h')
 			local filename = vim.fn.expand('%:t'):gsub([[.[^.]*$]], '.pdf')
 			local path = vim.fs.joinpath('/tmp/typst_output/', dir, filename)
-			local cmd = string.format('silent !open %s', path)
-			vim.cmd(cmd)
+
+			vim.system({ open, path }, { text = true }, function(job)
+				if job.code ~= 0 then
+					error(('code: %d, stderr: %s'):format(job.code, job.stderr))
+				end
+			end)
 		end, { buffer = true })
 	end,
 	settings = {
