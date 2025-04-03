5a6b1d40a297fa19bcf9031a46156c9a255d8632
kjuq <44694396+kjuq@users.noreply.github.com>
2025-04-03 12:13:09 +0900

    mod: use clang-format and uncrustify simultaneously

--- .config/nvim/lsp/efm.lua
+++ .config/nvim/lsp/efm.lua
@@ -1,51 +1,78 @@
-local settings = {
-	lintDebounce = '0.1s',
-	formatDebounce = '1s',
-	languages = {
-		lua = {
-			{
-				rootMarkers = { 'stylua.toml', '.stylua.toml' },
-				formatCanRange = true,
-				formatCommand = 'stylua --search-parent-directories ${--range-start=charStart} ${--range-end=charEnd} -',
-				formatStdin = true,
-			},
-			{
-				lintSource = 'efm/luacheck',
-				lintCommand = 'luacheck --codes --no-color --globals vim -- -',
-				lintIgnoreExitCode = true,
-				lintStdin = true,
-				lintAfterOpen = true,
-				lintFormats = { '%.%#:%l:%c: (%t%n) %m' },
-				rootMarkers = { '.luacheckrc' },
-			},
+local lua = {
+	{
+		rootMarkers = { 'stylua.toml', '.stylua.toml' },
+		formatCanRange = true,
+		formatCommand = 'stylua --search-parent-directories ${--range-start=charStart} ${--range-end=charEnd} -',
+		formatStdin = true,
+	},
+	{
+		lintSource = 'efm/luacheck',
+		lintCommand = 'luacheck --codes --no-color --globals vim -- -',
+		lintIgnoreExitCode = true,
+		lintStdin = true,
+		lintAfterOpen = true,
+		lintFormats = { '%.%#:%l:%c: (%t%n) %m' },
+		rootMarkers = { '.luacheckrc' },
+	},
+}
+local c = {
+	{
+		-- range-formatting is not supported probably
+		formatCommand = 'uncrustify -c "$UNCRUSTIFY_CONFIG" -q --assume "${INPUT}"',
+		formatStdin = true,
+		formatCanRange = false,
+	},
+	{
+		-- TODO: range using `--lines`
+		formatCommand = 'clang-format --assume-filename="${INPUT}" --fallback-style=none -',
+		formatStdin = true,
+		formatCanRange = true,
+	},
+}
+local python = {
+	{
+		lintSource = 'efm/mypy',
+		lintCommand = 'mypy --show-column-numbers --strict --strict-equality --shadow-file ${INPUT} <(cat) ${INPUT}',
+		lintAfterOpen = true,
+		lintStdin = true,
+		lintFormats = {
+			'%.%#:%l:%c: %trror: %m',
+			'%.%#:%l:%c: %tarning: %m',
+			'%.%#:%l:%c: %tote: %m',
 		},
-		sh = {
-			{
-				formatCommand = 'shfmt -',
-				formatStdin = true,
-				formatCanRange = false,
-			},
-		},
-		python = {
-			{
-				lintSource = 'efm/mypy',
-				lintCommand = 'mypy --show-column-numbers --strict --strict-equality --shadow-file ${INPUT} <(cat) ${INPUT}',
-				lintAfterOpen = true,
-				lintStdin = true,
-				lintFormats = {
-					'%.%#:%l:%c: %trror: %m',
-					'%.%#:%l:%c: %tarning: %m',
-					'%.%#:%l:%c: %tote: %m',
-				},
-				rootMarkers = {
-					'mypy.ini',
-					'pyproject.toml',
-					'setup.cfg',
-				},
-			},
+		rootMarkers = {
+			'mypy.ini',
+			'pyproject.toml',
+			'setup.cfg',
 		},
 	},
 }
+
+local sh = {
+	{
+		formatCommand = 'shfmt -',
+		formatStdin = true,
+		formatCanRange = false,
+	},
+}
+
+local settings = {
+	lintDebounce = '0.5s',
+	formatDebounce = '0.5s',
+	languages = {
+		lua = lua,
+		sh = sh,
+		python = python,
+		c = c,
+		cpp = c,
+	},
+}
+
+---@type table<string>
+local langs = {}
+for lang, _ in pairs(settings.languages) do
+	langs[#langs + 1] = lang
+end
 
 return {
 	cmd = { 'efm-langserver' },
@@ -57,5 +84,5 @@ return {
 		-- codeAction = true,
 	},
 	settings = settings,
-	filetypes = { 'lua', 'python', 'go' },
+	filetypes = langs,
 }
