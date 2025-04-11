b460aa2f87fe405ab609004ce2974597530d4282
kjuq <44694396+kjuq@users.noreply.github.com>
2025-04-11 16:24:27 +0900

    mod: snippet for shebang

--- .config/nvim/lua/kjuq/snippet/ft/all.lua
+++ .config/nvim/lua/kjuq/snippet/ft/all.lua
@@ -1,11 +1,13 @@
 local allsnp = require('kjuq.snippet.module').snippets.new()
 
-allsnp:add('@date', function()
+allsnp:add('^date', function()
 	return os.date('%Y-%m-%d') --[[@ as string]]
 end)
 
-allsnp:add('@time', function()
+allsnp:add('^time', function()
 	return os.date('%H:%M:%S') --[[@ as string]]
 end)
+
+allsnp:add('shebang', [[#!/usr/bin/env ${0}]])
 
 return allsnp
--- .config/nvim/lua/kjuq/snippet/ft/bash.lua
+++ .config/nvim/lua/kjuq/snippet/ft/bash.lua
@@ -1,6 +1,5 @@
 local shsnp = require('kjuq.snippet.module').snippets.new()
 
-shsnp:add('shebang', [[#!/usr/bin/env bash]])
 -- shsnp:add('scriptdir', [[script_dir="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"]])
 shsnp:add('scriptdir', [[script_dir="\$( cd -- "\$( dirname -- "\${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"]])
 shsnp:add('scriptname', [[script_name="$(basename "$0")"]])
