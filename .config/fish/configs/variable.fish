ac883ddeeea0583604c4c3c91a9170169e5b9096
kjuq <44694396+kjuq@users.noreply.github.com>
2025-04-28 11:45:04 +0900

    add: ssh-agent

--- .config/fish/configs/variable.fish
+++ .config/fish/configs/variable.fish
@@ -120,3 +120,8 @@ set --export QT_QPA_PLATFORMTHEME "qt5ct"
 if set --query XDG_RUNTIME_DIR
 	set --export XAUTHORITY "$XDG_RUNTIME_DIR/Xauthority"
 end
+
+# SSH-agent
+if command --search --quiet ssh-agent
+	eval "$(ssh-agent -c)"
+end
