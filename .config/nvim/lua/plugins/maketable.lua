0b9d03dd6401bf7bab0e5d782892541e5ebdfcb1
kjuq <44694396+kjuq@users.noreply.github.com>
2025-05-02 19:25:21 +0900

    fix: operator for maketable

--- .config/nvim/lua/plugins/maketable.lua
+++ .config/nvim/lua/plugins/maketable.lua
@@ -8,7 +8,7 @@ spec.cmd = {
 
 local map = require('kjuq.utils.lazy').generate_map('', 'MakeTable: ')
 spec.keys = {
-	map('<Space>ct', 'n', [[m'<Cmd>lua vim.o.operatorfunc='v:lua.Kjuq_maketable'<CR>g@]], 'Make'),
+	map('<Space>ct', 'n', [[m'<Cmd>lua vim.o.operatorfunc='v:lua._G.kjuq_maketable'<CR>g@]], 'Make'),
 	map('<Space>ct', 'x', ':MakeTable<CR>', 'Make'),
 	map('<Space>cT', 'n', '<Cmd>UnmakeTable<CR>', 'Unmake'),
 }
