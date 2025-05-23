3e82846ad3dfe8a1e711c422f175b5135ea54996
kjuq <44694396+kjuq@users.noreply.github.com>
2025-05-23 17:41:44 +0900

    del: not override vim.notify

--- .config/nvim/lua/kjuq/rc/experimental.lua
+++ .config/nvim/lua/kjuq/rc/experimental.lua
@@ -8,12 +8,3 @@ require('vim._extui').enable({
 		},
 	},
 })
-
-vim.notify = function(msg, level, opts)
-	local hl_group = opts and opts.hl_group or nil
-	if not hl_group then
-		hl_group = level == vim.log.levels.WARN and 'WarningMsg' or nil
-	end
-	local chunks = { { msg, hl_group } }
-	vim.api.nvim_echo(chunks, true, { err = level == vim.log.levels.ERROR })
-end
