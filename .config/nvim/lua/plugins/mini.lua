a99d2445f80bf4aceb16e5c296943fc9619fc39b
kjuq <44694396+kjuq@users.noreply.github.com>
2025-07-29 04:58:49 +0900

    add: mini.nvim for aligning

---
+++ .config/nvim/lua/plugins/mini.lua
@@ -0,0 +1,19 @@
+---@type LazySpec
+local spec = { 'https://github.com/echasnovski/mini.nvim' }
+
+spec.keys = {
+	{ 'ga', mode = { 'n', 'x' } }, -- for align
+	{ 'gA', mode = { 'n', 'x' } }, -- for align
+}
+
+spec.config = function()
+	require('mini.align').setup({
+		-- Module mappings. Use `''` (empty string) to disable one.
+		mappings = {
+			start = 'ga',
+			start_with_preview = 'gA',
+		},
+	})
+end
+
+return spec
