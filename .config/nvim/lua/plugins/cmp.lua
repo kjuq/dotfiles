58df2bb20252de1ced7a9e8d126a6dfae1d3a1b2
kjuq <44694396+kjuq@users.noreply.github.com>
2025-07-30 01:53:00 +0900

    mod: minor

--- .config/nvim/lua/plugins/cmp.lua
+++ .config/nvim/lua/plugins/cmp.lua
@@ -163,12 +163,11 @@ spec.config = function()
 			completion = cmp.config.window.bordered({ border = vim.o.winborder }),
 			documentation = cmp.config.window.bordered({ border = vim.o.winborder }),
 		},
-		---@diagnostic disable-next-line: missing-fields
 		formatting = {
 			format = require('lspkind').cmp_format({
 				mode = 'text_symbol', -- show only symbol annotations
-				maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
-				ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+				maxwidth = 50, -- prevent the popup from showing more than provided characters (50 will not show more than 50 chars)
+				ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead
 				symbol_map = { Copilot = '' },
 			}),
 		},
