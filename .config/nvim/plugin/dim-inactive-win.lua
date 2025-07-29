396ede65d6f05c482b8e534cdc88f4edd0298d27
kjuq <44694396+kjuq@users.noreply.github.com>
2025-07-30 02:21:43 +0900

    mod: disable tint.nvim

--- .config/nvim/plugin/dim-inactive-win.lua
+++ .config/nvim/plugin/dim-inactive-win.lua
@@ -1,13 +1,13 @@
 -- Dim inactive windows
 
-vim.cmd('highlight default DimInactiveWindows guifg=#666666')
+vim.api.nvim_set_hl(0, 'kjuq_inactive_windows', { fg = 'Gray' })
 
 -- When leaving a window, set all highlight groups to a "dimmed" hl_group
 vim.api.nvim_create_autocmd({ 'WinLeave', 'FocusLost' }, {
 	callback = function()
 		local highlights = {}
 		for hl, _ in pairs(vim.api.nvim_get_hl(0, {})) do
-			table.insert(highlights, hl .. ':DimInactiveWindows')
+			table.insert(highlights, hl .. ':kjuq_inactive_windows')
 		end
 		vim.wo.winhighlight = table.concat(highlights, ',')
 	end,
