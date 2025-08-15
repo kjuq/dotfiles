f39b07f8d48a9a6f24dc99aa6c639ab1c9fa08c6
kjuq <44694396+kjuq@users.noreply.github.com>
2025-08-16 01:45:15 +0900

    add: pluginpre.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,13 +5,20 @@ end
 
 vim.loader.enable()
 
-local myvimrc = os.getenv('MYVIMRC')
-local luahome = myvimrc and vim.fs.joinpath(vim.fn.fnamemodify(myvimrc, ':p:h'), 'lua') or nil -- mostly `$XDG_CONFIG_HOME/nvim/lua`
-local localrc = vim.fs.joinpath(luahome, 'kjuq', 'rc', 'local')
-
-if luahome and vim.uv.fs_stat(vim.fs.joinpath(localrc, 'initpre.lua')) then
-	require('kjuq.rc.local.initpre')
+local function loadlocalconf(name)
+	local myvimrc = os.getenv('MYVIMRC')
+	if not myvimrc then
+		vim.notify("'MYVIMRC' is not set", vim.log.levels.ERROR)
+		return
+	end
+	local path = vim.fs.joinpath(vim.fn.fnamemodify(myvimrc, ':p:h'), 'lua', 'kjuq', 'rc', 'local', name .. '.lua')
+	if not vim.uv.fs_stat(path) then
+		return -- skip if the local conf file does not exist
+	end
+	require('kjuq.rc.local.' .. name)
 end
+
+loadlocalconf('initpre')
 
 require('kjuq.rc.general')
 require('kjuq.rc.options')
@@ -21,11 +28,10 @@ require('kjuq.rc.mouse')
 require('kjuq.rc.colorscheme')
 require('kjuq.rc.experimental')
 require('kjuq.rc.internal_plugin')
-
 require('kjuq.rc.lsp')
+
+loadlocalconf('pluginpre')
 
 require('kjuq.rc.lazy')
 
-if luahome and vim.uv.fs_stat(vim.fs.joinpath(localrc, 'initpost.lua')) then
-	require('kjuq.rc.local.initpost')
-end
+loadlocalconf('initpost')
