11e106820634614394fe395c215aa0e28b89714c
kjuq <44694396+kjuq@users.noreply.github.com>
2025-08-15 23:42:17 +0900

    mod: minor (nvim-treesitter)

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -15,30 +15,31 @@ spec.opts = {
 spec.config = function(_, opts)
 	local ts = require('nvim-treesitter')
 	ts.setup(opts)
+	local callback = function(args)
+		local ft = vim.bo[args.buf].ft
+		local lang = vim.treesitter.language.get_lang(ft)
+		local installed = require('nvim-treesitter.config').get_installed
+		local function enable()
+			vim.treesitter.start(args.buf)
+			vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+		end
+		if not vim.tbl_contains(installed(), lang) then
+			ts.install({ lang }):await(function(err)
+				if err then
+					vim.notify('Treesitter install error\nft: ' .. ft .. '\nerr: ' .. err, vim.log.level.error)
+					return
+				end
+				if vim.tbl_contains(installed(), lang) then -- if `lang` was newly installed
+					enable()
+				end
+			end)
+			return
+		end
+		enable() -- if `lang` was already installed
+	end
 	vim.api.nvim_create_autocmd({ 'FileType' }, {
 		group = vim.api.nvim_create_augroup('kjuq_treesitter_start', {}),
-		callback = function(args)
-			local ft = vim.bo[args.buf].ft
-			local lang = vim.treesitter.language.get_lang(ft)
-			local installed = require('nvim-treesitter.config').get_installed
-			local function enable()
-				vim.treesitter.start(args.buf)
-				vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
-			end
-			if not vim.tbl_contains(installed(), lang) then
-				ts.install({ lang }):await(function(err)
-					if err then
-						vim.notify('Treesitter install error\nft: ' .. ft .. '\nerr: ' .. err, vim.log.level.error)
-						return
-					end
-					if vim.tbl_contains(installed(), lang) then -- if `lang` was newly installed
-						enable()
-					end
-				end)
-				return
-			end
-			enable() -- if `lang` was already installed
-		end,
+		callback = callback,
 	})
 	-- for lazy loading
 	vim.bo.filetype = vim.bo.filetype
