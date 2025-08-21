d8ec51544e21bd17b2b49ee05ea4121fb6737517
kjuq <44694396+kjuq@users.noreply.github.com>
2025-08-21 17:46:23 +0900

    mod: disable several plugins

--- .config/nvim/lua/plugins/help-vsplit.lua
+++
@@ -1,9 +0,0 @@
----@module 'lazy'
----@type LazySpec
-local spec = { 'https://github.com/anuvyklack/help-vsplit.nvim' }
-spec.keys = { { 'h', mode = 'c' } }
-spec.ft = { 'fzf', 'TelescopePrompt' }
-
-spec.opts = { side = 'right' }
-
-return spec
