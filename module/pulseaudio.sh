02c938e0f58bf6a91cf1bbd1b9f9f3883cb7a779
kjuq <44694396+kjuq@users.noreply.github.com>
2025-08-28 14:57:11 +0900

    mod: sudo in pulseaudio

--- module/pulseaudio.sh
+++ module/pulseaudio.sh
@@ -16,8 +16,6 @@ cookie_path="$XDG_CONFIG_HOME/pulse/cookie"
 conf_path="/etc/pulse/client.conf"
 line="cookie-file = $cookie_path"
 
-sudo=$([[ -n "${SUDO_ASKPASS}" ]] && echo "sudo -A" || echo "sudo")
-
 install() {
 	if grep "^$line$" $conf_path >/dev/null; then
 		# echo "'$line' is already set at '$conf_path'. Skipped."
@@ -25,7 +23,7 @@ install() {
 	fi
 
 	$sudo mkdir --parents "$(dirname conf_path)"
-	echo "$line" | $sudo tee --append $conf_path >/dev/null
+	echo "$line" | sudo tee --append $conf_path >/dev/null
 	echo "'$line' is successfully set at '$conf_path'"
 }
 
