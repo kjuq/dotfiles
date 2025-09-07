12dcd6716bc0e80b9f101dc594733fe3ac3fa4e8
kjuq <44694396+kjuq@users.noreply.github.com>
2025-09-07 20:26:48 +0900

    mod: c++ related

--- .config/nvim/lua/kjuq/snippet/ft/cpp.lua
+++ .config/nvim/lua/kjuq/snippet/ft/cpp.lua
@@ -1,8 +1,8 @@
 M = {}
 
 M.snippets = {
-	forauto = [[for (auto ${1:e} : ${2:elements}) {${0}}]],
-	rep = [[for (int ${1:i} : rep(${2:0}, ${3:10})) {${0}}]],
+	forauto = [[for (auto ${1} : ${2}) {${0}}]],
+	rep = [[for (int ${1} : rep(${2}, ${3})) {${0}}]],
 	cout = [[cout << ${0} << endl;]],
 	cin = [[cin >> ${0};]],
 }
