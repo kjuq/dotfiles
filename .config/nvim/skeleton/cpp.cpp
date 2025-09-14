51b90a7c68241448811e203f756cd8d8e30e5e5a
kjuq <44694396+kjuq@users.noreply.github.com>
2025-09-15 03:08:44 +0900

    mod: rep macro

--- .config/nvim/skeleton/cpp.cpp
+++ .config/nvim/skeleton/cpp.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-auto rep(int begin, int end) { return views::iota(begin, end); }
+auto rep(int end) { return(views::iota(0, end)); }
+auto seq(int begin, int end) { return(views::iota(begin, end + 1)); }
 
 signed main() {
 }
