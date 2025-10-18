e366fa5ab3d1feb452948dbec7966e7cc633f018
kjuq <44694396+kjuq@users.noreply.github.com>
2025-10-18 17:23:59 +0900

    mod: oil

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -15,38 +15,46 @@ spec.init = function()
 	vim.g.loaded_netrwPlugin = 1
 end
 
-spec.opts = function()
-	local hiddens = { '.DS_Store', '.git', '.gitmodules', 'node_modules' }
-
-	return {
-		delete_to_trash = true,
-		skip_confirm_for_simple_edits = true,
-		cleanup_delay_ms = 1000,
-		keymaps = {
-			['g?'] = 'actions.show_help',
-			['<CR>'] = 'actions.select',
-			['<C-s>'] = 'actions.select_vsplit',
-			['g<C-s>'] = 'actions.select_split',
-			['K'] = 'actions.preview',
-			['<C-Tab>'] = 'actions.close',
-			['<C-l>'] = 'actions.refresh',
-			['-'] = 'actions.parent',
-			['+'] = 'actions.open_cwd',
-			['_'] = 'actions.cd',
-			['`'] = 'actions.change_sort',
-			['gx'] = 'actions.open_external',
-			['g.'] = 'actions.toggle_hidden',
-			['g\\'] = 'actions.toggle_trash',
-		},
-		use_default_keymaps = false,
-		view_options = {
-			show_hidden = false,
-			is_always_hidden = function(name, _)
-				return vim.list_contains(hiddens, name)
-			end,
-		},
-	}
-end
+spec.opts = {
+	delete_to_trash = true,
+	skip_confirm_for_simple_edits = true,
+	cleanup_delay_ms = false,
+	constrain_cursor = 'editable', -- 'editable' | 'name' | false
+	columns = {
+		'icon',
+		-- "permissions",
+		-- 'size',
+		-- 'mtime',
+	},
+	buf_options = {
+		buflisted = true,
+		bufhidden = 'hide',
+	},
+	keymaps = {
+		['g?'] = 'actions.show_help',
+		['<CR>'] = 'actions.select',
+		['<C-s>'] = 'actions.select_vsplit',
+		['g<C-s>'] = 'actions.select_split',
+		['K'] = 'actions.preview',
+		-- ['<C-Tab>'] = 'actions.close',
+		['<C-l>'] = 'actions.refresh',
+		['-'] = 'actions.parent',
+		['+'] = 'actions.open_cwd',
+		['_'] = 'actions.cd',
+		['`'] = 'actions.change_sort',
+		['gx'] = 'actions.open_external',
+		['g.'] = 'actions.toggle_hidden',
+		['g\\'] = 'actions.toggle_trash',
+	},
+	use_default_keymaps = false,
+	view_options = {
+		show_hidden = false,
+		is_always_hidden = function(name, _)
+			local hiddens = { '.DS_Store', '.git', '.gitmodules', 'node_modules' }
+			return vim.list_contains(hiddens, name)
+		end,
+	},
+}
 
 spec.specs = { 'nvim-tree/nvim-web-devicons' }
 
