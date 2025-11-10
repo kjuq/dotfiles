e53ab169f6e0e1b716ea8990f891342a47e4e45e
kjuq <44694396+kjuq@users.noreply.github.com>
2025-11-11 01:58:17 +0900

    persistence -> mini.sessions

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -5,66 +5,83 @@ local spec = { 'https://github.com/echasnovski/mini.nvim' }
 spec.event = 'VeryLazy'
 
 spec.config = function()
-	require('mini.align').setup({
-		-- Module mappings. Use `''` (empty string) to disable one.
-		mappings = {
-			start = 'ga',
-			start_with_preview = 'gA',
-		},
-	})
-	require('mini.pick').setup({
-		delay = {
-			-- Delay between forcing asynchronous behavior
-			async = 1,
-			-- Delay between computation start and visual feedback about it
-			busy = 50,
-		},
-		mappings = {
-			caret_left = '<C-b>',
-			caret_right = '<C-f>',
+	do
+		require('mini.align').setup({
+			-- Module mappings. Use `''` (empty string) to disable one.
+			mappings = {
+				start = 'ga',
+				start_with_preview = 'gA',
+			},
+		})
+	end
+	do
+		require('mini.pick').setup({
+			delay = {
+				-- Delay between forcing asynchronous behavior
+				async = 1,
+				-- Delay between computation start and visual feedback about it
+				busy = 50,
+			},
+			mappings = {
+				caret_left = '<C-b>',
+				caret_right = '<C-f>',
 
-			choose = '<CR>',
-			choose_in_split = '<C-s>',
-			choose_in_tabpage = '',
-			choose_in_vsplit = '<C-v>',
-			choose_marked = '<M-CR>',
+				choose = '<CR>',
+				choose_in_split = '<C-s>',
+				choose_in_tabpage = '',
+				choose_in_vsplit = '<C-v>',
+				choose_marked = '<M-CR>',
 
-			delete_char = '<C-h>',
-			delete_char_right = '<C-d>',
-			delete_left = '<C-u>',
-			delete_word = '<C-w>',
+				delete_char = '<C-h>',
+				delete_char_right = '<C-d>',
+				delete_left = '<C-u>',
+				delete_word = '<C-w>',
 
-			mark = '<C-x>',
-			mark_all = '',
+				mark = '<C-x>',
+				mark_all = '',
 
-			move_down = '<C-n>',
-			move_start = '<C-g>',
-			move_up = '<C-p>',
+				move_down = '<C-n>',
+				move_start = '<C-g>',
+				move_up = '<C-p>',
 
-			paste = '<C-r>',
+				paste = '<C-r>',
 
-			refine = '<C-Space>',
-			refine_marked = '<M-Space>',
+				refine = '<C-Space>',
+				refine_marked = '<M-Space>',
 
-			scroll_down = '<M-n>',
-			scroll_left = '<M-b>',
-			scroll_right = '<M-f>',
-			scroll_up = '<M-p>',
+				scroll_down = '<M-n>',
+				scroll_left = '<M-b>',
+				scroll_right = '<M-f>',
+				scroll_up = '<M-p>',
 
-			stop = '<Esc>',
+				stop = '<Esc>',
 
-			toggle_info = '<S-Tab>',
-			toggle_preview = '<Tab>',
-		},
-	})
+				toggle_info = '<S-Tab>',
+				toggle_preview = '<Tab>',
+			},
+		})
+	end
+	do
+		require('mini.splitjoin').setup({
+			mappings = {
+				toggle = 'gS',
+				split = 'gS',
+				join = '',
+			},
+		})
+	end
+	do
+		require('mini.sessions').setup()
+		vim.keymap.set('n', '<Space>sl', '<Cmd>lua MiniSessions.read()<CR>', { desc = 'Mini.sessions: Read' })
+		vim.api.nvim_create_autocmd({ 'VimLeavePre' }, {
+			group = vim.api.nvim_create_augroup('kjuq_mini_sessions', {}),
+			callback = function()
+				MiniSessions.write('mini_sessions_main')
+			end,
+		})
+	end
+	require('mini.git').setup()
 	require('mini.icons').setup()
-	require('mini.splitjoin').setup({
-		mappings = {
-			toggle = 'gS',
-			split = 'gS',
-			join = '',
-		},
-	})
 end
 
 return spec
