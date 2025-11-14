0ae53e7fbaa17426b0cea44f52ab5cb1801a97e9
kjuq <44694396+kjuq@users.noreply.github.com>
2025-11-14 20:24:38 +0900

    Update autocd project

--- .config/nvim/plugin/autocd-project.lua
+++ .config/nvim/plugin/autocd-project.lua
@@ -1,15 +1,25 @@
 -- https://nanotipsforvim.prose.sh/automatically-set-the-cwd-without-rooter-plugin
 
+---@param bufnr? integer
+local function cdroot(bufnr)
+	vim.print(bufnr)
+	if not bufnr then
+		bufnr = 0
+	end
+	local patterns = {
+		'.git',
+		'Makefile',
+	}
+	local root = vim.fs.root(bufnr, patterns)
+	if root then
+		vim.fn.chdir(root)
+	end
+end
+vim.api.nvim_create_user_command('Cdroot', function()
+	cdroot(0)
+end, {})
+
 vim.api.nvim_create_autocmd({ 'BufEnter' }, {
 	group = vim.api.nvim_create_augroup('kjuq_autocd_project', {}),
-	callback = function(ctx)
-		local patterns = {
-			'.git',
-			'Makefile',
-		}
-		local root = vim.fs.root(ctx.buf, patterns)
-		if root then
-			vim.fn.chdir(root)
-		end
-	end,
+	command = 'Cdroot',
 })
