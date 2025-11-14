23a04740979e14a2c4cdcd33207387199aecbc3f
kjuq <44694396+kjuq@users.noreply.github.com>
2025-11-15 01:08:03 +0900

    Delete ftdetect/*

--- .config/nvim/ftdetect/snippets.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-	pattern = '*.snippets',
-	group = vim.api.nvim_create_augroup('kjuq_snippets', {}),
-	callback = function()
-		vim.opt_local.filetype = 'snippets'
-	end,
-})
--- .config/nvim/ftdetect/todotxt.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
-	pattern = { '[Tt]odo.txt', '*.[Tt]odo.txt', '[Dd]one.txt', '*.[Dd]one.txt' },
-	group = vim.api.nvim_create_augroup('kjuq_detectft_todotxt', {}),
-	callback = function()
-		vim.opt_local.filetype = 'todo'
-	end,
-})
