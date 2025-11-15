917a487a5f39e5280bf77704ee92bc9d09a1c61e
kjuq <44694396+kjuq@users.noreply.github.com>
2025-11-16 03:20:04 +0900

    Stop loading local conf

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -5,27 +5,8 @@ end
 
 vim.loader.enable()
 
-local function loadlocalconf(name)
-	local myvimrc = os.getenv('MYVIMRC')
-	if not myvimrc then
-		vim.notify("'MYVIMRC' is not set", vim.log.levels.ERROR)
-		return
-	end
-	local path = vim.fs.joinpath(vim.fn.fnamemodify(myvimrc, ':p:h'), 'lua', 'kjuq', 'rc', 'local', name .. '.lua')
-	if not vim.uv.fs_stat(path) then
-		return -- skip if the local conf file does not exist
-	end
-	require('kjuq.rc.local.' .. name)
-end
-
-loadlocalconf('initpre')
-
 require('kjuq.rc.options')
 require('kjuq.rc.keymaps')
 require('kjuq.rc.lsp')
 
-loadlocalconf('pluginpre')
-
 require('kjuq.rc.lazy')
-
-loadlocalconf('initpost')
