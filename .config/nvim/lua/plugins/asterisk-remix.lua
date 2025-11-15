98e6e35e9abcbf6ef581e8af5f5c317f1db71532
kjuq <44694396+kjuq@users.noreply.github.com>
2025-11-16 03:20:17 +0900

    Fix asterisk-remix

--- .config/nvim/lua/plugins/asterisk-remix.lua
+++ .config/nvim/lua/plugins/asterisk-remix.lua
@@ -1,19 +1,6 @@
 ---@module 'lazy'
 ---@type LazySpec
 local spec = { 'https://github.com/kjuq/nvim-asterisk-remix' }
-
-local map = require('kjuq.utils.lazy').generate_map('', 'Asterisk: ')
-spec.keys = {
-	-- stylua: ignore start
-	map('*', 'n', function() require('asterisk-remix').normal_search('*') end, '*'),
-	map('#', 'n', function() require('asterisk-remix').normal_search('#') end, '#'),
-	map('g*', 'n', function() require('asterisk-remix').normal_search('g*') end, 'g*'),
-	map('g#', 'n', function() require('asterisk-remix').normal_search('g#') end, 'g#'),
-	map('*', 'x', function() return require('asterisk-remix').visual_search('*') end, '*', { expr = true }),
-	map('#', 'x', function() return require('asterisk-remix').visual_search('#') end, '#', { expr = true }),
-	map('<Space>*', 'n', function() return require('asterisk-remix').operator_search('*') end, 'Ope *', { expr = true }),
-	map('<Space>#', 'n', function() return require('asterisk-remix').operator_search('#') end, 'Ope #', { expr = true }),
-	-- stylua: ignore end
-}
+spec.lazy = false
 
 return spec
