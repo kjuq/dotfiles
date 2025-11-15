d16f8196233726449f6f3145c691f6133f0aa7d3
kjuq <44694396+kjuq@users.noreply.github.com>
2025-11-16 03:19:51 +0900

    Move rm-multibyte and jp-spacing

---
+++ .config/nvim/lua/plugins/format-jp-doc.lua
@@ -0,0 +1,6 @@
+---@module 'lazy'
+---@type LazySpec
+local spec = { 'https://github.com/kjuq/nvim-format-jp-doc' }
+spec.lazy = false
+
+return spec
---
+++ .config/nvim/lua/plugins/rm-multibyte-letters.lua
@@ -0,0 +1,6 @@
+---@module 'lazy'
+---@type LazySpec
+local spec = { 'https://github.com/kjuq/nvim-rm-multibyte-letters' }
+spec.lazy = false
+
+return spec
