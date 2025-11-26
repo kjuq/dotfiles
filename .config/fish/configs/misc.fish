cbb74de85671a0affecc2dc71fcc7dc3820b0772
kjuq <44694396+kjuq@users.noreply.github.com>
2025-11-27 07:29:22 +0900

    Remove activatios of unnecessary external tool in fish

--- .config/fish/configs/misc.fish
+++ .config/fish/configs/misc.fish
@@ -1,20 +1,3 @@
-if command --search --quiet fzf_configure_bindings
-	fzf_configure_bindings --directory=\cs --variables=\ev
-end
-
-if command --search --quiet zoxide
-	zoxide init --cmd c fish | source
-end
-
-# if command --query --search mise
-# 	mise activate fish | source
-# end
-
-# if command --search --quiet direnv
-# 	direnv hook fish | source
-# 	set --export DIRENV_LOG_FORMAT ""
-# end
-#
-# if command --search --quiet mise
-# 	mise activate fish | source
+# if command --search --quiet zoxide
+# 	zoxide init --cmd c fish | source
 # end
