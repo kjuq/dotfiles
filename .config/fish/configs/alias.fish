0ebfa1c2c3e3e616f1d7b32169f67a74e8c23657
kjuq <44694396+kjuq@users.noreply.github.com>
2025-12-02 13:05:36 +0900

    Convert fish's functions into aliases

--- .config/fish/configs/alias.fish
+++ .config/fish/configs/alias.fish
@@ -1,75 +1,35 @@
-function ... --wraps='cd ../..'
-	cd ../..
-end
-function .... --wraps='cd ../../..'
-	cd ../../..
-end
-function ..... --wraps='cd ../../../..'
-	cd ../../../..
-end
-function ...... --wraps='cd ../../../../..'
-	cd ../../../../..
-end
-function ....... --wraps='cd ../../../../../..'
-	cd ../../../../../..
-end
-function ........ --wraps='cd ../../../../../../..'
-	cd ../../../../../../..
-end
+alias ...='../../..'
+alias ....='../../../..'
+alias .....='../../../..'
+alias ......='../../../../..'
+alias .......='../../../../../..'
 
-function la --wraps='ls --all'
-	ls --almost-all $argv
-end
-function lla --wraps='ll --all'
-	ll --almost-all $argv
-end
-function l1 --wraps='ls -1'
-	ls -1 $argv
-end
+alias la='ls --almost-all'
+alias lla='ll --almost-all'
 
 switch $(uname)
 case Linux
-	function ls
-		command ls --color=auto --classify --group-directories-first $argv
-	end
+	alias ls='ls --color=auto --classify --group-directories-first'
 case Darwin
-	function ls
-		ls -GF $argv
-	end
+	alias ls='ls -GF'
 end
 
 if command --search --quiet nvim
-	function Nvim --wraps='env KJUQ_NVIM_NO_EXT_PLUGINS=1 nvim'
-		env KJUQ_NVIM_NO_EXT_PLUGINS=1 nvim $argv
-	end
-	function nvimt --wraps='nvim +EditDashboard'
-		nvim +EditDashboard $argv
-	end
-	function nvimr --wraps='nvim +EditReponotes'
-		nvim +EditReponotes $argv
-	end
-	function nvimd --wraps='nvim +EditDailynote'
-		nvim +EditDailynote $argv
-	end
+	alias Nvim='env KJUQ_NVIM_NO_EXT_PLUGINS=1 nvim'
+	alias nvimt='nvim $KJUQ_DOCS/__index.md'
+	alias nvimd='nvim +EditReponotes'
+	alias nvimd='nvim +EditDailynote'
 end
 
 if command --search --quiet ghq
-	function ghf
-		set --local repo "$(ghq list | fzf)" && cd "$(ghq root)/$repo"
-	end
+	alias ghf='set --local repo "$(ghq list | fzf)" && cd "$(ghq root)/$repo"'
 end
 
 if command --search --quiet trash
-	function dl --wraps='trash -r'
-		command trash -r $argv
-	end
-	function rm --wraps="echo '`rm` is dangerous so use `dl` instead. (Given args:'"
-		command echo '`rm` is dangerous so use `dl` instead. (Given args:' $argv
-	end
+	alias dl='trash -r'
+	alias rm='echo "`rm` is dangerous so use `dl` instead."'
 end
 
 if command --search --quiet w3m
-	function w3m --wraps='env TERM=xterm-256color w3m'
-		env TERM=xterm-256color command w3m $argv
-	end
+	alias w3m='env TERM=xterm-256color command w3m'
 end
