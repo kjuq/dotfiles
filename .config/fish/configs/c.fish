d94706ec4269b6680fdabd45961ed1251164d6e4
kjuq <44694396+kjuq@users.noreply.github.com>
2025-12-05 08:14:38 +0900

    Fix c command

--- .config/fish/configs/c.fish
+++ .config/fish/configs/c.fish
@@ -1,5 +1,5 @@
 function c
-	set --export CDPATH '.' $cdpath
+	set --export CDPATH $bmpath
 	cd $argv
 	set --local success $status
 	set --erase CDPATH
@@ -7,25 +7,25 @@ function c
 end
 
 function cnew
-	if not contains $PWD $cdpath
-		set --universal --export --append cdpath "$PWD"
-		echo "$PWD is successfully added to cdpath"
+	if not contains $PWD $bmpath
+		set --universal --export --append bmpath "$PWD"
+		echo "$PWD is successfully added to bmpath"
 	else
-		echo "$PWD is already in cdpath"
+		echo "$PWD is already in bmpath"
 		return 1
 	end
 end
 
 function clist
-	echo "$cdpath"
+	echo "$bmpath"
 end
 
 function cdelete
-	if set --local idx $(contains -i $PWD $cdpath)
-		set --erase cdpath[$idx]
-		echo "$PWD is successfully removed from cdpath"
+	if set --local idx $(contains -i $PWD $bmpath)
+		set --erase bmpath[$idx]
+		echo "$PWD is successfully removed from bmpath"
 	else
-		echo 'pwd is not in cdpath'
+		echo 'pwd is not in bmpath'
 		return 1
 	end
 end
