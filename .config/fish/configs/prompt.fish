2c397425294c381598a59ca0045dfca9d767afa3
kjuq <44694396+kjuq@users.noreply.github.com>
2025-12-06 22:27:45 +0900

    Disable git prompt

--- .config/fish/configs/prompt.fish
+++ .config/fish/configs/prompt.fish
@@ -1,5 +1,5 @@
 set __fish_git_prompt_showdirtystate true
-set __fish_git_prompt_showuntrackedfiles true
+set __fish_git_prompt_showuntrackedfiles false
 set __fish_git_prompt_char_stateseparator ''
 
 function fish_prompt
@@ -24,7 +24,7 @@ function fish_prompt
 	echo -n -s \
 		$(set_color normal) $(dirs) \
 		$(set_color brblack) "$user_and_hostname" \
-		$(set_color brblack) "$(fish_vcs_prompt | tr -d '()')" \
+		# $(set_color brblack) "$(fish_vcs_prompt | tr -d '()')" \
 		" $prompt_status" \
 		\n \
 		$(set_color brblack) '$ '
