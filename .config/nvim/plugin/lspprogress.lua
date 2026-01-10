93f4bca3cfc5608aadc0cc74fca1e95b50f7337c
kjuq <44694396+kjuq@users.noreply.github.com>
2026-01-11 02:44:00 +0900

    Disable LspProgress

--- .config/nvim/plugin/lspprogress.lua
+++ .config/nvim/plugin/lspprogress.lua
@@ -10,20 +10,18 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	end,
 })
 
-vim.api.nvim_create_autocmd('LspProgress', {
-	callback = function(ev)
-		local value = ev.data.params.value
-		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-		local label = '[' .. client.name .. ']'
-
-		-- TODO: Show progress
-		-- local status = vim.lsp.status()
-		-- vim.api.nvim_echo({ { label .. ' ' .. status } }, true, {})
-
-		if value.kind == 'begin' then
-			vim.api.nvim_echo({ { label }, { ' ' }, { value.title }, { 'began' } }, true, {})
-		elseif value.kind == 'end' then
-			vim.api.nvim_echo({ { label }, { ' ' }, { value.title }, { ' ' }, { 'ended' } }, true, {})
-		end
-	end,
-})
+-- vim.api.nvim_create_autocmd('LspProgress', {
+-- 	callback = function(ev)
+-- 		local value = ev.data.params.value
+-- 		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+-- 		local label = '[' .. client.name .. ']'
+-- 		-- TODO: Show progress
+-- 		local status = vim.lsp.status()
+-- 		vim.api.nvim_echo({ { label .. ' ' .. status } }, true, {})
+-- 		if value.kind == 'begin' then
+-- 			vim.api.nvim_echo({ { label }, { ' ' }, { value.title }, { 'began' } }, true, {})
+-- 		elseif value.kind == 'end' then
+-- 			vim.api.nvim_echo({ { label }, { ' ' }, { value.title }, { ' ' }, { 'ended' } }, true, {})
+-- 		end
+-- 	end,
+-- })
