486aa2599759b02e1f4ea7ed4b8fdeba226749b6
kjuq <44694396+kjuq@users.noreply.github.com>
2026-01-11 00:35:41 +0900

    Update cmd-to-operator

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -329,25 +329,26 @@ local opts = {
 
 require('lazy').setup('plugins', opts) -- to load multiple dir https://zenn.dev/sisi0808/articles/36ff184554ddd6
 
-if not os.getenv('KJUQ_NVIM_LOAD_ALL_RUNTIME_PATH') then
-	return
-end
-local library = {
-	vim.fs.joinpath(vim.env.VIMRUNTIME, '/lua'),
-	vim.fs.joinpath(vim.fn.stdpath('config'), '/lua'),
-}
-for _, v in pairs(require('lazy.core.config').plugins) do
-	library[#library + 1] = vim.fs.joinpath(v.dir, '/lua')
-end
-vim.lsp.config.lua_ls = {
-	settings = {
-		Lua = {
-			workspace = {
-				library = library,
+if os.getenv('KJUQ_NVIM_LOAD_ALL_RUNTIME_PATH') then
+	local library = {
+		vim.fs.joinpath(vim.env.VIMRUNTIME, '/lua'),
+		vim.fs.joinpath(vim.fn.stdpath('config'), '/lua'),
+	}
+	for _, v in pairs(require('lazy.core.config').plugins) do
+		library[#library + 1] = vim.fs.joinpath(v.dir, '/lua')
+	end
+	vim.lsp.config.lua_ls = {
+		settings = {
+			Lua = {
+				workspace = {
+					library = library,
+				},
 			},
 		},
-	},
-}
+	}
+end
 -- }}}
+
+require('kjuq.cmd-to-operator').setup()
 
 -- vim: set foldmethod=marker :
--- .config/nvim/lua/kjuq/cmd-to-operator.lua
+++ .config/nvim/lua/kjuq/cmd-to-operator.lua
@@ -7,7 +7,7 @@ local M = {}
 
 ---@param cmd string
 ---@return fun():string?
-function M.operator(cmd)
+local function operator(cmd)
 	local name = cmd:gsub(' ', '_')
 	local opfunc = string.format('kjuq_%s', name)
 	_G[string.format('kjuq_%s', name)] = function()
@@ -30,16 +30,18 @@ function M.operator(cmd)
 end
 
 local function setup()
-	local sort = M.operator('sort')
+	local sort = operator('sort')
 	vim.keymap.set({ 'n', 'x' }, '<Space>cs', sort, { expr = true, desc = ':sort', silent = true })
-	local sort_n = M.operator('sort n')
+	local sort_n = operator('sort n')
 	vim.keymap.set({ 'n', 'x' }, '<Space>cS', sort_n, { expr = true, desc = ':sort n', silent = true })
-	local source = M.operator('source')
+	local source = operator('source')
 	vim.keymap.set({ 'n', 'x' }, '<Space>cx', source, { expr = true, desc = ':source', silent = true })
 	-- TODO: Add oneline option
 	vim.keymap.set('n', '<Space>cxx', '<Cmd>.source<CR>', { desc = '`:source` current line' })
 end
 
-setup()
+M = {
+	setup = setup,
+}
 
 return M
