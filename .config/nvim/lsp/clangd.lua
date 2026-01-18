162926a90386ff3b0e63380ae877b532f71e74fb
kjuq <44694396+kjuq@users.noreply.github.com>
2026-01-18 11:26:24 +0900

    ~/.clang-format is created only during formatting moment

--- .config/nvim/lsp/clangd.lua
+++ .config/nvim/lsp/clangd.lua
@@ -1,15 +1,44 @@
+-- DON'T install `clang-format` separately
+
+-- Create ~/.clang-format during formatting
 vim.api.nvim_create_autocmd({ 'LspAttach' }, {
 	group = vim.api.nvim_create_augroup('kjuq_lspattach_clangd', {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+	callback = function(parentargs)
+		local client = assert(vim.lsp.get_client_by_id(parentargs.data.client_id))
 		if client.name ~= 'clangd' then
 			return
 		end
-		local bufnr = args.buf
-		local groupname = string.format('kjuq_formatonsave_%s_buf_%d', client.name, bufnr)
-		vim.schedule(function()
-			vim.api.nvim_del_augroup_by_name(groupname)
-		end)
+		local bufnr = parentargs.buf
+		vim.api.nvim_create_autocmd({ 'LspRequest' }, {
+			group = vim.api.nvim_create_augroup('kjuq_clangd_tab_indent', {}),
+			callback = function(args)
+				local method = args.data.request.method
+				local type = args.data.request.type
+				local filepath = os.getenv('HOME') .. '/.clang-format'
+				if method == 'textDocument/formatting' and type == 'pending' then
+					local file = io.open(filepath, 'w')
+					if file then
+						file:write(table.concat({
+							'IndentWidth: 4',
+							'UseTab: ForIndentation',
+							'TabWidth: 4',
+							'ColumnLimit: 120',
+						}, '\n'))
+						file:close()
+					else
+						vim.notify('Failed to create ~/.clang-format', vim.log.levels.ERROR)
+					end
+				end
+				if method == 'textDocument/formatting' and type == 'complete' then
+					local file = io.open(filepath, 'r')
+					if file then
+						file:close()
+						os.remove(filepath)
+					end
+				end
+			end,
+			buffer = bufnr,
+		})
 	end,
 })
 
