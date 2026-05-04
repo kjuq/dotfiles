1e161685035d9e7b797cb3e85a08c44c2bc73c76
kjuq <44694396+kjuq@users.noreply.github.com>
2026-05-04 22:06:32 +0900

    substitute, surround, and telescope -> mini

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -20,25 +20,25 @@ spec.config = function()
 				-- Delay between forcing asynchronous behavior
 				async = 1,
 				-- Delay between computation start and visual feedback about it
-				busy = 50,
+				busy = 20,
 			},
 			mappings = {
-				caret_left = '<C-b>',
-				caret_right = '<C-f>',
+				caret_left = '<Left>',
+				caret_right = '<Right>',
 
 				choose = '<CR>',
 				choose_in_split = '<C-s>',
 				choose_in_tabpage = '',
 				choose_in_vsplit = '<C-v>',
-				choose_marked = '<M-CR>',
+				choose_marked = '<S-CR>',
 
-				delete_char = '<C-h>',
-				delete_char_right = '<C-d>',
+				delete_char = '<BS>',
+				delete_char_right = '<Del>',
 				delete_left = '<C-u>',
 				delete_word = '<C-w>',
 
 				mark = '<C-x>',
-				mark_all = '',
+				mark_all = '<C-a>',
 
 				move_down = '<C-n>',
 				move_start = '<C-g>',
@@ -46,13 +46,13 @@ spec.config = function()
 
 				paste = '<C-r>',
 
-				refine = '<C-Space>',
-				refine_marked = '<M-Space>',
+				refine = '<C-k ',
+				refine_marked = '<C-t>',
 
-				scroll_down = '<M-n>',
-				scroll_left = '<M-b>',
-				scroll_right = '<M-f>',
-				scroll_up = '<M-p>',
+				scroll_down = '<PageDown>',
+				scroll_left = '<C-Left>', -- <M-b>
+				scroll_right = '<C-Right>', -- <M-f>
+				scroll_up = '<PageUp>',
 
 				stop = '<Esc>',
 
@@ -60,6 +60,14 @@ spec.config = function()
 				toggle_preview = '<Tab>',
 			},
 		})
+		require('mini.extra').setup()
+		vim.keymap.set('n', '<Space>fg', '<Cmd>Pick grep_live<CR>', { desc = 'Mini: pick grep_live' })
+		vim.keymap.set('n', '<Space>fG', '<Cmd>Pick git_files<CR>', { desc = 'Mini: pick git_files' })
+		vim.keymap.set('n', '<Space>fi', '<Cmd>Pick help<CR>', { desc = 'Mini: Pick help tags' })
+		vim.keymap.set('n', '<Space>fb', '<Cmd>Pick buffers<CR>', { desc = 'Mini: Pick buffers' })
+		vim.keymap.set('n', '<Space>fh', '<Cmd>Pick oldfiles current_dir=true<CR>', { desc = 'Mini: MRU current dir' })
+		vim.keymap.set('n', '<Space>fH', '<Cmd>Pick oldfiles current_dir=false<CR>', { desc = 'Mini: MRU' })
+		vim.keymap.set('n', '<Space>fr', '<Cmd>Pick resume<CR>', { desc = 'Mini: Pick resume finding' })
 	end
 	do
 		require('mini.sessions').setup()
@@ -73,6 +81,17 @@ spec.config = function()
 	end
 	require('mini.surround').setup({
 		highlight_duration = require('kjuq.common_params').highlight_duration,
+		silent = true,
+	})
+	require('mini.operators').setup({
+		evaluate = { prefix = '' },
+		exchange = { prefix = '' },
+		multiply = { prefix = '' },
+		sort = { prefix = '' },
+		replace = { -- substitution for 'gbprod/substitute.nvim'
+			prefix = 'st',
+			reindent_linewise = true,
+		},
 	})
 	require('mini.git').setup()
 	require('mini.icons').setup()
