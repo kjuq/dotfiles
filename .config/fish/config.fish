502443aafdb7c3ad4470397f9c78adec64b1d425
kjuq <44694396+kjuq@users.noreply.github.com>
2026-05-07 10:59:45 +0900

    Fish uses fish/configs/local/*

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,8 +2,8 @@ if not status is-interactive
 	exit 1
 end
 
-if [ -e "$__fish_config_dir/configs/localpre.fish"  ]
-	source "$__fish_config_dir/configs/localpre.fish"
+if [ -e "$__fish_config_dir/configs/local/pre.fish"  ]
+	source "$__fish_config_dir/configs/local/pre.fish"
 end
 
 source "$__fish_config_dir/configs/variable.fish" # need to load first
@@ -13,8 +13,8 @@ source "$__fish_config_dir/configs/misc.fish"
 source "$__fish_config_dir/configs/prompt.fish"
 source "$__fish_config_dir/configs/c.fish"
 
-if [ -e "$__fish_config_dir/configs/localpost"  ]
-	source "$__fish_config_dir/configs/localpost.fish"
+if [ -e "$__fish_config_dir/configs/local/post"  ]
+	source "$__fish_config_dir/configs/local/post.fish"
 end
 
 if ! [ "$_KJUQ_FISH_CONFIG_LOADED" = '0' ];
