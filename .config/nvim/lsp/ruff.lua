09684cc0e28d57610da5ea7321854d5d58ca238c
kjuq <44694396+kjuq@users.noreply.github.com>
2026-05-09 22:12:20 +0900

    Disable fixall of ruff's codeaction

--- .config/nvim/lsp/ruff.lua
+++ .config/nvim/lsp/ruff.lua
@@ -1,19 +1,19 @@
-vim.api.nvim_create_autocmd({ 'LspAttach' }, {
-	group = vim.api.nvim_create_augroup('kjuq_lspattach_ruff', {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		if client.name ~= 'ruff' then
-			return
-		end
-		client.server_capabilities.hoverProvider = false
-		vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
-			group = vim.api.nvim_create_augroup('kjuq_ruff_codeaction_onsave', {}),
-			callback = function()
-				require('kjuq.lsp_module').apply_codeaction(client, 'source.fixAll.ruff', args.buf)
-			end,
-		})
-	end,
-})
+-- vim.api.nvim_create_autocmd({ 'LspAttach' }, {
+-- 	group = vim.api.nvim_create_augroup('kjuq_lspattach_ruff', {}),
+-- 	callback = function(args)
+-- 		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+-- 		if client.name ~= 'ruff' then
+-- 			return
+-- 		end
+-- 		client.server_capabilities.hoverProvider = false
+-- 		vim.api.nvim_create_autocmd({ 'BufWritePre' }, {
+-- 			group = vim.api.nvim_create_augroup('kjuq_ruff_codeaction_onsave', {}),
+-- 			callback = function()
+-- 				require('kjuq.lsp_module').apply_codeaction(client, 'source.fixAll.ruff', args.buf)
+-- 			end,
+-- 		})
+-- 	end,
+-- })
 
 ---@type vim.lsp.Config
 return {
