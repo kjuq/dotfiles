fd8287560427d47f60b3d6e5d9ef74000eab15a0
kjuq <44694396+kjuq@users.noreply.github.com>
2026-05-20 13:51:22 +0900

    Fix aerial.nvim

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,19 +1,13 @@
-local map = require('kjuq.lazy').generate_map('', 'Aerial: ')
-
 ---@module 'lazy'
 ---@type LazySpec
 local spec = { 'https://github.com/stevearc/aerial.nvim' }
 
-spec.keys = {
-	map('gO', 'n', '<CMD>AerialToggle<CR>', 'Toggle'),
-}
-
 spec.init = function()
 	vim.api.nvim_create_autocmd({ 'FileType' }, {
 		pattern = 'markdown',
-		group = vim.api.nvim_create_augroup('kjuq_disable_builtin_gO', {}),
+		group = vim.api.nvim_create_augroup('kjuq_aerial_override_builtin_gO', {}),
 		callback = function()
-			vim.keymap.del('n', 'gO', { buffer = true })
+			vim.keymap.set('n', 'gO', require('aerial').open, { desc = 'Aerial: Toggle', buffer = true })
 		end,
 	})
 end
