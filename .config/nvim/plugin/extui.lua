7df9242e9a81177f4d75d710e694a76a84a986f0
kjuq <44694396+kjuq@users.noreply.github.com>
2026-07-17 18:32:50 +0900

    chore: ui2

--- .config/nvim/plugin/extui.lua
+++ .config/nvim/plugin/extui.lua
@@ -4,9 +4,13 @@ if vim.fn.has('nvim-0.12') == 0 then
 end
 
 require('vim._core.ui2').enable({
-	enable = true,
-	msg = {
-		target = 'msg',
-		timeout = 1500, -- ms
+	enable = true, -- Whether to enable or disable the UI.
+	msg = { -- Options related to the message module.
+		targets = 'msg',
+		msg = { -- Options related to msg window.
+			timeout = 4000, -- ms
+		},
 	},
 })
+
+vim.cmd.set('fillchars+=msgsep:┄')
--- .config/nvim/plugin/lspprogress.lua
+++ .config/nvim/plugin/lspprogress.lua
@@ -3,25 +3,25 @@ if vim.fn.has('nvim-0.12') == 0 then
 	return
 end
 
-vim.api.nvim_create_autocmd('LspAttach', {
-	callback = function(ev)
-		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-		vim.api.nvim_echo({ { '[' }, { client.name }, { '] Attached' } }, true, {})
-	end,
-})
-
--- vim.api.nvim_create_autocmd('LspProgress', {
+-- vim.api.nvim_create_autocmd('LspAttach', {
 -- 	callback = function(ev)
--- 		local value = ev.data.params.value
 -- 		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
--- 		local label = '[' .. client.name .. ']'
--- 		-- TODO: Show progress
--- 		local status = vim.lsp.status()
--- 		vim.api.nvim_echo({ { label .. ' ' .. status } }, true, {})
--- 		if value.kind == 'begin' then
--- 			vim.api.nvim_echo({ { label }, { ' ' }, { value.title }, { 'began' } }, true, {})
--- 		elseif value.kind == 'end' then
--- 			vim.api.nvim_echo({ { label }, { ' ' }, { value.title }, { ' ' }, { 'ended' } }, true, {})
--- 		end
+-- 		vim.api.nvim_echo({ { '[' }, { client.name }, { '] Attached' } }, true, {})
 -- 	end,
 -- })
+
+vim.api.nvim_create_autocmd('LspProgress', {
+	callback = function(ev)
+		local value = ev.data.params.value
+		local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+		vim.api.nvim_echo({ { value.message or 'done' } }, false, {
+			-- id = 'lsp.' .. ev.data.client_id,
+			id = 'lsp.' .. client.id,
+			kind = 'progress',
+			source = 'vim.lsp',
+			title = '[' .. client.name .. '] ' .. value.title,
+			status = value.kind ~= 'end' and 'running' or 'success',
+			percent = value.percentage,
+		})
+	end,
+})
