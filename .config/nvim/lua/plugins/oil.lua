8361999cf3a064112f3f6eee3a460a1eb533e0bc
kjuq <44694396+kjuq@users.noreply.github.com>
2026-07-23 16:40:04 +0900

    not lazy loading oil.nvim

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -2,22 +2,7 @@
 ---@type LazySpec
 local spec = { 'https://github.com/stevearc/oil.nvim' }
 
-local argv_contains = function(patterns)
-	patterns = type(patterns) == 'string' and { patterns } or patterns --[[@ as string[] ]]
-	local argv = vim.fn.argv() --[[@ as string[] ]]
-	for _, arg in pairs(argv) do
-		for _, pattern in pairs(patterns) do
-			if string.find(arg, pattern, 1, true) then
-				return true
-			end
-		end
-	end
-	return false
-end
-
-spec.lazy = not argv_contains('oil-ssh://')
-
-spec.event = 'VeryLazy'
+spec.lazy = false
 
 local map = require('kjuq.lazy').generate_map('', 'Oil: ')
 spec.keys = {
